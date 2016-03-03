50ccfb0a76e6931758fadd547790f69de2fc94a4
Stuart Colville <scolville@mozilla.com>
2016-03-03 19:06:07 +0000

    Add stub gettext

--- karma.conf.js
+++ karma.conf.js
@@ -1,4 +1,4 @@
 // This allows the karma conf to use ES6.
 require('./server.babel');
 
-module.exports = exports = require('./config/karma.config').default;
+module.exports = exports = require('./src/config/karma.config').default;
