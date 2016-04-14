899d294fa2540279a39cef698218cfa28d0cfbbd
Stuart Colville <scolville@mozilla.com>
2016-04-14 17:55:51 +0100

    Add a createStore function to disco pane

--- src/disco/server.js
+++ src/disco/server.js
@@ -1,7 +1,7 @@
 import baseServer from 'core/server/base';
+import createStore from './store';
 
 import routes from './routes';
 
-const app = baseServer(routes);
+const app = baseServer(routes, createStore);
 export default app;
-
