85590d291c110dcda7fadd5b9a3c8e402976a1d3
Stuart Colville <scolville@mozilla.com>
2016-05-03 12:21:46 +0100

    Add customizable env vars

--- tests/server/TestConfig.js
+++ tests/server/TestConfig.js
@@ -44,3 +44,27 @@ describe('Config', () => {
     assert.equal(config.util.getEnv('NODE_ENV'), 'development');
   });
 });
+
+
+describe('Config Environment Variables', () => {
+  afterEach(() => {
+    delete process.env.SERVER_HOST;
+    delete process.env.SERVER_PORT;
+  });
+
+  it('should allow host overrides', () => {
+    let config = requireUncached('config');
+    assert.equal(config.get('serverHost'), '127.0.0.1', 'initial host is set');
+    process.env.SERVER_HOST = '0.0.0.0';
+    config = requireUncached('config');
+    assert.equal(config.get('serverHost'), '0.0.0.0', 'host is overidden');
+  });
+
+  it('should allow port overrides', () => {
+    let config = requireUncached('config');
+    assert.equal(config.get('serverPort'), '4000', 'Initial port is set');
+    process.env.SERVER_PORT = '5000';
+    config = requireUncached('config');
+    assert.equal(config.get('serverPort'), '5000', 'Port is overidden');
+  });
+});
