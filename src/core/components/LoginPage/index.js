661c5abf47b25bccac5684c40e149825651b0218
Mark Striemer <mstriemer@mozilla.com>
2016-05-09 17:20:17 -0500

    Refactor how LoginRequired is used (fixes #345)

--- src/core/components/LoginPage/index.js
+++ src/core/components/LoginPage/index.js
@@ -1,4 +1,5 @@
 import React, { PropTypes } from 'react';
+import Helmet from 'react-helmet';
 
 import { startLoginUrl } from 'core/api';
 import { gettext as _ } from 'core/utils';
@@ -9,10 +10,12 @@ export default class LoginPage extends React.Component {
   }
 
   render() {
+    const title = _('Login Required');
     const { message } = this.props;
     return (
       <div>
-        <h1>{_('Login Required')}</h1>
+        <Helmet title={title} />
+        <h1>{title}</h1>
         <p className="login-message">
           {message || _('You must be logged in to access this page.')}
         </p>
