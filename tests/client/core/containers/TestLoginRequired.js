859e37b97d6db6be6a1f5a7125bfa0962fe17564
Mark Striemer <mstriemer@mozilla.com>
2016-05-10 00:09:30 -0500

    Don't fix things

--- tests/client/core/containers/TestLoginRequired.js
+++ tests/client/core/containers/TestLoginRequired.js
@@ -4,44 +4,42 @@ import { shallowRender } from 'tests/client/helpers';
 import { mapStateToProps, LoginRequired } from 'core/containers/LoginRequired';
 import LoginPage from 'core/components/LoginPage';
 
-describe('LoginRequired helpers', () => {
+describe('<LoginRequired />', () => {
   class MyComponent extends React.Component {
     render() {
       return <p>Authenticated content.</p>;
     }
   }
 
-  describe('rendered component when not authenticated', () => {
-    it('renders <LoginPage />', () => {
-      const root = shallowRender(<LoginRequired Component={MyComponent} authenticated={false} />);
-      assert.equal(root.type, LoginPage);
-    });
+  it('renders <LoginPage /> when unauthenticated', () => {
+    const root = shallowRender(
+      <LoginRequired authenticated={false}>
+        <MyComponent />
+      </LoginRequired>
+    );
+    assert.equal(root.type, LoginPage);
   });
 
-  describe('rendered component when authenticated', () => {
-    it('renders the child component', () => {
-      const root = shallowRender(<LoginRequired Component={MyComponent} authenticated />);
-      assert.equal(root.type, MyComponent);
-    });
-
-    it('passes along its props', () => {
-      const root = shallowRender(
-        <LoginRequired Component={MyComponent} authenticated foo="bar" />);
-      assert.deepEqual(root.props, {foo: 'bar'});
-    });
+  it('renders the children when authenticated', () => {
+    const root = shallowRender(
+      <LoginRequired authenticated>
+        <MyComponent />
+      </LoginRequired>
+    );
+    assert.equal(root.type, MyComponent);
   });
 
   describe('mapStateToProps', () => {
-    it('sets authenticated and Component when authenticated', () => {
+    it('sets authenticated to true when there is a token', () => {
       assert.deepEqual(
-        mapStateToProps(MyComponent)({auth: {token: 'foo'}}),
-        {authenticated: true, Component: MyComponent});
+        mapStateToProps({auth: {token: 'foo'}}),
+        {authenticated: true});
     });
 
-    it('sets authenticated and Component when unauthenticated', () => {
+    it('sets authenticated to false when there is not a token', () => {
       assert.deepEqual(
-        mapStateToProps(MyComponent)({auth: {}}),
-        {authenticated: false, Component: MyComponent});
+        mapStateToProps({auth: {}}),
+        {authenticated: false});
     });
   });
 });
