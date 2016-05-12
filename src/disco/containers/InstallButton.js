458c2695eb804419ba119cc48fadd6d00782076d
Mark Striemer <mstriemer@gmail.com>
2016-05-12 13:44:27 -0500

    Fake the install status (fixes #312) (#366)

--- src/disco/containers/InstallButton.js
+++ src/disco/containers/InstallButton.js
@@ -7,40 +7,52 @@ import 'disco/css/InstallButton.scss';
 import {
   DOWNLOADING,
   INSTALLED,
+  UNINSTALLED,
   UNKNOWN,
   validInstallStates as validStates,
 } from 'disco/constants';
 
 export class InstallButton extends React.Component {
   static propTypes = {
-    handleClick: PropTypes.func,
+    install: PropTypes.func,
+    uninstall: PropTypes.func,
     handleChange: PropTypes.func,
     guid: PropTypes.string,
-    progress: PropTypes.number,
+    url: PropTypes.string,
+    downloadProgress: PropTypes.number,
     slug: PropTypes.string.isRequired,
-    state: PropTypes.oneOf(validStates),
+    status: PropTypes.oneOf(validStates),
   }
 
   static defaultProps = {
-    state: UNKNOWN,
-    progress: 0,
+    status: UNKNOWN,
+    downloadProgress: 0,
+  }
+
+  handleClick = () => {
+    const { status } = this.props;
+    if (status === UNINSTALLED) {
+      this.props.install();
+    } else if (status === INSTALLED) {
+      this.props.uninstall();
+    }
   }
 
   render() {
-    const { state, progress } = this.props;
+    const { status, downloadProgress } = this.props;
 
-    if (validStates.indexOf(state) === -1) {
-      throw new Error('Invalid add-on state');
+    if (!validStates.includes(status)) {
+      throw new Error('Invalid add-on status');
     }
 
-    const isInstalled = state === INSTALLED;
-    const isDisabled = state === UNKNOWN;
-    const isDownloading = state === DOWNLOADING;
-    const switchClasses = `switch ${state}`;
+    const isInstalled = status === INSTALLED;
+    const isDisabled = status === UNKNOWN;
+    const isDownloading = status === DOWNLOADING;
+    const switchClasses = `switch ${status}`;
 
     return (
-      <div className={switchClasses} onClick={this.props.handleClick}
-        data-download-progress={isDownloading ? progress : 0}>
+      <div className={switchClasses} onClick={this.handleClick}
+        data-download-progress={isDownloading ? downloadProgress : 0}>
         <input
           className="visually-hidden"
           checked={isInstalled}
@@ -56,8 +68,40 @@ export class InstallButton extends React.Component {
   }
 }
 
-function mapStateToProps(state, ownProps) {
-  return (state.installations || {})[ownProps.slug];
+export function mapStateToProps(state, ownProps) {
+  return (state.installations || {})[ownProps.slug] || {};
 }
 
-export default connect(mapStateToProps)(InstallButton);
+export function mapDispatchToProps(dispatch, ownProps) {
+  const { slug } = ownProps;
+  const url = 'foo';
+  const guid = 'foo@foo.com';
+  dispatch({type: 'INSTALL_STATE', payload: {slug, guid, url, status: UNINSTALLED}});
+  /* istanbul ignore next */
+  return {
+    install() {
+      dispatch({type: 'START_DOWNLOAD', payload: {slug, guid, url}});
+      let downloadProgress = 0;
+      const downloading = setInterval(() => {
+        downloadProgress += Math.ceil(Math.random() * 10 + 15);
+        downloadProgress = Math.min(downloadProgress, 100);
+        dispatch({type: 'DOWNLOAD_PROGRESS', payload: {slug, downloadProgress}});
+        if (downloadProgress >= 100) {
+          clearInterval(downloading);
+          dispatch({type: 'START_INSTALL', payload: {slug}});
+          setTimeout(() => {
+            dispatch({type: 'INSTALL_COMPLETE', payload: {slug}});
+          }, 5000);
+        }
+      }, 500);
+    },
+    uninstall() {
+      dispatch({type: 'START_UNINSTALL', payload: {slug}});
+      setTimeout(() => {
+        dispatch({type: 'UNINSTALL_COMPLETE', payload: {slug}});
+      }, 5000);
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(InstallButton);
--- src/disco/store.js
+++ src/disco/store.js
@@ -2,9 +2,11 @@ import { createStore as _createStore, combineReducers } from 'redux';
 import { reducer as reduxAsyncConnect } from 'redux-async-connect';
 import { middleware } from 'core/store';
 
+import installations from 'disco/reducers/installations';
+
 export default function createStore(initialState = {}) {
   return _createStore(
-    combineReducers({reduxAsyncConnect}),
+    combineReducers({installations, reduxAsyncConnect}),
     initialState,
     middleware(),
   );
