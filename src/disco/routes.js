523157cee1ddccbbb5fd378b8ba9b5200486e187
Mark Striemer <mstriemer@mozilla.com>
2016-05-19 14:55:57 -0500

    Support the disco pane URL format (fixes #306)

--- src/disco/routes.js
+++ src/disco/routes.js
@@ -7,5 +7,9 @@ import DiscoPane from './containers/DiscoPane';
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={DiscoPane} />
+    <Route
+      path="/:locale/firefox/discovery/pane/:version/:platform/:compatibilityMode"
+      component={DiscoPane}
+    />
   </Route>
 );
