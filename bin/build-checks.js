e03d9b2991340686e816cb38a9d78b827e9a39fb
Stuart Colville <scolville@mozilla.com>
2016-07-05 20:30:46 +0100

    Prevent build and start without NODE_APP_INSTANCE

--- bin/build-checks.js
+++ bin/build-checks.js
@@ -8,19 +8,9 @@ const config = require('config');
 
 const appName = config.get('appName');
 
-if (!appName) {
+// Bail if appName isn't set unless explicitly enabled.
+if (!appName && process.env.ADDONS_FRONTEND_BUILD_ALL !== '1') {
   console.log(
     chalk.red('Please specify the appName with NODE_APP_INSTANCE'));
   process.exit(1);
 }
-
-if (config.util.getEnv('NODE_ENV') === 'development') {
-  if (!require('piping')({
-    hook: true,
-    ignore: /(\/\.|~$|\.json|\.scss$)/i,
-  })) {
-    return;
-  }
-}
-
-require('core/server/base').runServer();
