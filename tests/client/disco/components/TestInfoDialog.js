cc1b9c1fcd96ca185a486908c0d5ae3eb7bea5b5
Stuart Colville <scolville@mozilla.com>
2016-07-13 14:01:00 +0100

    Dismiss notification when clicking outside

--- tests/client/disco/components/TestInfoDialog.js
+++ tests/client/disco/components/TestInfoDialog.js
@@ -1,32 +1,32 @@
 import React from 'react';
 import { Simulate, renderIntoDocument } from 'react-addons-test-utils';
-import { findDOMNode } from 'react-dom';
-import { sprintf } from 'jed';
+import ReactDOM, { findDOMNode } from 'react-dom';
 
-import {
-  InfoDialog,
-} from 'disco/components/InfoDialog';
+import InfoDialog from 'disco/components/InfoDialog';
+import I18nProvider from 'core/i18n/Provider';
 
 import { getFakeI18nInst } from 'tests/client/helpers';
 
 
+let closeAction;
+
+function getInfoDialog(props = {}) {
+  closeAction = sinon.stub();
+  const renderProps = {
+    addonName: 'A Test Add-on',
+    imageURL: 'https://addons-dev-cdn.allizom.org/whatever',
+    closeAction,
+    ...props,
+  };
+  return <InfoDialog {...renderProps} />;
+}
+
 describe('<InfoDialog />', () => {
-  let closeAction;
-
   function renderInfoDialog(props = {}) {
-    closeAction = sinon.stub();
-    const renderProps = {
-      addonName: 'A Test Add-on',
-      imageURL: 'https://addons-dev-cdn.allizom.org/whatever',
-      closeAction,
-      i18n: getFakeI18nInst(),
-      ...props,
-    };
-
-    renderProps.i18n.sprintf = sprintf;
-
     return renderIntoDocument(
-      <InfoDialog {...renderProps} />);
+      <I18nProvider i18n={getFakeI18nInst()}>
+        {getInfoDialog(props)}
+      </I18nProvider>);
   }
 
   it('Should render a dialog with aria role', () => {
@@ -57,6 +57,48 @@ describe('<InfoDialog />', () => {
     const dialog = renderInfoDialog();
     const root = findDOMNode(dialog);
     Simulate.click(root.querySelector('button'));
+    assert.ok(closeAction.called, 'closeAction stub was called');
+  });
+});
+
+
+describe('Clicking outside <InfoDialog />', () => {
+  let mountNode;
+
+  function simulateClick(node) {
+    const event = document.createEvent('Event');
+    event.initEvent('mousedown', true, true);
+    node.dispatchEvent(event);
+    return event;
+  }
+
+  beforeEach(() => {
+    mountNode = document.createElement('div');
+    document.body.appendChild(mountNode);
+  });
+
+  afterEach(() => {
+    ReactDOM.unmountComponentAtNode(mountNode);
+    document.body.removeChild(mountNode);
+  });
+
+  it('should call closeAction func when clicking outside', () => {
+    class FakeContainer extends React.Component {
+      render() {
+        return (
+          <I18nProvider i18n={getFakeI18nInst()}>
+            <div>
+              {getInfoDialog()}
+              <div ref="outsideComponent" onClick={(e) => e.stopPropagation()} />
+            </div>
+          </I18nProvider>
+        );
+      }
+    }
+
+    const rootComponent = ReactDOM.render(<FakeContainer />, mountNode);
+    const outsideNode = rootComponent.refs.outsideComponent;
+    simulateClick(outsideNode);
     assert.ok(closeAction.called, 'closeAction stub was called');
   });
 });
