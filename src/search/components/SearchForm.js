a658296144128b76425a06bbabc096b447f98182
Mark Striemer <mstriemer@mozilla.com>
2016-08-09 14:50:35 -0500

    Admin search I'm feeling lucky (fixes #808)

--- src/search/components/SearchForm.js
+++ src/search/components/SearchForm.js
@@ -1,12 +1,17 @@
 import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
 
+import { fetchAddon } from 'core/api';
+import { loadEntities } from 'core/actions';
 import { gettext as _ } from 'core/utils';
 
 import 'search/css/SearchForm.scss';
 import 'search/css/lib/buttons.scss';
 
-export default class SearchForm extends React.Component {
+export class SearchForm extends React.Component {
   static propTypes = {
+    api: PropTypes.object.isRequired,
+    loadAddon: PropTypes.func.isRequired,
     pathname: PropTypes.string.isRequired,
     query: PropTypes.string,
   }
@@ -14,11 +19,23 @@ export default class SearchForm extends React.Component {
     router: PropTypes.object,
   }
 
+  goToSearch(query) {
+    const { pathname } = this.props;
+    this.context.router.push(`${pathname}?q=${query}`);
+  }
+
   handleSubmit = (e) => {
     e.preventDefault();
-    const { pathname } = this.props;
+    this.goToSearch(this.refs.query.value);
+  }
+
+  handleGo = (e) => {
+    e.preventDefault();
     const query = this.refs.query.value;
-    this.context.router.push(`${pathname}?q=${query}`);
+    this.props.loadAddon({ api: this.props.api, query })
+      .then(
+        (slug) => this.context.router.push(`/search/addons/${slug}`),
+        () => this.goToSearch(query));
   }
 
   render() {
@@ -27,8 +44,29 @@ export default class SearchForm extends React.Component {
       <form ref="form" className="search-form" onSubmit={this.handleSubmit}>
         <label className="visually-hidden" htmlFor="q">{_('Search')}</label>
         <input ref="query" type="search" name="q" placeholder={_('Search')} defaultValue={query} />
-        <button className="button" ref="submit" type="submit">{_('Search')}</button>
+        <button className="button button-middle" ref="submit" type="submit">{_('Search')}</button>
+        <button className="button button-end button-inverse" ref="go" onClick={this.handleGo}>
+          {_("I'm Feeling Lucky")}
+        </button>
       </form>
     );
   }
 }
+
+export function mapStateToProps({ api }) {
+  return { api };
+}
+
+export function mapDispatchToProps(dispatch) {
+  return {
+    loadAddon({ api, query }) {
+      return fetchAddon({ slug: query, api })
+        .then(({ entities, result }) => {
+          dispatch(loadEntities(entities));
+          return result;
+        });
+    },
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SearchForm);
