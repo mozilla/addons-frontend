3c0ef350075512d6ba71d163ac6d44f7e09b6b9b
Stuart Colville <scolville@mozilla.com>
2016-09-01 14:02:02 +0100

    Add server tests with mocked API

---
+++ tests/server/amo/TestDetailsPage.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-loop-func */
+import config from 'config';
+import request from 'supertest-as-promised';
+
+import { runServer } from 'core/server/base';
+
+
+const fetchMock = require('fetch-mock');
+
+const defaultURL = '/en-US/firefox/addon/fakeaddon/';
+const detailsAPIURL = `${config.get('apiHost')}/api/v3/addons/addon/fakeaddon/?lang=en-US`;
+
+describe('Details Page', () => {
+  let app;
+
+  before(() => runServer({ listen: false, app: 'amo' })
+    .then((server) => {
+      app = server;
+    }));
+
+  after(() => {
+    webpackIsomorphicTools.undo();
+  });
+
+  afterEach(() => {
+    fetchMock.restore();
+  });
+
+  it('should surface a 401 error from the API', () => {
+    fetchMock.get(detailsAPIURL, 401);
+    return request(app)
+      .get(defaultURL)
+      .expect(401);
+  });
+
+  it('should surface a 404 error from the API', () => {
+    fetchMock.get(detailsAPIURL, 404);
+    return request(app)
+      .get(defaultURL)
+      .expect(404);
+  });
+
+  it('should surface an unknown error from the API as a 500', () => {
+    fetchMock.get(detailsAPIURL, 503);
+    return request(app)
+      .get(defaultURL)
+      .expect(500);
+  });
+});
