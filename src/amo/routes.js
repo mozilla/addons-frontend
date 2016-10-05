138cd5eb28e1e68501b2ca7fd9fca53dd18dd988
Kumar McMillan <kumar.mcmillan@gmail.com>
2016-10-05 12:16:36 -0500

    setting aside AddonReview routes for now

--- src/amo/routes.js
+++ src/amo/routes.js
@@ -3,7 +3,6 @@ import { IndexRoute, Route } from 'react-router';
 
 import HandleLogin from 'core/containers/HandleLogin';
 
-import AddonReview from './components/AddonReview';
 import App from './containers/App';
 import Home from './containers/Home';
 import DetailPage from './containers/DetailPage';
@@ -13,7 +12,6 @@ export default (
   <Route path="/:lang/:application" component={App}>
     <IndexRoute component={Home} />
     <Route path="addon/:slug/" component={DetailPage} />
-    <Route path="addon/:slug/review/:id/" component={AddonReview} />
     <Route path="fxa-authenticate" component={HandleLogin} />
     <Route path="search/" component={SearchPage} />
   </Route>
