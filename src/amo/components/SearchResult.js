3e2d42f52304a6fa51c0eb990cfb78a30dbd006d
tofumatt ☕️ <matt@lonelyvegan.com>
2016-10-12 20:10:42 +0100

    Implement styles from new mocks (close #1192) (#1194)
    Update styles from new mocks (close #1192)

--- src/amo/components/SearchResult.js
+++ src/amo/components/SearchResult.js
@@ -14,6 +14,20 @@ class SearchResult extends React.Component {
 
   render() {
     const { i18n, lang, result } = this.props;
+
+    // TODO: Implement a rating component and style the stars.
+    const rating = result.ratings && result.ratings.average ? (
+      <h3 className="SearchResult-rating">
+        <span className="visually-hidden">{i18n.sprintf(
+          i18n.gettext('Average rating: %(rating)s out of 5'),
+          { rating: Math.round(result.ratings.average * 2) / 2 }
+        )}</span>
+      </h3>
+    ) : (
+      <h3 className="SearchResult-rating visually-hidden">
+        {i18n.gettext('No ratings')}
+      </h3>
+    );
     return (
       <li className="SearchResult">
         <Link to={`/${lang}/firefox/addon/${result.slug}/`}
@@ -22,6 +36,7 @@ class SearchResult extends React.Component {
           <section className="SearchResult-main">
             <img className="SearchResult-icon" src={result.icon_url} alt="" />
             <h2 className="SearchResult-heading">{result.name}</h2>
+            {rating}
             <h3 className="SearchResult-author">{result.authors[0].name}</h3>
             <h3 className="SearchResult-users">{i18n.sprintf(
               i18n.ngettext('%(users)s user', '%(users)s users',
