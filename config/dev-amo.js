7aca181ae4b55b3837734bbe57fb6e4a0e7e972c
Jason Thomas <jason@lithiumfox.com>
2016-10-17 17:18:49 -0400

    Add unique staticHost for dev and stage amo APP

---
+++ config/dev-amo.js
@@ -0,0 +1,24 @@
+const amoCDN = 'https://addons-dev-cdn.allizom.org';
+const staticHost = 'https://addons-amo-dev-cdn.allizom.org';
+
+module.exports = {
+  staticHost,
+
+  CSP: {
+    directives: {
+      scriptSrc: [
+        staticHost,
+        'https://www.google-analytics.com/analytics.js',
+      ],
+      styleSrc: [staticHost],
+      imgSrc: [
+        "'self'",
+        'data:',
+        amoCDN,
+        staticHost,
+        'https://www.google-analytics.com',
+      ],
+      mediaSrc: [staticHost],
+    },
+  },
+};
