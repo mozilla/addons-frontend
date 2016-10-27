b27d89377044c4e9c442ec2ff086704d349a7eb4
Stuart Colville <scolville@mozilla.com>
2016-10-27 21:43:31 +0100

    Remove padding compensation for FF50+

--- tests/client/disco/containers/TestApp.js
+++ tests/client/disco/containers/TestApp.js
@@ -5,25 +5,70 @@ import {
   renderIntoDocument,
 } from 'react-addons-test-utils';
 
-import App from 'disco/containers/App';
+import { AppBase, mapStateToProps } from 'disco/containers/App';
 import { getFakeI18nInst } from 'tests/client/helpers';
 
+class MyComponent extends React.Component {
+  render() {
+    return <p>The component</p>;
+  }
+}
+
+function renderApp(extraProps = {}) {
+  const props = {
+    browserVersion: '50',
+    i18n: getFakeI18nInst(),
+    ...extraProps,
+  };
+  const root = findRenderedComponentWithType(renderIntoDocument(
+    <AppBase {...props}>
+      <MyComponent />
+    </AppBase>
+  ), AppBase);
+  return findDOMNode(root);
+}
 
 describe('App', () => {
   it('renders its children', () => {
-    class MyComponent extends React.Component {
-      render() {
-        return <p>The component</p>;
-      }
-    }
-    const root = findRenderedComponentWithType(renderIntoDocument(
-      <App i18n={getFakeI18nInst()}>
-        <MyComponent />
-      </App>
-    ), App).getWrappedInstance();
-
-    const rootNode = findDOMNode(root);
+    const rootNode = renderApp();
     assert.equal(rootNode.tagName.toLowerCase(), 'div');
     assert.equal(rootNode.querySelector('p').textContent, 'The component');
+  });
+
+  it('renders padding compensation class for FF < 50', () => {
+    const rootNode = renderApp({ browserVersion: '49.0' });
+    assert.include(rootNode.className, 'padding-compensation');
+  });
+
+  it('renders padding compensation class for a bogus value', () => {
+    const rootNode = renderApp({ browserVersion: 'whatever' });
+    assert.include(rootNode.className, 'padding-compensation');
+  });
+
+  it('renders padding compensation class for a undefined value', () => {
+    const rootNode = renderApp({ browserVersion: undefined });
+    assert.include(rootNode.className, 'padding-compensation');
+  });
+
+  it('does not render padding compensation class for FF == 50', () => {
+    const rootNode = renderApp({ browserVersion: '50.0' });
+    assert.notInclude(rootNode.className, 'padding-compensation');
+  });
+
+  it('does not render padding compensation class for FF > 50', () => {
+    const rootNode = renderApp({ browserVersion: '52.0a1' });
+    assert.notInclude(rootNode.className, 'padding-compensation');
+  });
+});
+
+describe('mapStateToProps', () => {
+  const fakeRouterParams = {
+    params: {
+      version: '49.0',
+    },
+  };
+
+  it('returns browserVersion', () => {
+    assert.equal(mapStateToProps(null, fakeRouterParams).browserVersion, '49.0');
   });
 });
