a09b60ac9e2e529321708fc1caafdc1e3dae2505
Kumar McMillan <kumar.mcmillan@gmail.com>
2016-12-04 22:30:53 -1000

    fix alphabetical import

--- src/amo/store.js
+++ src/amo/store.js
@@ -1,13 +1,13 @@
 import { createStore as _createStore, combineReducers } from 'redux';
 import { reducer as reduxAsyncConnect } from 'redux-connect';
 
+import reviews from 'amo/reducers/reviews';
 import addons from 'core/reducers/addons';
 import api from 'core/reducers/api';
 import auth from 'core/reducers/authentication';
 import categories from 'core/reducers/categories';
 import errors from 'core/reducers/errors';
 import installations from 'core/reducers/installations';
-import reviews from 'amo/reducers/reviews';
 import search from 'core/reducers/search';
 import { middleware } from 'core/store';
 
