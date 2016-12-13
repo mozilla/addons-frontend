ec8120d949e353cee7174969d3bd1acb7b209595
Stuart Colville <scolville@mozilla.com>
2016-12-13 14:51:58 +0000

    Verbose names + colour vars

--- src/amo/containers/Home.js
+++ src/amo/containers/Home.js
@@ -16,23 +16,23 @@ export class HomePageBase extends React.Component {
     return (
       <div className="HomePage">
         <h2 className="HomePage-subheading">{i18n.gettext('What do you want Firefox to do?')}</h2>
-        <ul className="HomePage-cat-list">
-          <li className="HomePage-cat-li HomePage-block-ads">
+        <ul className="HomePage-category-list">
+          <li className="HomePage-category-li HomePage-block-ads">
             <Link to="#block-ads"><span>{i18n.gettext('Block ads')}</span></Link>
           </li>
-          <li className="HomePage-cat-li HomePage-screenshot">
+          <li className="HomePage-category-li HomePage-screenshot">
             <Link to="#screenshot"><span>{i18n.gettext('Screenshot')}</span></Link>
           </li>
-          <li className="HomePage-cat-li HomePage-save-stuff">
+          <li className="HomePage-category-li HomePage-save-stuff">
             <Link to="#save-stuff"><span>{i18n.gettext('Save stuff')}</span></Link>
           </li>
-          <li className="HomePage-cat-li HomePage-shop-online">
+          <li className="HomePage-category-li HomePage-shop-online">
             <Link to="#shop-online"><span>{i18n.gettext('Shop online')}</span></Link>
           </li>
-          <li className="HomePage-cat-li HomePage-be-social">
+          <li className="HomePage-category-li HomePage-be-social">
             <Link to="#share-stuff"><span>{i18n.gettext('Be social')}</span></Link>
           </li>
-          <li className="HomePage-cat-li HomePage-share-stuff">
+          <li className="HomePage-category-li HomePage-share-stuff">
             <Link to="#share-stuff"><span>{i18n.gettext('Share stuff')}</span></Link>
           </li>
         </ul>
@@ -41,23 +41,23 @@ export class HomePageBase extends React.Component {
         </Link>
 
         <h2 className="HomePage-subheading">{i18n.gettext('How do you want Firefox to look?')}</h2>
-        <ul className="HomePage-cat-list">
-          <li className="HomePage-cat-li HomePage-wild">
+        <ul className="HomePage-category-list">
+          <li className="HomePage-category-li HomePage-wild">
             <Link to="#wild"><span>{i18n.gettext('Wild')}</span></Link>
           </li>
-          <li className="HomePage-cat-li HomePage-abstract">
+          <li className="HomePage-category-li HomePage-abstract">
             <Link to="#abstract"><span>{i18n.gettext('Abstract')}</span></Link>
           </li>
-          <li className="HomePage-cat-li HomePage-fashionable">
+          <li className="HomePage-category-li HomePage-fashionable">
             <Link to="#fashionable"><span>{i18n.gettext('Fashionable')}</span></Link>
           </li>
-          <li className="HomePage-cat-li HomePage-scenic">
+          <li className="HomePage-category-li HomePage-scenic">
             <Link to="#scenic"><span>{i18n.gettext('Scenic')}</span></Link>
           </li>
-          <li className="HomePage-cat-li HomePage-sporty">
+          <li className="HomePage-category-li HomePage-sporty">
             <Link to="#sporty"><span>{i18n.gettext('Sporty')}</span></Link>
           </li>
-          <li className="HomePage-cat-li HomePage-mystical">
+          <li className="HomePage-category-li HomePage-mystical">
             <Link to="#mystical"><span>{i18n.gettext('Mystical')}</span></Link>
           </li>
         </ul>
