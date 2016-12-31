86d71d024f34f069910acaf45528c8e31b2e8677
Mark Striemer <mstriemer@mozilla.com>
2016-12-31 12:46:50 -0600

    Update based on review comments

--- src/amo/containers/Home.js
+++ src/amo/containers/Home.js
@@ -7,17 +7,17 @@ import translate from 'core/i18n/translate';
 
 import 'amo/css/Home.scss';
 
-const CategoryLink = ({ name, slug, text, type }) => (
+const CategoryLink = ({ children, name, slug, type }) => (
   <li className={classNames('HomePage-category-li', `HomePage-${name}`)}>
     <Link to={`/${type}/${slug}/`} className="HomePage-category-link">
-      <span>{text}</span>
+      <span>{children}</span>
     </Link>
   </li>
 );
 CategoryLink.propTypes = {
+  children: PropTypes.node.isRequired,
   name: PropTypes.string.isRequired,
   slug: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
 };
 const ExtensionLink = (props) => <CategoryLink type="extensions" {...props} />;
@@ -34,12 +34,24 @@ export class HomePageBase extends React.Component {
       <div className="HomePage">
         <h2 className="HomePage-subheading">{i18n.gettext('What do you want Firefox to do?')}</h2>
         <ul className="HomePage-category-list">
-          <ExtensionLink name="block-ads" slug="security-privacy" text={i18n.gettext('Block ads')} />
-          <ExtensionLink name="screenshot" slug="photos-media" text={i18n.gettext('Screenshot')} />
-          <ExtensionLink name="news" slug="feeds-news-blogging" text={i18n.gettext('Find news')} />
-          <ExtensionLink name="shop-online" slug="shopping" text={i18n.gettext('Shop online')} />
-          <ExtensionLink name="be-social" slug="social-networking" text={i18n.gettext('Be social')} />
-          <ExtensionLink name="play-games" slug="sports-games" text={i18n.gettext('Play games')} />
+          <ExtensionLink name="block-ads" slug="security-privacy">
+            {i18n.gettext('Block ads')}
+          </ExtensionLink>
+          <ExtensionLink name="screenshot" slug="photos-media">
+            {i18n.gettext('Screenshot')}
+          </ExtensionLink>
+          <ExtensionLink name="news" slug="feeds-news-blogging">
+            {i18n.gettext('Find news')}
+          </ExtensionLink>
+          <ExtensionLink name="shop-online" slug="shopping">
+            {i18n.gettext('Shop online')}
+          </ExtensionLink>
+          <ExtensionLink name="be-social" slug="social-networking">
+            {i18n.gettext('Be social')}
+          </ExtensionLink>
+          <ExtensionLink name="play-games" slug="sports-games">
+            {i18n.gettext('Play games')}
+          </ExtensionLink>
         </ul>
         <Link className="HomePage-extensions-link" to="/extensions/">
           {i18n.gettext('Browse all extensions')}
@@ -47,12 +59,12 @@ export class HomePageBase extends React.Component {
 
         <h2 className="HomePage-subheading">{i18n.gettext('How do you want Firefox to look?')}</h2>
         <ul className="HomePage-category-list">
-          <ThemeLink name="wild" slug="nature" text={i18n.gettext('Wild')} />
-          <ThemeLink name="abstract" slug="abstract" text={i18n.gettext('Abstract')} />
-          <ThemeLink name="fashionable" slug="fashion" text={i18n.gettext('Fashionable')} />
-          <ThemeLink name="scenic" slug="scenery" text={i18n.gettext('Scenic')} />
-          <ThemeLink name="sporty" slug="sports" text={i18n.gettext('Sporty')} />
-          <ThemeLink name="solid" slug="solid" text={i18n.gettext('Solid')} />
+          <ThemeLink name="wild" slug="nature">{i18n.gettext('Wild')}</ThemeLink>
+          <ThemeLink name="abstract" slug="abstract">{i18n.gettext('Abstract')}</ThemeLink>
+          <ThemeLink name="fashionable" slug="fashion">{i18n.gettext('Fashionable')}</ThemeLink>
+          <ThemeLink name="scenic" slug="scenery">{i18n.gettext('Scenic')}</ThemeLink>
+          <ThemeLink name="sporty" slug="sports">{i18n.gettext('Sporty')}</ThemeLink>
+          <ThemeLink name="solid" slug="solid">{i18n.gettext('Solid')}</ThemeLink>
         </ul>
         <Link className="HomePage-themes-link" to="/themes/">
           {i18n.gettext('Browse all themes')}
