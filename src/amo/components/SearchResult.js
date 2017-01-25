e18930bae383b505650c75729090a0d715823e9f
Kumar McMillan <kumar.mcmillan@gmail.com>
2017-01-25 16:03:46 -0600

    move to Rating and get styles looking right

--- src/amo/components/SearchResult.js
+++ src/amo/components/SearchResult.js
@@ -3,6 +3,7 @@ import { compose } from 'redux';
 
 import Link from 'amo/components/Link';
 import translate from 'core/i18n/translate';
+import Rating from 'ui/components/Rating';
 
 import 'core/css/SearchResult.scss';
 
@@ -17,18 +18,12 @@ export class SearchResultBase extends React.Component {
     const { addon, i18n } = this.props;
     const averageDailyUsers = addon.average_daily_users;
 
-    // TODO: Implement a rating component and style the stars.
     const rating = addon.ratings && addon.ratings.average ? (
-      <h3 className="SearchResult-rating">
-        <span className="visually-hidden">{i18n.sprintf(
-          i18n.gettext('Average rating: %(rating)s out of 5'),
-          { rating: Math.round(addon.ratings.average * 2) / 2 }
-        )}</span>
-      </h3>
+      // TODO: round up/down average ratings.
+      <Rating rating={addon.ratings.average} readOnly size="small" />
     ) : (
-      <h3 className="SearchResult-rating visually-hidden">
-        {i18n.gettext('No ratings')}
-      </h3>
+      // TODO: hmm, make this part of Rating?
+      <h3 className="visually-hidden">{i18n.gettext('No ratings')}</h3>
     );
     return (
       <li className="SearchResult">
@@ -38,7 +33,7 @@ export class SearchResultBase extends React.Component {
           <section className="SearchResult-main">
             <img className="SearchResult-icon" src={addon.icon_url} alt="" />
             <h2 className="SearchResult-heading">{addon.name}</h2>
-            {rating}
+            <div className="SearchResult-rating">{rating}</div>
             <h3 className="SearchResult-author">{addon.authors[0].name}</h3>
             <h3 className="SearchResult-users">{i18n.sprintf(
               i18n.ngettext('%(total)s user', '%(total)s users', averageDailyUsers),
