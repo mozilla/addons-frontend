d6fac869d5489674bfe93bde21543fbaa6094eba
Kumar McMillan <kumar.mcmillan@gmail.com>
2017-02-27 16:36:03 -0600

    Use setTimeout() to simulate an event loop error (#1910)

--- src/core/containers/SimulateError.js
+++ src/core/containers/SimulateError.js
@@ -3,5 +3,8 @@ import log from 'core/logger';
 // This HOC can be connected to a route to test internal error handling.
 export default () => {
   log.info('Simulating an error');
-  throw new Error('This is a simulated error');
+  setTimeout(() => {
+    throw new Error('This is a simulated error in the event loop');
+  }, 50);
+  throw new Error('This is a simulated error in Component.render()');
 };
