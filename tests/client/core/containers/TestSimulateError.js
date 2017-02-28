fa2ffef7eac24867b6b1b0d30789457775adfa05
Kumar McMillan <kumar.mcmillan@gmail.com>
2017-02-28 08:58:30 -0600

    Fix set timeout breakage (#1929)

--- tests/client/core/containers/TestSimulateError.js
+++ tests/client/core/containers/TestSimulateError.js
@@ -4,9 +4,25 @@ import { renderIntoDocument } from 'react-addons-test-utils';
 import SimulateError from 'core/containers/SimulateError';
 
 describe('SimulateError', () => {
-  it('simulates an error', () => {
+  let clock;
+
+  beforeEach(() => {
+    clock = sinon.useFakeTimers();
+  });
+
+  afterEach(() => {
+    clock.restore();
+  });
+
+  it('throws a simulated error', () => {
     assert.throws(
       () => renderIntoDocument(<SimulateError />),
-      /simulated error/);
+      /simulated error in Component.render/);
+  });
+
+  it('throws an async error', () => {
+    assert.throws(() => renderIntoDocument(<SimulateError />));
+    // Trigger the setTimeout() callback:
+    assert.throws(() => clock.tick(50), /simulated error in the event loop/);
   });
 });
