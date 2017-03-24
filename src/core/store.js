a59b8a5d0a401c18bc34720f16b8d5c1738a4035
Matthew Riley MacPherson <matt@lonelyvegan.com>
2017-03-24 14:38:35 +0000

    PR tweaks

--- src/core/store.js
+++ src/core/store.js
@@ -6,12 +6,16 @@ import createLogger from 'redux-logger';
 import config from 'config';
 
 
-// These are the actions the loading indicator will response to for showing
+// These are the actions the loading indicator will respond to for showing
 // or hiding the loading bar.
+// These are existing reduxConnect actions that we hook into to dispatch
+// the loading bar actions.
 // This can probably go away when we move to something like redux-saga.
 const PROMISE_PREFIXES = {
   promiseTypeSuffixes: [
+    // Dispatching this action shows the loading bar and starts its animation.
     'BEGIN_GLOBAL_LOAD',
+    // Dispatching either of the following actions stop/hide the loading bar.
     'END_GLOBAL_LOAD',
     'LOAD_FAIL',
   ],
