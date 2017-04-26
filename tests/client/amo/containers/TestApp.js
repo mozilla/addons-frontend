4ff7a3457324de4312dafb251b55e83f139da536
Kumar McMillan <kumar.mcmillan@gmail.com>
2017-04-26 14:26:51 -0500

    fix comment

--- tests/client/amo/containers/TestApp.js
+++ tests/client/amo/containers/TestApp.js
@@ -247,7 +247,7 @@ describe('App', () => {
       // Simulate updating the component with new properties.
       root.componentWillReceiveProps({ authToken });
 
-      const fuzz = 3; // acount for rounding the offset calculation.
+      const fuzz = 3; // account for the rounded offset calculation.
       clock.tick((authTokenValidFor + fuzz) * 1000);
       assert.ok(logOutUser.called, 'expected logOutUser() to be called');
       assert.ok(logOutUser.calledOnce,
