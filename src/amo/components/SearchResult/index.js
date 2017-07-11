a6108549414bc6083aa3a8b4f779ff382e9fc5b7
Kumar McMillan <kumar.mcmillan@gmail.com>
2017-07-11 16:30:26 -0500

    Allow SearchResult to load without an addon

--- src/amo/components/SearchResult/index.js
+++ src/amo/components/SearchResult/index.js
@@ -9,6 +9,7 @@ import { ADDON_TYPE_THEME } from 'core/constants';
 import { isAllowedOrigin, sanitizeHTML } from 'core/utils';
 import { getAddonIconUrl } from 'core/imageUtils';
 import Icon from 'ui/components/Icon';
+import LoadingText from 'ui/components/LoadingText';
 import Rating from 'ui/components/Rating';
 
 import './styles.scss';
@@ -16,21 +17,25 @@ import './styles.scss';
 
 export class SearchResultBase extends React.Component {
   static propTypes = {
-    addon: PropTypes.object.isRequired,
+    addon: PropTypes.object,
     i18n: PropTypes.object.isRequired,
   }
 
-  render() {
+  addonIsTheme() {
+    const { addon } = this.props;
+    return addon && addon.type === ADDON_TYPE_THEME;
+  }
+
+  renderResult() {
     const { addon, i18n } = this.props;
-    const averageDailyUsers = addon.average_daily_users;
-    const isTheme = addon.type === ADDON_TYPE_THEME;
-    const resultClassnames = classNames('SearchResult', {
-      'SearchResult--theme': isTheme,
-    });
+
+    const isTheme = this.addonIsTheme();
+    const averageDailyUsers = addon && addon.average_daily_users;
 
     // Fall-back to default icon if invalid icon url.
     const iconURL = getAddonIconUrl(addon);
-    const themeURL = (addon.theme_data && isAllowedOrigin(addon.theme_data.previewURL)) ?
+    const themeURL = (addon && addon.theme_data &&
+      isAllowedOrigin(addon.theme_data.previewURL)) ?
       addon.theme_data.previewURL : null;
     const imageURL = isTheme ? themeURL : iconURL;
 
@@ -41,60 +46,91 @@ export class SearchResultBase extends React.Component {
       ),
     });
 
-    /* eslint-disable react/no-danger */
+    let addonAuthors = null;
+    const addonAuthorsData = addon && addon.authors && addon.authors.length ?
+      addon.authors : null;
+    if (!addon || addonAuthorsData) {
+      addonAuthors = (
+        <h3 className="SearchResult-author SearchResult--meta-section">
+          {addon ? addonAuthorsData[0].name : <LoadingText />}
+        </h3>
+      );
+    }
+
+    const summaryProps = {};
+    if (addon) {
+      summaryProps.dangerouslySetInnerHTML = sanitizeHTML(addon.summary);
+    } else {
+      summaryProps.children = <LoadingText />;
+    }
+
+    return (
+      <div className="SearchResult-result">
+        <div className={iconWrapperClassnames}>
+          {imageURL ? (
+            <img className="SearchResult-icon" src={imageURL} alt="" />
+          ) : (
+            <p className="SearchResult-notheme">
+              {i18n.gettext('No theme preview available')}
+            </p>
+          )}
+        </div>
+
+        <div className="SearchResult-contents">
+          <h2 className="SearchResult-name">
+            {addon ? addon.name : <LoadingText />}
+          </h2>
+          <p
+            className="SearchResult-summary"
+            {...summaryProps}
+          />
+
+          <div className="SearchResult-metadata">
+            <div className="SearchResult-rating">
+              <Rating
+                rating={addon ? addon.ratings.average : 0}
+                readOnly
+                styleName="small"
+              />
+            </div>
+            {addonAuthors}
+          </div>
+        </div>
+
+        <h3 className="SearchResult-users SearchResult--meta-section">
+          <Icon className="SearchResult-users-icon" name="user-fill" />
+          <span className="SearchResult-users-text">
+            {addon ? i18n.sprintf(i18n.ngettext(
+              '%(total)s user', '%(total)s users', averageDailyUsers),
+              { total: i18n.formatNumber(averageDailyUsers) },
+            ) : <LoadingText width={100} />}
+          </span>
+        </h3>
+      </div>
+    );
+  }
+
+  render() {
+    const { addon } = this.props;
+
+    const result = this.renderResult();
+    const isTheme = this.addonIsTheme();
+    const resultClassnames = classNames('SearchResult', {
+      'SearchResult--theme': isTheme,
+    });
+
     return (
       <li className={resultClassnames}>
-        <Link
-          to={`/addon/${addon.slug}/`}
-          className="SearchResult-link"
-          ref={(el) => { this.name = el; }}
-        >
-          <div className={iconWrapperClassnames}>
-            {imageURL ? (
-              <img className="SearchResult-icon" src={imageURL} alt="" />
-            ) : (
-              <p className="SearchResult-notheme">
-                {i18n.gettext('No theme preview available')}
-              </p>
-            )}
-          </div>
-
-          <div className="SearchResult-contents">
-            <h2 className="SearchResult-name">{addon.name}</h2>
-            <p
-              className="SearchResult-summary"
-              dangerouslySetInnerHTML={sanitizeHTML(addon.summary)}
-            />
-
-            <div className="SearchResult-metadata">
-              <div className="SearchResult-rating">
-                <Rating
-                  rating={addon.ratings.average}
-                  readOnly
-                  styleName="small"
-                />
-              </div>
-              { addon.authors && addon.authors.length ? (
-                <h3 className="SearchResult-author SearchResult--meta-section">
-                  {addon.authors[0].name}
-                </h3>
-              ) : null }
-            </div>
-          </div>
-
-          <h3 className="SearchResult-users SearchResult--meta-section">
-            <Icon className="SearchResult-users-icon" name="user-fill" />
-            <span className="SearchResult-users-text">
-              {i18n.sprintf(i18n.ngettext(
-                '%(total)s user', '%(total)s users', averageDailyUsers),
-                { total: i18n.formatNumber(averageDailyUsers) },
-              )}
-            </span>
-          </h3>
-        </Link>
+        {addon ?
+          <Link
+            to={`/addon/${addon.slug}/`}
+            className="SearchResult-link"
+            ref={(el) => { this.name = el; }}
+          >{result}</Link>
+          : result
+        }
       </li>
     );
-    /* eslint-enable react/no-danger */
   }
 }
 
