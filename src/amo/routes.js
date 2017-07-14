14741ce0ac510dd3522be4c8f784e40d3eb47712
Trishul Goel <trishul.goel@gmail.com>
2017-07-14 10:17:30 +0530

    updated about description

--- src/amo/routes.js
+++ src/amo/routes.js
@@ -20,6 +20,7 @@ import Home from './components/Home';
 import Addon from './components/Addon';
 import NotAuthorized from './components/ErrorPage/NotAuthorized';
 import NotFound from './components/ErrorPage/NotFound';
+import About from './components/StaticPages/About';
 import Search from './components/Search';
 import ServerError from './components/ErrorPage/ServerError';
 
@@ -30,6 +31,7 @@ import ServerError from './components/ErrorPage/ServerError';
 export default (
   <Route path="/:lang/:application" component={App}>
     <IndexRoute component={Home} />
+    <Route path="about" component={About} />
     <Route path="addon/:slug/" component={Addon} />
     <Route path="addon/:addonSlug/reviews/" component={AddonReviewList} />
     <Route path=":visibleAddonType/categories/" component={CategoriesPage} />
