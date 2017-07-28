929aaa1ef469b8fa0f84d999143a72b930d48b80
Kumar McMillan <kumar.mcmillan@gmail.com>
2017-07-28 14:53:46 -0500

    Call APIs asynchronously

--- src/amo/sagas/landing.js
+++ src/amo/sagas/landing.js
@@ -3,7 +3,7 @@ import { oneLine } from 'common-tags';
 // https://github.com/benmosher/eslint-plugin-import/issues/793
 /* eslint-disable import/order */
 import { hideLoading, showLoading } from 'react-redux-loading-bar';
-import { call, put, select, takeEvery } from 'redux-saga/effects';
+import { all, call, put, select, takeEvery } from 'redux-saga/effects';
 /* eslint-enable import/order */
 
 import { loadLanding } from 'amo/actions/landing';
@@ -26,18 +26,19 @@ export function* fetchLandingAddons(
     const api = yield select(getApi);
     const filters = { addonType, page_size: LANDING_PAGE_ADDON_COUNT };
 
-    // TODO: make these calls async
-    const featured = yield call(featuredApi, { api, filters });
-    const highlyRated = yield call(searchApi, {
-      api,
-      filters: { ...filters, sort: SEARCH_SORT_TOP_RATED },
-      page: 1,
-    });
-    const popular = yield call(searchApi, {
-      api,
-      filters: { ...filters, sort: SEARCH_SORT_POPULAR },
-      page: 1,
-    });
+    const [featured, highlyRated, popular] = yield all([
+      call(featuredApi, { api, filters }),
+      call(searchApi, {
+        api,
+        filters: { ...filters, sort: SEARCH_SORT_TOP_RATED },
+        page: 1,
+      }),
+      call(searchApi, {
+        api,
+        filters: { ...filters, sort: SEARCH_SORT_POPULAR },
+        page: 1,
+      }),
+    ]);
 
     yield put(loadLanding({
       addonType, featured, highlyRated, popular,
