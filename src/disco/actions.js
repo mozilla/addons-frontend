ec651cd674e0664177e853a33556cb55b798f271
Kumar McMillan <kumar.mcmillan@gmail.com>
2017-08-04 14:12:06 -0500

    added todo note

--- src/disco/actions.js
+++ src/disco/actions.js
@@ -2,6 +2,7 @@ import { GET_DISCO_RESULTS, DISCO_RESULTS } from 'disco/constants';
 
 export function discoResults(results) {
   return {
+    // TODO: maybe rename this to GOT_DISO_RESULTS
     type: DISCO_RESULTS,
     payload: {
       results,
