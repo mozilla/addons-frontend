faeadba185614fae654b8953ab94bef3d398a048
Kumar McMillan <kumar.mcmillan@gmail.com>
2017-08-10 11:21:51 -0500

    require a context in setViewContext()

---
+++ tests/unit/amo/actions/testViewContext.js
@@ -0,0 +1,8 @@
+import { setViewContext } from 'amo/actions/viewContext';
+
+describe('amo/actions/setViewContext', () => {
+  it('requires a context', () => {
+    expect(() => setViewContext())
+      .toThrow(/context parameter is required/);
+  });
+});
