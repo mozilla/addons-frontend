6e3dfccbba23cfea0c18847c63a6740c211d6f89
Kumar McMillan <kumar.mcmillan@gmail.com>
2017-08-11 10:54:49 -0500

    re-order action creators

--- src/disco/actions.js
+++ src/disco/actions.js
@@ -1,5 +1,15 @@
 import { GET_DISCO_RESULTS, LOAD_DISCO_RESULTS } from 'disco/constants';
 
+export function getDiscoResults({ errorHandlerId } = {}) {
+  if (!errorHandlerId) {
+    throw new Error('errorHandlerId is required');
+  }
+  return {
+    type: GET_DISCO_RESULTS,
+    payload: { errorHandlerId },
+  };
+}
+
 export function loadDiscoResults({ entities, result } = {}) {
   if (!entities) {
     throw new Error('entities parameter is required');
@@ -10,15 +20,5 @@ export function loadDiscoResults({ entities, result } = {}) {
   return {
     type: LOAD_DISCO_RESULTS,
     payload: { entities, result },
-  };
-}
-
-export function getDiscoResults({ errorHandlerId } = {}) {
-  if (!errorHandlerId) {
-    throw new Error('errorHandlerId is required');
-  }
-  return {
-    type: GET_DISCO_RESULTS,
-    payload: { errorHandlerId },
   };
 }
