24462a41e4d7e0b07c56a3cc4615bea8665c6dcd
William Durand <will+git@drnd.me>
2017-08-31 15:43:04 +0200

    Cancel autocomplete saga on AUTOCOMPLETE_CANCELLED

--- src/core/sagas/autocomplete.js
+++ src/core/sagas/autocomplete.js
@@ -1,10 +1,14 @@
 // Disabled because of
 // https://github.com/benmosher/eslint-plugin-import/issues/793
 /* eslint-disable import/order */
-import { call, put, select, takeLatest } from 'redux-saga/effects';
+import { call, put, race, select, take, takeLatest } from 'redux-saga/effects';
 /* eslint-enable import/order */
 
-import { AUTOCOMPLETE_STARTED, autocompleteLoad } from 'core/reducers/autocomplete';
+import {
+  AUTOCOMPLETE_CANCELLED,
+  AUTOCOMPLETE_STARTED,
+  autocompleteLoad,
+} from 'core/reducers/autocomplete';
 import { autocomplete as autocompleteApi } from 'core/api';
 import log from 'core/logger';
 import { createErrorHandler, getState } from 'core/sagas/utils';
@@ -35,5 +39,10 @@ export function* fetchAutocompleteResults({ payload }) {
 }
 
 export default function* autocompleteSaga() {
-  yield takeLatest(AUTOCOMPLETE_STARTED, fetchAutocompleteResults);
+  yield takeLatest(AUTOCOMPLETE_STARTED, function* fetchOrCancel(...args) {
+    yield race({
+      fetch: call(fetchAutocompleteResults, ...args),
+      cancel: take(AUTOCOMPLETE_CANCELLED),
+    });
+  });
 }
