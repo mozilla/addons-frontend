f94fdde7b4a0198bf469c5d6b3df003c82f160b2
Stuart Colville <scolville@mozilla.com>
2017-09-14 10:57:28 +0100

    Fix review comments

--- src/amo/components/Categories/index.js
+++ src/amo/components/Categories/index.js
@@ -137,7 +137,8 @@ export class CategoriesBase extends React.Component {
               return (
                 <li className="Categories-item" key={name}>
                   <Button
-                    className={`Categories-link Categories--category-color-${getCategoryColor(category)}`}
+                    className={`Categories-link
+                      Categories--category-color-${getCategoryColor(category)}`}
                     to={`/${visibleAddonType(addonType)}/${slug}/`}
                   >
                     {name}
