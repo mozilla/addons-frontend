2581feb06d0d3f40d13f083ee125cca405786987
Trishul <trishul.goel@gmail.com>
2017-10-05 20:50:27 +0530

    better use of enzyme function

--- tests/unit/amo/components/TestFooter.js
+++ tests/unit/amo/components/TestFooter.js
@@ -19,18 +19,18 @@ describe('Footer', () => {
     // cause a 404 error.
     // See:
     // github.com/mozilla/addons-frontend/pull/2524#pullrequestreview-42911624
-    expect(root.find('.Footer-privacy-link').text()).toEqual('Privacy');
-    expect(root.find('.Footer-privacy-link').prop('href')).toEqual('https://www.mozilla.org/privacy/websites/');
-    expect(root.find('.Footer-legal-link').text()).toEqual('Legal');
-    expect(root.find('.Footer-legal-link').prop('href')).toEqual('https://www.mozilla.org/about/legal/');
-    expect(root.find('.Footer-cookies-link').text()).toEqual('Cookies');
-    expect(root.find('.Footer-cookies-link').prop('href')).toEqual('https://www.mozilla.org/privacy/websites/#cookies');
-    expect(root.find('.Footer-trademark-abuse-link').text()).toEqual('Report Trademark Abuse');
-    expect(root.find('.Footer-trademark-abuse-link').prop('href')).toEqual('https://www.mozilla.org/about/legal/fraud-report/');
+    expect(root.find('.Footer-privacy-link')).toHaveText('Privacy');
+    expect(root.find('.Footer-privacy-link')).toHaveProp('href', 'https://www.mozilla.org/privacy/websites/');
+    expect(root.find('.Footer-legal-link')).toHaveText('Legal');
+    expect(root.find('.Footer-legal-link')).toHaveProp('href', 'https://www.mozilla.org/about/legal/');
+    expect(root.find('.Footer-cookies-link')).toHaveText('Cookies');
+    expect(root.find('.Footer-cookies-link')).toHaveProp('href', 'https://www.mozilla.org/privacy/websites/#cookies');
+    expect(root.find('.Footer-trademark-abuse-link')).toHaveText('Report Trademark Abuse');
+    expect(root.find('.Footer-trademark-abuse-link')).toHaveProp('href', 'https://www.mozilla.org/about/legal/fraud-report/');
 
     // This link isn't localized because MDN will 404 on some
     // locales and not others.
     // See also https://bugzilla.mozilla.org/show_bug.cgi?id=1283422
-    expect(root.find('.Footer-bug-report-link').prop('href')).toEqual('https://developer.mozilla.org/Add-ons/AMO/Policy/Contact');
+    expect(root.find('.Footer-bug-report-link')).toHaveProp('href', 'https://developer.mozilla.org/Add-ons/AMO/Policy/Contact');
   });
 });
