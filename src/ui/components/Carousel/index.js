09f46c1843f9af9487a3cf4d0722931633512aee
tofumatt ☕️ <matt@lonelyvegan.com>
2017-10-05 14:38:50 +0100

    fix: Render carousel on client to fix CSP errors (fix #3349) (#3351)
    * fix: Render carousel on client to fix CSP errors (fix #3349)
    
    * fix: Don't rely on config
    
    * fix: RTL CSS issues
    
    * fix: Improve Carousel loading and scrolling without JS
    
    * fix: RTL ordering on first-load

--- src/ui/components/Carousel/index.js
+++ src/ui/components/Carousel/index.js
@@ -1,8 +1,10 @@
 /* @flow */
-import NukaCarousel from 'nuka-carousel';
+import classNames from 'classnames';
 import React from 'react';
 import { compose } from 'redux';
+import NukaCarousel from 'nuka-carousel';
 
+import log from 'core/logger';
 import translate from 'core/i18n/translate';
 import { getDirection } from 'core/i18n/utils';
 import Card from 'ui/components/Card';
@@ -16,31 +18,80 @@ type PropTypes = {|
   sections: Array<React.Element<typeof CarouselSection>>,
 |};
 
-export const CarouselBase = ({ i18n, sections }: PropTypes) => {
-  if (!sections) {
-    throw new Error('sections are required for a Carousel component');
+type StateTypes = {|
+  hasRendered: boolean,
+|};
+
+export class CarouselBase extends React.Component {
+  constructor(props: PropTypes) {
+    super(props);
+
+    this.state = { hasRendered: false };
   }
 
-  return (
-    <Card className="Carousel">
-      <NukaCarousel
-        autoplay
-        autoplayInterval={4000}
-        cellAlign={getDirection(i18n.lang) === 'ltr' ? 'left' : 'right'}
-        cellSpacing={10}
-        framePadding="0 10%"
-        frameOverflow="visible"
-        slidesToShow={1}
-        slidesToScroll={1}
-        slideWidth={1}
+  state: StateTypes;
+
+  // HACK: NukaCarousel uses inline styles to create the carousel effect, which
+  // work fine on the client but cause CSP issues during initial server render.
+  // To get around this, we re-render the component on the client side.
+  // We use componentDidMount to cause it to update on the client because this
+  // lifecycle method is only called on the client.
+  // See: https://github.com/mozilla/addons-frontend/issues/3349
+  componentDidMount() {
+    if (!this.state.hasRendered) {
+      log.debug('Re-rendering Carousel to avoid CSP issues.');
+      // eslint-disable-next-line react/no-did-mount-set-state
+      this.setState({ hasRendered: true });
+    }
+  }
+
+  props: PropTypes;
+
+  render() {
+    const { i18n, sections } = this.props;
+
+    if (!sections) {
+      throw new Error('sections are required for a Carousel component');
+    }
+
+    return (
+      <Card
+        className={classNames('Carousel', {
+          'Carousel--first-render-wrapper': !this.state.hasRendered,
+        })}
       >
-        {(sections.map((section) => {
-          return section;
-        }))}
-      </NukaCarousel>
-    </Card>
-  );
-};
+        {this.state.hasRendered ? (
+          <NukaCarousel
+            autoplay
+            autoplayInterval={4000}
+            cellAlign={getDirection(i18n.lang) === 'ltr' ? 'left' : 'right'}
+            cellSpacing={10}
+            framePadding="0 10%"
+            frameOverflow="visible"
+            slidesToShow={1}
+            slidesToScroll={1}
+            slideWidth={1}
+          >
+            {(sections.map((section) => {
+              return section;
+            }))}
+          </NukaCarousel>
+        ) : (
+          <div className="Carousel--first-render">
+            {/*
+              We render the sections here to keep the server-side content
+              similar to the client-side content so the re-render isn't as
+              jarring–and so non-JS clients can still see the carousel.
+            */}
+            {(sections.map((section) => {
+              return section;
+            }))}
+          </div>
+        )}
+      </Card>
+    );
+  }
+}
 
 export default compose(
   translate(),
--- tests/unit/ui/components/TestCarousel.js
+++ tests/unit/ui/components/TestCarousel.js
@@ -1,37 +1,29 @@
-import NukaCarousel from 'nuka-carousel';
+import { mount } from 'enzyme';
 import React from 'react';
+import NukaCarousel from 'nuka-carousel';
 
 import { fakeI18n, shallowUntilTarget } from 'tests/unit/helpers';
 import Carousel, { CarouselBase } from 'ui/components/Carousel';
 
 
 describe(__filename, () => {
-  function shallowRender({
-    i18n = fakeI18n(),
-    ...props
-  } = {}) {
+  const defaultProps = {
+    i18n: fakeI18n(),
+  };
+
+  function shallowRender({ ...props } = {}) {
     return shallowUntilTarget(
-      <Carousel i18n={i18n} {...props} />,
+      <Carousel {...defaultProps} {...props} />,
       CarouselBase
     );
   }
 
-  it('renders a Carousel', () => {
-    const root = shallowRender({ sections: [] });
-
-    expect(root.find('.Carousel')).toHaveLength(1);
-    expect(root.find(NukaCarousel)).toHaveProp('cellAlign', 'left');
-    expect(root.find('div.Carousel-section-wrapper')).toHaveLength(0);
-  });
-
-  it('renders a Carousel with cellAlign=right for RTL langs', () => {
-    const root = shallowRender({
-      i18n: fakeI18n({ lang: 'ar' }),
-      sections: [],
-    });
-
-    expect(root.find(NukaCarousel)).toHaveProp('cellAlign', 'right');
-  });
+  // We mount certain tests to ensure lifecycle methods like componentDidMount
+  // are called.
+  // See: https://github.com/mozilla/addons-frontend/issues/3349
+  function mountRender({ ...props } = {}) {
+    return mount(<Carousel {...defaultProps} {...props} />);
+  }
 
   it('throws an error if sections are not supplied', () => {
     expect(() => {
@@ -39,7 +31,57 @@ describe(__filename, () => {
     }).toThrow('sections are required for a Carousel component');
   });
 
-  it('renders sections', () => {
+  it('renders a Carousel on the client', () => {
+    const root = mountRender({ sections: [] });
+
+    expect(root.find('.Carousel')).toHaveLength(1);
+    expect(root.find(NukaCarousel)).toHaveProp('cellAlign', 'left');
+    expect(root.find('.Carousel--first-render-wrapper')).toHaveLength(0);
+    expect(root.find('.Carousel--first-render')).toHaveLength(0);
+    expect(root.find('.Carousel-section-wrapper')).toHaveLength(0);
+  });
+
+  it('renders a Carousel with cellAlign=right for RTL langs', () => {
+    const root = mountRender({
+      i18n: fakeI18n({ lang: 'ar' }),
+      sections: [],
+    });
+
+    expect(root.find(NukaCarousel)).toHaveProp('cellAlign', 'right');
+  });
+
+  it('renders sections inside NukaCarousel on re-render', () => {
+    const root = mountRender({
+      sections: [
+        <p className="something" key="1">Howdy!</p>,
+        <p className="something-else" key="2">Bonjour !</p>,
+      ],
+    });
+
+    expect(root.find(NukaCarousel).find('p')).toHaveLength(2);
+    expect(root.find(NukaCarousel).find('.something')).toHaveLength(1);
+    expect(root.find(NukaCarousel).find('.something-else')).toHaveLength(1);
+  });
+
+  it('does not render a NukaCarousel component on the server', () => {
+    const root = shallowRender({ sections: [] });
+
+    expect(root.find('.Carousel')).toHaveLength(1);
+    expect(root.find(NukaCarousel)).toHaveLength(0);
+    expect(root.find('.Carousel--first-render-wrapper')).toHaveLength(1);
+    expect(root.find('.Carousel--first-render')).toHaveLength(1);
+  });
+
+  it('updates state on componentDidMount and renders a NukaCarousel', () => {
+    const root = mountRender({ sections: [] });
+
+    expect(root.find('.Carousel')).toHaveLength(1);
+    expect(root.find(NukaCarousel)).toHaveLength(1);
+    expect(root.find('.Carousel--first-render-wrapper')).toHaveLength(0);
+    expect(root.find('.Carousel--first-render')).toHaveLength(0);
+  });
+
+  it('renders sections inside server render on the server', () => {
     const root = shallowRender({
       sections: [
         <p className="something" key="1">Howdy!</p>,
@@ -47,6 +89,8 @@ describe(__filename, () => {
       ],
     });
 
+    expect(root.find('.Carousel--first-render-wrapper')).toHaveLength(1);
+    expect(root.find('.Carousel--first-render')).toHaveLength(1);
     expect(root.find('p')).toHaveLength(2);
     expect(root.find('.something')).toHaveLength(1);
     expect(root.find('.something-else')).toHaveLength(1);
