c49d21a4db48a7f1c49d018770b69e71cd259b84
Trishul <trishul.goel@gmail.com>
2017-10-06 14:11:38 +0530

    nit picks

--- src/amo/reducers/home.js
+++ src/amo/reducers/home.js
@@ -6,7 +6,7 @@ export const FETCH_HOME_ADDONS: 'FETCH_HOME_ADDONS' = 'FETCH_HOME_ADDONS';
 export const LOAD_HOME_ADDONS: 'LOAD_HOME_ADDONS' = 'LOAD_HOME_ADDONS';
 
 export type HomeState = {
-  popularExtensions: ?Array<AddonType>,
+  popularExtensions: Array<AddonType>| null,
 };
 
 export const initialState: HomeState = {
