8b2b7c1f88bab7691ca9cca658520c33954f6192
Benjamin Forehand Jr <bennyjr169@gmail.com>
2017-11-16 11:47:06 -0800

    Changed logic and added server host to docker build.

--- bin/start-func-test-server.js
+++ bin/start-func-test-server.js
@@ -123,6 +123,8 @@ new Promise((resolve) => {
       `NODE_ENV=${nodeEnv}`,
       '-e',
       `USE_HTTPS=true`,
+      `-e`,
+      `SERVER_HOST=example.com`,
       `--cidfile=${containerIdFile}`,
       // This will make sure we can read the logs.
       '--log-driver=json-file',
