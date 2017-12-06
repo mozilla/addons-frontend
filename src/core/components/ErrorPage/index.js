8cc54adae0332f1faac2e80f66032702eb42d490
Kumar McMillan <kumar.mcmillan@gmail.com>
2017-12-06 17:38:43 -0600

    Show a 500 for component errors (#4051)

--- src/core/components/ErrorPage/index.js
+++ src/core/components/ErrorPage/index.js
@@ -3,12 +3,15 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 
+import log from 'core/logger';
 import { getErrorComponent as getErrorComponentDefault } from 'core/utils';
+import { loadErrorPage } from 'core/reducers/errorPage';
 
 
 export class ErrorPageBase extends React.Component {
   static propTypes = {
     children: PropTypes.node.isRequired,
+    dispatch: PropTypes.func.isRequired,
     errorPage: PropTypes.object.isRequired,
     getErrorComponent: PropTypes.func.isRequired,
   }
@@ -16,6 +19,12 @@ export class ErrorPageBase extends React.Component {
   static defaultProps = {
     errorPage: {},
     getErrorComponent: getErrorComponentDefault,
+  }
+
+  componentDidCatch(error, info) {
+    const { dispatch } = this.props;
+    dispatch(loadErrorPage({ error }));
+    log.error('Caught application error:', error, info);
   }
 
   render() {
