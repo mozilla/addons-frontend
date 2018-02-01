b85953a393c02697f94de3ce8ba8acfd05c8f198
Benjamin Forehand Jr <bennyjr169@gmail.com>
2018-02-01 11:21:05 -0800

    Add config for functional tests.
    * A config named functional-development-amo for setting up the
    server for running the uitests at the addons-server repo.

---
+++ config/functional-development-amo.js
@@ -0,0 +1,17 @@
+module.exports = {
+  // Statics will be served by node.
+  staticHost: '',
+  // FIXME: sign-in isn't working.
+  // fxaConfig: 'local',
+
+  enableClientConsole: true,
+  apiStageHost: "http://olympia.test",
+
+  CSP: false,
+
+  // This is needed to serve assets locally.
+  enableNodeStatics: true,
+  trackingEnabled: false,
+  // Do not send client side errors to Sentry.
+  publicSentryDsn: null,
+};
