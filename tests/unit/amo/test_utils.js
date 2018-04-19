fed88c6218145b8ee6b322600076f217e696e89f
Stuart Colville <scolville@mozilla.com>
2018-04-19 20:54:40 +0100

    Add some tests before updating the base62 lib

--- tests/unit/amo/test_utils.js
+++ tests/unit/amo/test_utils.js
@@ -1,7 +1,7 @@
 import NotAuthorized from 'amo/components/ErrorPage/NotAuthorized';
 import NotFound from 'amo/components/ErrorPage/NotFound';
 import ServerError from 'amo/components/ErrorPage/ServerError';
-import { getErrorComponent } from 'amo/utils';
+import { getDjangoBase62, getErrorComponent } from 'amo/utils';
 
 
 describe('amo/utils', () => {
@@ -20,6 +20,18 @@ describe('amo/utils', () => {
 
     it('returns a ServerError component by default', () => {
       expect(getErrorComponent(501)).toEqual(ServerError);
+    });
+  });
+
+  describe('amo/utils/getDjangoBase62', () => {
+    const base62 = getDjangoBase62();
+
+    it('encodes a example number', () => {
+      expect(base62.encode(535493287492)).toEqual('9QVvGFw');
+    });
+
+    it('decodes a example encoded number', () => {
+      expect(base62.decode('9QVvGFw')).toEqual(535493287492);
     });
   });
 });
