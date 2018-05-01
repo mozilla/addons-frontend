b0d9e71901c2251439a512dc7ccd693e6242d104
Stuart Colville <scolville@mozilla.com>
2018-05-01 10:46:00 +0200

    Greenkeeper/base62 2.0.0 (#4868)
    * Add some tests before updating the base62 lib
    * Update the base62 lib from 1.2.8 -> 2.0.0
    * Update usage of base62 lib to take advantage of new layout
    * Allow for base62.js returning NaN
    * Update yarn.lock
    * Add tests to monitor the handling of unexpected input for base62 encode/decode
    * Fix lint
    * Upgrade to explicit version of uglify webpack to gain es parsing powers
    * Add constants and add test to show returning 0 for base62.decode
    * Fix caret prefix for dep

--- tests/unit/amo/test_utils.js
+++ tests/unit/amo/test_utils.js
@@ -1,7 +1,7 @@
 import NotAuthorized from 'amo/components/ErrorPage/NotAuthorized';
 import NotFound from 'amo/components/ErrorPage/NotFound';
 import ServerError from 'amo/components/ErrorPage/ServerError';
-import { getErrorComponent } from 'amo/utils';
+import { getDjangoBase62, getErrorComponent } from 'amo/utils';
 
 
 describe('amo/utils', () => {
@@ -20,6 +20,32 @@ describe('amo/utils', () => {
 
     it('returns a ServerError component by default', () => {
       expect(getErrorComponent(501)).toEqual(ServerError);
+    });
+  });
+
+  describe('getDjangoBase62', () => {
+    const base62 = getDjangoBase62();
+    const exampleBase62 = '9QVvGFw';
+    const exampleTimeStamp = 535493287492;
+
+    it('encodes a example number', () => {
+      expect(base62.encode(exampleTimeStamp)).toEqual(exampleBase62);
+    });
+
+    it('decodes a example encoded number', () => {
+      expect(base62.decode(exampleBase62)).toEqual(exampleTimeStamp);
+    });
+
+    it('returns NaN when decoding bogus input', () => {
+      expect(Number.isNaN(base62.decode('dfhhsk%%$#^@#$@'))).toBe(true);
+    });
+
+    it('returns 0 when decoding empty string', () => {
+      expect(base62.decode('')).toBe(0);
+    });
+
+    it('returns "" when encoding bogus input', () => {
+      expect(base62.encode('a-string')).toEqual('');
     });
   });
 });
