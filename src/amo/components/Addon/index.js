mozilla/addons-frontend

commit 46400f62e5b744d6d47ac2cd8955d22191eebd83

Fix problem with Addon component when an add-on has zero authors (#4909)
This fixes the issue and also adds a test for it.

src/amo/components/Addon/index.js
@@ -485,15 +485,17 @@ export class AddonBase extends React.Component {
 
         <div className="Addon-details">
           <div className="Addon-main-content">
-            {addon && addonType === ADDON_TYPE_THEME && (
-              <AddonsByAuthorsCard
-                addonType={addonType}
-                authorDisplayName={addon.authors[0].name}
-                authorUsernames={addon.authors.map((author) => author.username)}
-                className="Addon-MoreAddonsCard"
-                forAddonSlug={addon.slug}
-                numberOfAddons={6}
-              />
+            {addon &&
+             addon.authors.length &&
+             addonType === ADDON_TYPE_THEME && (
+             <AddonsByAuthorsCard
+               addonType={addonType}
+               authorDisplayName={addon.authors[0].name}
+               authorUsernames={addon.authors.map((author) => author.username)}
+               className="Addon-MoreAddonsCard"
+               forAddonSlug={addon.slug}
+               numberOfAddons={6}
+             />
             )}
 
             {addonPreviews.length > 0 ? (
@@ -520,7 +522,7 @@ export class AddonBase extends React.Component {
 
           {this.renderVersionReleaseNotes()}
 
-          {addon && addonType !== ADDON_TYPE_THEME && (
+          {addon && addon.authors.length && addonType !== ADDON_TYPE_THEME && (
             <AddonsByAuthorsCard
               addonType={addonType}
               authorDisplayName={addon.authors[0].name}

tests/unit/amo/components/TestAddon.js
@@ -1129,6 +1129,17 @@ describe(__filename, () => {
         .toHaveLength(0);
     });
 
+    it('is hidden when add-on has no authors', () => {
+      const root = shallowRender({
+        addon: createInternalAddon({
+          ...fakeAddon,
+          authors: [],
+        }),
+      });
+      expect(root.find('.AddonDescription-more-addons'))
+        .toHaveLength(0);
+    });
+
     it('displays more add-ons by authors', () => {
       const addon = createInternalAddon({ ...fakeAddon });
       const addonsByAuthors = [

commit f2f9e680cc456afa9e427445fdf38f375639a73f

Fix the regression introduced by #4909

src/amo/components/Addon/index.js
@@ -338,6 +338,29 @@ export class AddonBase extends React.Component {
     /* eslint-enable react/no-danger */
   }
 
+  renderAddonsByAuthorsCard(isForTheme) {
+    const { addon } = this.props;
+    if (
+      !addon ||
+      !addon.authors.length ||
+      (isForTheme && addon.type !== ADDON_TYPE_THEME) ||
+      (!isForTheme && addon.type === ADDON_TYPE_THEME)
+    ) {
+      return null;
+    }
+
+    return (
+      <AddonsByAuthorsCard
+        addonType={addon.type}
+        authorDisplayName={addon.authors[0].name}
+        authorUsernames={addon.authors.map((author) => author.username)}
+        className="Addon-MoreAddonsCard"
+        forAddonSlug={addon.slug}
+        numberOfAddons={6}
+      />
+    );
+  }
+
   render() {
     const {
       addon,
@@ -485,18 +508,7 @@ export class AddonBase extends React.Component {
 
         <div className="Addon-details">
           <div className="Addon-main-content">
-            {addon &&
-             addon.authors.length &&
-             addonType === ADDON_TYPE_THEME && (
-             <AddonsByAuthorsCard
-               addonType={addonType}
-               authorDisplayName={addon.authors[0].name}
-               authorUsernames={addon.authors.map((author) => author.username)}
-               className="Addon-MoreAddonsCard"
-               forAddonSlug={addon.slug}
-               numberOfAddons={6}
-             />
-            )}
+            {this.renderAddonsByAuthorsCard(true)}
 
             {addonPreviews.length > 0 ? (
               <Card
@@ -522,16 +534,7 @@ export class AddonBase extends React.Component {
 
           {this.renderVersionReleaseNotes()}
 
-          {addon && addon.authors.length && addonType !== ADDON_TYPE_THEME && (
-            <AddonsByAuthorsCard
-              addonType={addonType}
-              authorDisplayName={addon.authors[0].name}
-              authorUsernames={addon.authors.map((author) => author.username)}
-              className="Addon-MoreAddonsCard"
-              forAddonSlug={addon.slug}
-              numberOfAddons={6}
-            />
-          )}
+          {this.renderAddonsByAuthorsCard(false)}
         </div>
       </div>
     );