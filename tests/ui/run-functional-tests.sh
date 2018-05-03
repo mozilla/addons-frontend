3060cc20a516dd2addbf3199ff6acc9bdbf0d297
Benjamin Forehand Jr <bennyjr169@gmail.com>
2018-05-03 13:54:40 -0400

    Added hostname script to set correct hostname for integration tests.

--- tests/ui/run-functional-tests.sh
+++ tests/ui/run-functional-tests.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env sh
 git clone --depth 1 https://github.com/mozilla/addons-server.git
 docker-compose -f addons-server/docker-compose.yml -f tests/ui/docker-compose.functional-tests.yml up -d --build
-until docker-compose -f addons-server/docker-compose.yml -f tests/ui/docker-compose.functional-tests.yml images | grep "addons-server_addons-frontend_1" ;
+until docker-compose -f addons-server/docker-compose.yml -f tests/ui/docker-compose.functional-tests.yml images | grep "addonsserver_addons-frontend_1" ;
     do printf "."; sleep 1
 done
 echo
 docker-compose -f addons-server/docker-compose.yml -f tests/ui/docker-compose.functional-tests.yml ps
-./addons-server/scripts/ui-test-hostname-setup.sh
+# This script sets the ip of the addons-frontend image as localhost within the selenium-firefox image.
+# This MUST be run before any user integration tests.
+cd addons-server
+UI_IP="`docker inspect addonsserver_addons-frontend_1 | grep "IPAddress" | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"`"
+echo $UI_IP
+HOSTS_LINE="$UI_IP\tlocalhost"
+cd ..
+docker-compose -f addons-server/docker-compose.yml -f tests/ui/docker-compose.functional-tests.yml exec --user root selenium-firefox sudo -- sh -c -e "echo '$HOSTS_LINE' >> /etc/hosts"
+docker-compose -f addons-server/docker-compose.yml -f tests/ui/docker-compose.functional-tests.yml exec --user root selenium-firefox cat /etc/hosts
 # Make sure dependencies get updated in worker and web container
 docker-compose -f addons-server/docker-compose.yml -f tests/ui/docker-compose.functional-tests.yml exec worker make -f Makefile-docker update_deps
 docker-compose -f addons-server/docker-compose.yml -f tests/ui/docker-compose.functional-tests.yml restart worker
