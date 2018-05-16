6237080f6bbac020e0f5e4dd40b71580d5c56867
William Durand <will+git@drnd.me>
2018-05-16 16:12:00 +0200

    Add tests for UserAvatar

--- tests/unit/ui/components/TestUserAvatar.js
+++ tests/unit/ui/components/TestUserAvatar.js
@@ -62,4 +62,12 @@ describe(__filename, () => {
     expect(root.find('.UserAvatar').find(Icon))
       .toHaveProp('name', 'anonymous-user');
   });
+
+  it('renders a preview image when supplied', () => {
+    const preview = 'https://example.org/image.jpg';
+    const root = renderUserAvatar({ preview });
+
+    expect(root.find('.UserAvatar-image')).toHaveLength(1);
+    expect(root.find('.UserAvatar-image')).toHaveProp('src', preview);
+  });
 });
