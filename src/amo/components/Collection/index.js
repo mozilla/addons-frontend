af9824271050a68978c5d17efa871eca88c5016a
Snehil Verma <vsnehil92@gmail.com>
2018-06-08 10:17:53 +0530

    only owner can edit the code revoved for admin

--- src/amo/components/Collection/index.js
+++ src/amo/components/Collection/index.js
@@ -385,10 +385,6 @@ export const mapStateToProps = (
   let hasEditPermission = false;
 
   const collection = getCurrentCollection(state.collections);
-  if (collection && currentUser) {
-    hasEditPermission = collection.authorId === currentUser.id ||
-      hasPermission(state, COLLECTIONS_EDIT);
-  }
 
   return {
     collection,
