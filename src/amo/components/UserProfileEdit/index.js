092b1c94ecded07b205c3f78ac9829bc9c857a66
William Durand <will+git@drnd.me>
2018-06-25 18:49:36 +0200

    Fix tooltip for email field (#5393)

--- src/amo/components/UserProfileEdit/index.js
+++ src/amo/components/UserProfileEdit/index.js
@@ -506,15 +506,16 @@ export class UserProfileEditBase extends React.Component<Props, State> {
                 value={this.state.username}
               />
 
-              <div title={i18n.gettext('Email address cannot be changed here')}>
+              <div>
                 <label className="UserProfileEdit--label" htmlFor="email">
                   {i18n.gettext('Email Address')}
                 </label>
                 <input
                   className="UserProfileEdit-email"
-                  disabled
                   defaultValue={user && user.email}
+                  disabled
                   onChange={this.onFieldChange}
+                  title={i18n.gettext('Email address cannot be changed here')}
                   type="email"
                 />
                 {isEditingCurrentUser && (
