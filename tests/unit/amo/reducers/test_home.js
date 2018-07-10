16f1c03bab3d960e8f011a664f3b6d379e89358b
Rebecca <rebjmullin@gmail.com>
2018-07-10 16:47:57 -0400

    adding test for reducer update

--- tests/unit/amo/reducers/test_home.js
+++ tests/unit/amo/reducers/test_home.js
@@ -1,10 +1,14 @@
-import { LANDING_PAGE_EXTENSION_COUNT } from 'amo/constants';
+import {
+  LANDING_PAGE_EXTENSION_COUNT,
+  LANDING_PAGE_THEME_COUNT,
+} from 'amo/constants';
 import homeReducer, {
   fetchHomeAddons,
   initialState,
   loadHomeAddons,
 } from 'amo/reducers/home';
 import { createInternalAddon } from 'core/reducers/addons';
+import { ADDON_TYPE_THEME } from 'core/constants';
 import {
   createAddonsApiResult,
   createFakeCollectionAddon,
@@ -59,6 +63,42 @@ describe(__filename, () => {
       expect(homeState.featuredThemes).toEqual([
         createInternalAddon(fakeTheme),
       ]);
+    });
+
+    it('loads the the correct amount of theme add-ons in a collection to display on homepage', () => {
+      const { store } = dispatchClientMetadata();
+
+      store.dispatch(
+        loadHomeAddons({
+          collections: [
+            createFakeCollectionAddonsListResponse({
+              addons: Array(10).fill({
+                ...createFakeCollectionAddon({
+                  addon: {
+                    ...fakeAddon,
+                    type: ADDON_TYPE_THEME,
+                  },
+                }),
+              }),
+            }),
+          ],
+          featuredExtensions: createAddonsApiResult([fakeAddon]),
+        }),
+      );
+
+      const homeState = store.getState().home;
+
+      expect(homeState.resultsLoaded).toEqual(true);
+      expect(homeState.collections).toHaveLength(1);
+
+      expect(homeState.collections[0]).toEqual(
+        Array(LANDING_PAGE_THEME_COUNT).fill(
+          createInternalAddon({
+            ...fakeAddon,
+            type: ADDON_TYPE_THEME,
+          }),
+        ),
+      );
     });
 
     it('loads a null for a missing collection', () => {
