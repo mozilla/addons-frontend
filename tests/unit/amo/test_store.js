fc7eea9b6de968c45a2a48eeedf45e8e82aa3066
William Durand <will+git@drnd.me>
2018-07-18 17:18:54 +0200

    Remove test case about reducers/store (#5648)

--- tests/unit/amo/test_store.js
+++ tests/unit/amo/test_store.js
@@ -2,38 +2,6 @@ import createStore from 'amo/store';
 import { initialState } from 'core/reducers/addons';
 
 describe(__filename, () => {
-  it('sets the reducers', () => {
-    const { store } = createStore();
-    expect(Object.keys(store.getState()).sort()).toEqual([
-      'abuse',
-      'addons',
-      'addonsByAuthors',
-      'api',
-      'autocomplete',
-      'categories',
-      'collections',
-      'errorPage',
-      'errors',
-      'formOverlay',
-      'heroBanners',
-      'home',
-      'infoDialog',
-      'installations',
-      'landing',
-      'languageTools',
-      'recommendations',
-      'redirectTo',
-      'reviews',
-      'routing',
-      'search',
-      'survey',
-      'uiState',
-      'userAbuseReports',
-      'users',
-      'viewContext',
-    ]);
-  });
-
   it('creates an empty store', () => {
     const { store } = createStore();
     expect(store.getState().addons).toEqual(initialState);
