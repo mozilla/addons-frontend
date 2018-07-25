2dd194ed5b380c9bd24d06bc22484b8e4126cd70
Benjamin Forehand Jr <bennyjr169@gmail.com>
2018-07-25 12:52:52 -0400

    fix typo.

--- tests/ui/run-functional-tests.sh
+++ tests/ui/run-functional-tests.sh
@@ -20,6 +20,6 @@ docker-compose -f addons-server/docker-compose.yml -f tests/ui/docker-compose.fu
 docker-compose -f addons-server/docker-compose.yml -f tests/ui/docker-compose.functional-tests.yml restart worker
 docker-compose -f addons-server/docker-compose.yml -f tests/ui/docker-compose.functional-tests.yml exec web make -f Makefile-docker update_deps
 docker-compose -f addons-server/docker-compose.yml -f tests/ui/docker-compose.functional-tests.yml restart web
-# This is done because cirlceci has permissions do not allow the user `circleci` to remove or add files created by the root, even if it is part of the root group.
+# This is done because cirlceci has permissions to not allow the user `circleci` to remove or add files created by the root, even if it is part of the root group.
 sudo chown -R circleci:circleci .
 docker-compose -f addons-server/docker-compose.yml -f tests/ui/docker-compose.functional-tests.yml exec --user cirlceci selenium-firefox tox -e ui-tests
