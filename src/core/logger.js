610c092585d0e7acdaac0db1205d992e48db93a6
William Durand <will+git@drnd.me>
2018-08-07 23:39:05 +0200

    Use pino/browser in test env

--- src/core/logger.js
+++ src/core/logger.js
@@ -1,5 +1,20 @@
 import config from 'config';
-import pino from 'pino';
+
+let pino = null;
+if (process.env.NODE_ENV === 'test') {
+  // We explicitely require the "browser" (client) version of Pino to write
+  // logs with `console` and not `stdout` so that Jest can handle the output.
+  // See: https://github.com/mozilla/addons-frontend/issues/5869
+  //
+  // eslint-disable-next-line global-require
+  pino = require('pino/browser');
+} else {
+  // Pino is an isomorphic logging library and works well on both the server
+  // and the client without configuration.
+  //
+  // eslint-disable-next-line global-require
+  pino = require('pino');
+}
 
 const appName = config.get('appName');
 
