87100bc04546b6631f2bb6df30db9450238305ef
William Durand <will+git@drnd.me>
2018-08-07 15:05:42 +0200

    Rewrite InfoDialog component

--- src/core/components/InfoDialog/index.js
+++ src/core/components/InfoDialog/index.js
@@ -1,41 +1,63 @@
+/* @flow */
+import invariant from 'invariant';
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import onClickOutside from 'react-onclickoutside';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 
 import translate from 'core/i18n/translate';
+import type { AppState } from 'amo/store';
+import type { I18nType } from 'core/types/i18n';
 
 import './styles.scss';
 
-class InfoDialogRaw extends React.Component {
-  static propTypes = {
-    addonName: PropTypes.string.isRequired,
-    closeAction: PropTypes.func.isRequired,
-    imageURL: PropTypes.string.isRequired,
-    i18n: PropTypes.object.isRequired,
-  };
+type Props = {||};
 
+type InternalProps = {|
+  ...Props,
+  addonName: string | null,
+  closeAction: Function | null,
+  i18n: I18nType,
+  imageURL: string | null,
+  show: boolean,
+|};
+
+export class InfoDialogBase extends React.Component<InternalProps> {
   handleClickOutside = () => {
-    this.props.closeAction();
+    const { closeAction } = this.props;
+
+    invariant(closeAction, 'closeAction is required');
+
+    closeAction();
   };
 
   render() {
-    const { addonName, closeAction, i18n, imageURL } = this.props;
+    const { addonName, closeAction, i18n, imageURL, show } = this.props;
+
+    if (!show) {
+      return null;
+    }
+
+    invariant(addonName, 'addonName is required when show=true');
+    invariant(closeAction, 'closeAction is required when show=true');
+    invariant(imageURL, 'imageURL is required when show=true');
+
     return (
       <div
-        className="show-info"
+        className="InfoDialog"
         role="dialog"
-        aria-labelledby="show-info-title"
-        aria-describedby="show-info-description"
+        aria-labelledby="InfoDialog-title"
+        aria-describedby="InfoDialog-description"
       >
-        <div className="info">
-          <div className="logo">
-            <img src={imageURL} alt="" />
+        <div className="InfoDialog-info">
+          <div className="InfoDialog-logo">
+            <img src={imageURL} alt={addonName} />
           </div>
-          <div className="copy">
-            <h3 id="show-info-title">{i18n.gettext('Your add-on is ready')}</h3>
-            <p id="show-info-description">
+          <div className="InfoDialog-copy">
+            <h3 className="InfoDialog-title">
+              {i18n.gettext('Your add-on is ready')}
+            </h3>
+            <p className="InfoDialog-description">
               {i18n.sprintf(
                 i18n.gettext('Now you can access %(name)s from the toolbar.'),
                 { name: addonName },
@@ -43,14 +65,7 @@ class InfoDialogRaw extends React.Component {
             </p>
           </div>
         </div>
-        <button
-          ref={(button) => {
-            if (button != null) {
-              button.focus();
-            }
-          }}
-          onClick={closeAction}
-        >
+        <button className="InfoDialog-button" onClick={closeAction}>
           {i18n.gettext('OK!')}
         </button>
       </div>
@@ -58,24 +73,22 @@ class InfoDialogRaw extends React.Component {
   }
 }
 
-export const InfoDialogBase = compose(
+export const mapStateToProps = (state: AppState) => {
+  const { data, show } = state.infoDialog;
+
+  return {
+    addonName: data ? data.addonName : null,
+    closeAction: data ? data.closeAction : null,
+    imageURL: data ? data.imageURL : null,
+    show,
+  };
+};
+
+const InfoDialog: React.ComponentType<Props> = compose(
+  connect(mapStateToProps),
   translate(),
+  // This HOC must be positioned last.
   onClickOutside,
-)(InfoDialogRaw);
+)(InfoDialogBase);
 
-export const ShowInfoDialog = ({ data, show }) => {
-  return show ? <InfoDialogBase {...data} /> : null;
-};
-
-ShowInfoDialog.propTypes = {
-  data: PropTypes.shape({
-    addonName: PropTypes.string.isRequired,
-    closeAction: PropTypes.func.isRequired,
-    imageURL: PropTypes.string.isRequired,
-  }).isRequired,
-  show: PropTypes.bool.isRequired,
-};
-
-export const mapStateToProps = (state) => state.infoDialog;
-
-export default compose(connect(mapStateToProps))(ShowInfoDialog);
+export default InfoDialog;
