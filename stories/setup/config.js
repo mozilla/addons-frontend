49b873ba554c86eb311eed5e27e681d7b3d2c6ce
Rebecca <rebjmullin@gmail.com>
2018-08-29 20:02:35 -0400

    updating per commnts-part2-updating path config

---
+++ stories/setup/config.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { withConsole } from '@storybook/addon-console';
+import { addDecorator, configure, setAddon } from '@storybook/react';
+import {
+	withInfo,
+	setDefaults as setAddonInfoDefaults,
+} from '@storybook/addon-info';
+import { withKnobs } from '@storybook/addon-knobs';
+import { setOptions } from '@storybook/addon-options';
+
+// Override withInfo options.
+setAddonInfoDefaults({
+	header: false, // Hides header display of name and description.
+	// inline: true, // Shows everything on the same page.
+});
+
+// Override some global-y setup options.
+// See https://www.npmjs.com/package/@storybook/addon-options.
+setOptions({
+	name: 'Mozilla Addons frontend',
+	url: 'https://github.com/mozilla/addons-frontend',
+});
+
+// Global Decorators.
+addDecorator(withKnobs);
+addDecorator((story, context) => withConsole()(story)(context));
+
+function loadStories() {
+	require('./../index');
+}
+
+configure(loadStories, module);
