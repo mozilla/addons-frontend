4a3be8efe6eaee8bcf0fa1b1c38366ae54561707
Rebecca <rebjmullin@gmail.com>
2018-09-07 14:55:04 -0400

    I6719 updating the structure of home hero arry-2

---
+++ src/amo/homeHeroItems.js
@@ -0,0 +1,155 @@
+export const heroes = [
+  {
+    title: 'Facebook Container',
+    description: 'Prevent Facebook from tracking you around the web',
+    url: '/addon/facebook-container/',
+  },
+  {
+    title: 'Midnight Lizard',
+    description: 'Give the entire internet a new look',
+    url: '/addon/midnight-lizard-quantum/',
+  },
+  {
+    title: 'Turbo Download Manager',
+    description: 'Increase download speeds with multi-threading support',
+    url: '/addon/turbo-download-manager/',
+  },
+  {
+    title: 'Authenticator',
+    description: 'Generate 2-step verification codes right in Firefox',
+    url: '/addon/auth-helper/',
+  },
+  {
+    title: 'IP Address and Domain Information',
+    description:
+      'See detailed info about every website you visit—IP address, location, provider & more',
+    url: '/addon/ip-address-and-domain-info/',
+  },
+  {
+    title: 'New Tab Override',
+    description: 'Choose the page you see every time you open a new tab',
+    url: '/addon/new-tab-override/',
+  },
+  {
+    title: 'Forecastfox',
+    description: 'Get instant global weather information right in Firefox',
+    url: '/addon/forecastfox-fix-version/',
+  },
+  {
+    title: 'Multi-Account Containers',
+    description:
+      'Keep different parts of your online life—work, personal, etc.—separated by color-coded tabs',
+    url: '/addon/multi-account-containers/',
+  },
+  {
+    title: 'Transparent Standalone Images',
+    description: 'Render images on transparent backgrounds',
+    url: '/addon/transparent-standalone-image/',
+  },
+  {
+    title: 'Tabliss',
+    description:
+      'Enjoy a gorgeous new tab page with customizable backgrounds, local weather & more',
+    url: '/addon/tabliss/',
+  },
+  {
+    title: 'Tree Style Tab',
+    description: 'Have a ton of open tabs? Organize them in a tidy sidebar',
+    url: '/addon/tree-style-tab/',
+  },
+
+  {
+    title: 'Imagus',
+    description: 'Enlarge images by hovering your mouse over it',
+    url: '/addon/imagus/',
+  },
+  {
+    title: 'Privacy Possum',
+    description: 'Protect yourself against the sneakiest trackers',
+    url: '/addon/privacy-possum/',
+  },
+  {
+    title: 'Default Bookmark Folder',
+    description: 'Change the default destination for your bookmarks',
+    url: '/addon/default-bookmark-folder/',
+  },
+  {
+    title: 'Textmarker',
+    description: 'Highly customizable text highlighter',
+    url: '/addon/textmarkerpro/',
+  },
+  {
+    title: 'Watch2Gether',
+    description: 'Watch videos simultaneously with others',
+    url: '/addon/w2g/',
+  },
+  {
+    title: 'Context Search',
+    description: 'Highlight text on any webpage to easily search the term',
+    url: '/addon/contextual-search/',
+  },
+  {
+    title: 'Stylus',
+    description: 'Give your favorite websites a new look',
+    url: '/addon/styl-us/',
+  },
+  {
+    title: 'Search Encrypt',
+    description: 'Privacy protection for your search data',
+    url: '/addon/search-encrypt/',
+  },
+  {
+    title: 'Auto Tab Discard',
+    description: 'Save memory by disabling inactive tabs',
+    url: '/addon/auto-tab-discard/',
+  },
+  {
+    title: 'Update Scanner',
+    description:
+      'Get notified when your selected websites update with new content',
+    url: '/addon/update-scanner/',
+  },
+  {
+    title: 'Auto Fullscreen',
+    description: 'Automatically start Firefox in full screen mode',
+    url: '/addon/autofullscreen/',
+  },
+  {
+    title: 'Video Speed Controller',
+    description: 'Adjust video playback speeds with shortcuts',
+    url: '/addon/videospeed/',
+  },
+  {
+    title: 'View Image',
+    description:
+      'Revive the ‘View Image’ and ‘Search by Image’ buttons on Google Images',
+    url: '/addon/view-image/',
+  },
+  {
+    title: 'Neat URL',
+    description: 'Clean up links for easy sharing',
+    url: '/addon/neat-url/',
+  },
+  {
+    title: 'Glitter Drag',
+    description:
+      'Drag text, images, or links to perform actions like copy, open, search, and more',
+    url: '/addon/glitterdrag/',
+  },
+  {
+    title: 'Behind The Overlay Revival',
+    description: 'Click a button to close annoying pop-ups',
+    url: '/addon/behind-the-overlay-revival/',
+  },
+  {
+    title: 'Auto-Sort Bookmarks',
+    description:
+      'Automatically sorts bookmarks so they’re in your preferred position',
+    url: '/addon/auto-sort-bookmarks/',
+  },
+  {
+    title: 'Britannica Insights',
+    description: 'Add Britannica facts to your search results',
+    url: '/addon/britannica-insights/',
+  },
+];
