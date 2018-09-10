2ede90e942a6a5dcec0e131c7c9e6783e0fe5f52
William Durand <will+git@drnd.me>
2018-09-10 15:55:14 +0200

    Open dev hub link in another tab

--- src/amo/components/Header/index.js
+++ src/amo/components/Header/index.js
@@ -81,6 +81,7 @@ export class HeaderBase extends React.Component {
             href="/developers/"
             external
             prependClientApp={false}
+            target="_blank"
           >
             {i18n.gettext('Developer Hub')}
           </Link>
