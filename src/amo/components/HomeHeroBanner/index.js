d9dab4e0b8a3a3de21c622bee70de537f6d7ab2d
Rebecca Mullin <rebjmullin@gmail.com>
2018-09-10 15:26:18 -0400

    I6179 update structure home hero array (#6180)
    * I6719 updating the structure of home hero arry
    
    * I6719 updating the structure of home hero arry-2
    
    * updating per comments
    
    * updating per comments -2
    
    * updating per comments-3

--- src/amo/components/HomeHeroBanner/index.js
+++ src/amo/components/HomeHeroBanner/index.js
@@ -17,202 +17,202 @@ export class HomeHeroBannerBase extends React.Component<Props> {
   sections() {
     const { i18n } = this.props;
 
-    return [
-      <HeroSection key="hero-1" linkTo="/addon/facebook-container/">
-        <h3>{i18n.gettext('Facebook Container')}</h3>
-
-        <p>
-          {i18n.gettext('Prevent Facebook from tracking you around the web')}
-        </p>
-      </HeroSection>,
-      <HeroSection key="hero-2" linkTo="/addon/midnight-lizard-quantum/">
-        <h3>{i18n.gettext('Midnight Lizard')}</h3>
-
-        <p>{i18n.gettext('Give the entire internet a new look')}</p>
-      </HeroSection>,
-      <HeroSection key="hero-3" linkTo="/addon/turbo-download-manager/">
-        <h3>{i18n.gettext('Turbo Download Manager')}</h3>
-
-        <p>
-          {i18n.gettext(`Increase download speeds with multi-threading
-              support`)}
-        </p>
-      </HeroSection>,
-      <HeroSection key="hero-4" linkTo="/addon/auth-helper/">
-        <h3>{i18n.gettext('Authenticator')}</h3>
-        <p>
-          {i18n.gettext('Generate 2-step verification codes right in Firefox')}
-        </p>
-      </HeroSection>,
-      <HeroSection key="hero-5" linkTo="/addon/ip-address-and-domain-info/">
-        <h3>{i18n.gettext('IP Address and Domain Information')}</h3>
-        <p>
-          {i18n.gettext(
-            `See detailed info about every website you visit—IP address, location, provider & more`,
-          )}
-        </p>
-      </HeroSection>,
-      <HeroSection key="hero-6" linkTo="/addon/new-tab-override/">
-        <h3>{i18n.gettext('New Tab Override')}</h3>
-
-        <p>
-          {i18n.gettext(
-            'Choose the page you see every time you open a new tab',
-          )}
-        </p>
-      </HeroSection>,
-      <HeroSection key="hero-7" linkTo="/addon/forecastfox-fix-version/">
-        <h3>{i18n.gettext('Forecastfox')}</h3>
-
-        <p>
-          {i18n.gettext(
-            'Get instant global weather information right in Firefox',
-          )}
-        </p>
-      </HeroSection>,
-      <HeroSection key="hero-8" linkTo="/addon/multi-account-containers/">
-        <h3>{i18n.gettext('Multi-Account Containers')}</h3>
-
-        <p>
-          {i18n.gettext(`Keep different parts of your online
-              life—work, personal, etc.—separated by color-coded tabs`)}
-        </p>
-      </HeroSection>,
-      <HeroSection key="hero-9" linkTo="/addon/transparent-standalone-image/">
-        <h3>{i18n.gettext('Transparent Standalone Images')}</h3>
-
-        <p>{i18n.gettext('Render images on transparent backgrounds')}</p>
-      </HeroSection>,
-      <HeroSection key="hero-10" linkTo="/addon/tabliss/">
-        <h3>{i18n.gettext('Tabliss')}</h3>
-
-        <p>
-          {i18n.gettext(`Enjoy a gorgeous new tab page with customizable
-              backgrounds, local weather & more`)}
-        </p>
-      </HeroSection>,
-      <HeroSection key="hero-11" linkTo="/addon/tree-style-tab/">
-        <h3>{i18n.gettext('Tree Style Tab')}</h3>
-
-        <p>
-          {i18n.gettext(
-            'Have a ton of open tabs? Organize them in a tidy sidebar',
-          )}
-        </p>
-      </HeroSection>,
-      <HeroSection key="hero-12" linkTo="/addon/imagus/">
-        <h3>{i18n.gettext('Imagus')}</h3>
-
-        <p>{i18n.gettext('Enlarge images by hovering your mouse over it')}</p>
-      </HeroSection>,
-      <HeroSection key="hero-13" linkTo="/addon/privacy-possum/">
-        <h3>{i18n.gettext('Privacy Possum')}</h3>
-
-        <p>{i18n.gettext('Protect yourself against the sneakiest trackers')}</p>
-      </HeroSection>,
-      <HeroSection key="hero-14" linkTo="/addon/default-bookmark-folder/">
-        <h3>{i18n.gettext('Default Bookmark Folder')}</h3>
-
-        <p>
-          {i18n.gettext('Change the default destination for your bookmarks')}
-        </p>
-      </HeroSection>,
-      <HeroSection key="hero-15" linkTo="/addon/textmarkerpro/">
-        <h3>{i18n.gettext('Textmarker')}</h3>
-
-        <p>{i18n.gettext('Highly customizable text highlighter')}</p>
-      </HeroSection>,
-      <HeroSection key="hero-16" linkTo="/addon/w2g/">
-        <h3>{i18n.gettext('Watch2Gether')}</h3>
-
-        <p>{i18n.gettext('Watch videos simultaneously with others')}</p>
-      </HeroSection>,
-      <HeroSection key="hero-17" linkTo="/addon/contextual-search/">
-        <h3>{i18n.gettext('Context Search')}</h3>
-
-        <p>
-          {i18n.gettext(
-            'Highlight text on any webpage to easily search the term',
-          )}
-        </p>
-      </HeroSection>,
-      <HeroSection key="hero-18" linkTo="/addon/styl-us/">
-        <h3>{i18n.gettext('Stylus')}</h3>
-
-        <p>{i18n.gettext('Give your favorite websites a new look')}</p>
-      </HeroSection>,
-      <HeroSection key="hero-19" linkTo="/addon/search-encrypt/">
-        <h3>{i18n.gettext('Search Encrypt')}</h3>
-
-        <p>{i18n.gettext('Privacy protection for your search data')}</p>
-      </HeroSection>,
-      <HeroSection key="hero-20" linkTo="/addon/auto-tab-discard/">
-        <h3>{i18n.gettext('Auto Tab Discard')}</h3>
-
-        <p>{i18n.gettext('Save memory by disabling inactive tabs')}</p>
-      </HeroSection>,
-      <HeroSection key="hero-21" linkTo="/addon/update-scanner/">
-        <h3>{i18n.gettext('Update Scanner')}</h3>
-
-        <p>
-          {i18n.gettext(
-            'Get notified when your selected websites update with new content',
-          )}
-        </p>
-      </HeroSection>,
-      <HeroSection key="hero-22" linkTo="/addon/autofullscreen/">
-        <h3>{i18n.gettext('Auto Fullscreen')}</h3>
-
-        <p>{i18n.gettext('Automatically start Firefox in full screen mode')}</p>
-      </HeroSection>,
-      <HeroSection key="hero-23" linkTo="/addon/videospeed/">
-        <h3>{i18n.gettext('Video Speed Controller')}</h3>
-
-        <p>{i18n.gettext('Adjust video playback speeds with shortcuts')}</p>
-      </HeroSection>,
-      <HeroSection key="hero-24" linkTo="/addon/view-image/">
-        <h3>{i18n.gettext('View Image')}</h3>
-
-        <p>
-          {i18n.gettext(
-            `Revive the ‘View Image’ and ‘Search by Image’ buttons on Google Images`,
-          )}
-        </p>
-      </HeroSection>,
-      <HeroSection key="hero-25" linkTo="/addon/neat-url/">
-        <h3>{i18n.gettext('Neat URL')}</h3>
-
-        <p>{i18n.gettext('Clean up links for easy sharing')}</p>
-      </HeroSection>,
-      <HeroSection key="hero-26" linkTo="/addon/glitterdrag/">
-        <h3>{i18n.gettext('Glitter Drag')}</h3>
-
-        <p>
-          {i18n.gettext(
-            'Drag text, images, or links to perform actions like copy, open, search, and more',
-          )}
-        </p>
-      </HeroSection>,
-      <HeroSection key="hero-27" linkTo="/addon/behind-the-overlay-revival/">
-        <h3>{i18n.gettext('Behind The Overlay Revival')}</h3>
-
-        <p>{i18n.gettext('Click a button to close annoying pop-ups')}</p>
-      </HeroSection>,
-      <HeroSection key="hero-28" linkTo="/addon/auto-sort-bookmarks/">
-        <h3>{i18n.gettext('Auto-Sort Bookmarks')}</h3>
-
-        <p>
-          {i18n.gettext(
-            'Automatically sorts bookmarks so they’re in your preferred position',
-          )}
-        </p>
-      </HeroSection>,
-      <HeroSection key="hero-29" linkTo="/addon/britannica-insights/">
-        <h3>{i18n.gettext('Britannica Insights')}</h3>
-
-        <p>{i18n.gettext('Add Britannica facts to your search results')}</p>
-      </HeroSection>,
+    const heroes = [
+      {
+        title: i18n.gettext('Facebook Container'),
+        description: i18n.gettext(
+          'Prevent Facebook from tracking you around the web',
+        ),
+        url: '/addon/facebook-container/',
+      },
+      {
+        title: i18n.gettext('Midnight Lizard'),
+        description: i18n.gettext('Give the entire internet a new look'),
+        url: '/addon/midnight-lizard-quantum/',
+      },
+      {
+        title: i18n.gettext('Turbo Download Manager'),
+        description: i18n.gettext(
+          'Increase download speeds with multi-threading support',
+        ),
+        url: '/addon/turbo-download-manager/',
+      },
+      {
+        title: i18n.gettext('Authenticator'),
+        description: i18n.gettext(
+          'Generate 2-step verification codes right in Firefox',
+        ),
+        url: '/addon/auth-helper/',
+      },
+      {
+        title: i18n.gettext('IP Address and Domain Information'),
+        description: i18n.gettext(
+          'See detailed info about every website you visit—IP address, location, provider & more',
+        ),
+        url: '/addon/ip-address-and-domain-info/',
+      },
+      {
+        title: i18n.gettext('New Tab Override'),
+        description: i18n.gettext(
+          'Choose the page you see every time you open a new tab',
+        ),
+        url: '/addon/new-tab-override/',
+      },
+      {
+        title: i18n.gettext('Forecastfox'),
+        description: i18n.gettext(
+          'Get instant global weather information right in Firefox',
+        ),
+        url: '/addon/forecastfox-fix-version/',
+      },
+      {
+        title: i18n.gettext('Multi-Account Containers'),
+        description: i18n.gettext(
+          'Keep different parts of your online life—work, personal, etc.—separated by color-coded tabs',
+        ),
+        url: '/addon/multi-account-containers/',
+      },
+      {
+        title: i18n.gettext('Transparent Standalone Images'),
+        description: i18n.gettext('Render images on transparent backgrounds'),
+        url: '/addon/transparent-standalone-image/',
+      },
+      {
+        title: i18n.gettext('Tabliss'),
+        description: i18n.gettext(
+          'Enjoy a gorgeous new tab page with customizable backgrounds, local weather & more',
+        ),
+        url: '/addon/tabliss/',
+      },
+      {
+        title: i18n.gettext('Tree Style Tab'),
+        description: i18n.gettext(
+          'Have a ton of open tabs? Organize them in a tidy sidebar',
+        ),
+        url: '/addon/tree-style-tab/',
+      },
+      {
+        title: i18n.gettext('Imagus'),
+        description: i18n.gettext(
+          'Enlarge images by hovering your mouse over it',
+        ),
+        url: '/addon/imagus/',
+      },
+      {
+        title: i18n.gettext('Privacy Possum'),
+        description: i18n.gettext(
+          'Protect yourself against the sneakiest trackers',
+        ),
+        url: '/addon/privacy-possum/',
+      },
+      {
+        title: i18n.gettext('Default Bookmark Folder'),
+        description: i18n.gettext(
+          'Change the default destination for your bookmarks',
+        ),
+        url: '/addon/default-bookmark-folder/',
+      },
+      {
+        title: i18n.gettext('Textmarker'),
+        description: i18n.gettext('Highly customizable text highlighter'),
+        url: '/addon/textmarkerpro/',
+      },
+      {
+        title: i18n.gettext('Watch2Gether'),
+        description: i18n.gettext('Watch videos simultaneously with others'),
+        url: '/addon/w2g/',
+      },
+      {
+        title: i18n.gettext('Context Search'),
+        description: i18n.gettext(
+          'Highlight text on any webpage to easily search the term',
+        ),
+        url: '/addon/contextual-search/',
+      },
+      {
+        title: i18n.gettext('Stylus'),
+        description: i18n.gettext('Give your favorite websites a new look'),
+        url: '/addon/styl-us/',
+      },
+      {
+        title: i18n.gettext('Search Encrypt'),
+        description: i18n.gettext('Privacy protection for your search data'),
+        url: '/addon/search-encrypt/',
+      },
+      {
+        title: i18n.gettext('Auto Tab Discard'),
+        description: i18n.gettext('Save memory by disabling inactive tabs'),
+        url: '/addon/auto-tab-discard/',
+      },
+      {
+        title: i18n.gettext('Update Scanner'),
+        description: i18n.gettext(
+          'Get notified when your selected websites update with new content',
+        ),
+        url: '/addon/update-scanner/',
+      },
+      {
+        title: i18n.gettext('Auto Fullscreen'),
+        description: i18n.gettext(
+          'Automatically start Firefox in full screen mode',
+        ),
+        url: '/addon/autofullscreen/',
+      },
+      {
+        title: i18n.gettext('Video Speed Controller'),
+        description: i18n.gettext(
+          'Adjust video playback speeds with shortcuts',
+        ),
+        url: '/addon/videospeed/',
+      },
+      {
+        title: i18n.gettext('View Image'),
+        description: i18n.gettext(
+          'Revive the ‘View Image’ and ‘Search by Image’ buttons on Google Images',
+        ),
+        url: '/addon/view-image/',
+      },
+      {
+        title: i18n.gettext('Neat URL'),
+        description: i18n.gettext('Clean up links for easy sharing'),
+        url: '/addon/neat-url/',
+      },
+      {
+        title: i18n.gettext('Glitter Drag'),
+        description: i18n.gettext(
+          'Drag text, images, or links to perform actions like copy, open, search, and more',
+        ),
+        url: '/addon/glitterdrag/',
+      },
+      {
+        title: i18n.gettext('Behind The Overlay Revival'),
+        description: i18n.gettext('Click a button to close annoying pop-ups'),
+        url: '/addon/behind-the-overlay-revival/',
+      },
+      {
+        title: i18n.gettext('Auto-Sort Bookmarks'),
+        description: i18n.gettext(
+          'Automatically sorts bookmarks so they’re in your preferred position',
+        ),
+        url: '/addon/auto-sort-bookmarks/',
+      },
+      {
+        title: i18n.gettext('Britannica Insights'),
+        description: i18n.gettext(
+          'Add Britannica facts to your search results',
+        ),
+        url: '/addon/britannica-insights/',
+      },
     ];
+
+    return heroes.map((hero) => {
+      return (
+        <HeroSection key={hero.url} linkTo={hero.url}>
+          <h3>{hero.title}</h3>
+          <p>{hero.description}</p>
+        </HeroSection>
+      );
+    });
   }
 
   render() {
