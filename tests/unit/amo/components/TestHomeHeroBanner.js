b9c7f33369b03eb24e2d928e4dfb49ccfaefd6d1
Rebecca <rebjmullin@gmail.com>
2018-09-11 15:24:13 -0400

    updating tests to corrleate w/ updates in src

--- tests/unit/amo/components/TestHomeHeroBanner.js
+++ tests/unit/amo/components/TestHomeHeroBanner.js
@@ -1,7 +1,12 @@
 import * as React from 'react';
 
-import { fakeI18n, shallowUntilTarget } from 'tests/unit/helpers';
+import {
+  createFakeTracking,
+  fakeI18n,
+  shallowUntilTarget,
+} from 'tests/unit/helpers';
 import HomeHeroBanner, {
+  AB_HOME_HERO_TEST_NAME,
   HomeHeroBannerBase,
 } from 'amo/components/HomeHeroBanner';
 import Hero from 'ui/components/Hero';
@@ -32,5 +37,53 @@ describe(__filename, () => {
 
     expect(carousel).toHaveLength(1);
     expect(carousel).toHaveProp('random', true);
+  });
+
+  it('calls tracking on component page view', () => {
+    const fakeTracking = createFakeTracking();
+    const root = shallowRender({ _tracking: fakeTracking });
+
+    const { variant } = root.instance().props;
+
+    sinon.assert.calledWith(fakeTracking.sendEvent, {
+      action: `${AB_HOME_HERO_TEST_NAME} Page View`,
+      category: `AMO ${variant}`,
+      label: '',
+    });
+
+    sinon.assert.calledOnce(fakeTracking.sendEvent);
+  });
+
+  it('calls tracking on click', () => {
+    const fakeTracking = createFakeTracking();
+
+    const root = shallowRender({
+      _tracking: fakeTracking,
+    });
+
+    const { variant } = root.instance().props;
+
+    sinon.assert.calledWith(fakeTracking.sendEvent, {
+      action: `${AB_HOME_HERO_TEST_NAME} Page View`,
+      category: `AMO ${variant}`,
+      label: '',
+    });
+
+    sinon.assert.calledOnce(fakeTracking.sendEvent);
+
+    const heroBanner = root.find(Hero);
+
+    expect(heroBanner).toHaveLength(1);
+    const firstItem = heroBanner.prop('sections')[0];
+
+    firstItem.props.onClick();
+
+    sinon.assert.calledWith(fakeTracking.sendEvent, {
+      action: `${AB_HOME_HERO_TEST_NAME} Click`,
+      category: `AMO ${variant}`,
+      label: `${firstItem.key}`,
+    });
+
+    sinon.assert.calledTwice(fakeTracking.sendEvent);
   });
 });
--- tests/unit/core/test_withExperiment.js
+++ tests/unit/core/test_withExperiment.js
@@ -4,152 +4,140 @@ import { shallow } from 'enzyme';
 import * as React from 'react';
 
 import { withExperiment } from 'core/withExperiment';
-import {
-  createFakeEvent,
-  createFakeTracking,
-  fakeCookie,
-  shallowUntilTarget,
-} from 'tests/unit/helpers';
+import { fakeCookie } from 'tests/unit/helpers';
 
 describe(__filename, () => {
   describe('withExperiment', () => {
-    let fakeTracking;
-
     class SomeComponentBase extends React.Component {
       render() {
         return <div className="component" />;
       }
     }
 
-    const SomeComponent = withExperiment({
-      nameId: 'ABtest',
-      AName: 'AName',
-      BName: 'BName',
-    })(SomeComponentBase);
+    function componentWithExperiment({ experimentProps, props } = {}) {
+      const TestComponent = withExperiment({
+        id: 'ABtest',
+        variantA: 'AName',
+        variantB: 'BName',
+        ...experimentProps,
+      })(SomeComponentBase);
 
-    const render = ({ ...props } = {}) => {
-      const root = shallowUntilTarget(
-        <SomeComponent {...props} />,
-        SomeComponentBase,
-      );
-      return root;
-    };
+      return shallow(<TestComponent {...props} />);
+    }
 
-    const shallowRender = ({ ...props } = {}) => {
-      return shallow(<SomeComponent {...props} />);
-    };
-
-    beforeEach(() => {
-      fakeTracking = createFakeTracking();
+    it('passes the variant prop', () => {
+      const root = componentWithExperiment();
+      expect(root).toHaveProp('variant');
     });
 
-    it('passes the experimentIsOn prop', () => {
-      const componentWithExperiment = shallowRender();
-      expect(componentWithExperiment).toHaveProp('experimentIsOn');
-    });
-
-    it('sets experimentIsOn prop to be true or false', () => {
-      const nameId = 'Hero';
-      const AName = 'Big';
-      const BName = 'Small';
-      const root = render({ nameId, AName, BName });
-
-      const options = [true, false];
-
-      const { experimentIsOn } = root.instance().props;
-
-      expect(options).toEqual(expect.arrayContaining([experimentIsOn]));
-    });
-
-    it('loads and saves cookie to contain one of the variant names', () => {
+    it('loads and saves cookie', () => {
       const _cookie = fakeCookie();
-      const nameId = 'Hero';
-      const AName = 'Big';
-      const BName = 'Small';
-      const root = shallowRender({
-        nameId,
-        AName,
-        BName,
-        _cookie,
+      const id = 'Hero';
+
+      const root = componentWithExperiment({
+        props: {
+          _cookie,
+          id,
+        },
       });
 
-      sinon.assert.calledWith(_cookie.load, `AB_${nameId}_COOKIE`);
+      sinon.assert.calledWith(_cookie.load, `experiment_${id}`);
 
       sinon.assert.calledWith(
         _cookie.save,
-        `AB_${nameId}_COOKIE`,
-        root.instance().abTestCookie,
-        { path: '/' },
+        `experiment_${id}`,
+        root.instance().experimentCookie,
       );
     });
 
-    it('calls tracking on component page view', () => {
-      const nameId = 'someTestName';
-      const AName = 'VersionA';
-      const BName = 'VersionB';
-      const root = render({ _tracking: fakeTracking, AName, BName, nameId });
-      const { experimentIsOn } = root.instance().props;
+    it('saves cookie with updated config', () => {
+      const overrideCookieConfig = { path: '/test' };
+      const _cookie = fakeCookie();
+      const id = 'layoutTest';
 
-      const variantName = experimentIsOn
-        ? `AB_TEST_${nameId}_${AName}`
-        : `AB_TEST_${nameId}_${BName}`;
-
-      sinon.assert.calledWith(fakeTracking.sendEvent, {
-        action: `${nameId} Page View`,
-        category: `AMO ${variantName}`,
-        label: '',
+      const root = componentWithExperiment({
+        props: {
+          _cookie,
+          id,
+        },
+        experimentProps: { cookieConfig: overrideCookieConfig },
       });
 
-      sinon.assert.calledOnce(fakeTracking.sendEvent);
-    });
-
-    it('calls tracking on click', () => {
-      const nameId = 'someTestName';
-      const AName = 'VersionA';
-      const BName = 'VersionB';
-      const addonUrl = '/some-test-url';
-
-      const root = render({
-        _tracking: fakeTracking,
-        AName,
-        BName,
-        nameId,
-      });
-
-      const { experimentIsOn } = root.instance().props;
-
-      const variantName = experimentIsOn
-        ? `AB_TEST_${nameId}_${AName}`
-        : `AB_TEST_${nameId}_${BName}`;
-
-      // This is called via render which we need to do
-      // to get access to click.
-      sinon.assert.calledWith(fakeTracking.sendEvent, {
-        action: `${nameId} Page View`,
-        category: `AMO ${variantName}`,
-        label: '',
-      });
-
-      sinon.assert.calledOnce(fakeTracking.sendEvent);
-
-      // Simulate clicking an "a" tag with trackClick.
-      root.instance().props.trackClick(
-        createFakeEvent({
-          ...createFakeEvent(),
-          currentTarget: {
-            nodeName: 'A',
-          },
-        }),
-        addonUrl,
+      sinon.assert.calledWith(
+        _cookie.save,
+        `experiment_${id}`,
+        root.instance().experimentCookie,
+        overrideCookieConfig,
       );
-
-      sinon.assert.calledWith(fakeTracking.sendEvent, {
-        action: `${nameId} Click`,
-        category: `AMO ${variantName}`,
-        label: addonUrl,
-      });
-
-      sinon.assert.calledTwice(fakeTracking.sendEvent);
     });
+
+    // it('calls tracking on component page view', () => {
+    //   const nameId = 'someTestName';
+    //   const AName = 'VersionA';
+    //   const BName = 'VersionB';
+    //   const root = render({ _tracking: fakeTracking, AName, BName, nameId });
+    //   const { experimentIsOn } = root.instance().props;
+
+    //   const variantName = experimentIsOn
+    //     ? `AB_TEST_${nameId}_${AName}`
+    //     : `AB_TEST_${nameId}_${BName}`;
+
+    //   sinon.assert.calledWith(fakeTracking.sendEvent, {
+    //     action: `${nameId} Page View`,
+    //     category: `AMO ${variantName}`,
+    //     label: '',
+    //   });
+
+    //   sinon.assert.calledOnce(fakeTracking.sendEvent);
+    // });
+
+    // it('calls tracking on click', () => {
+    //   const nameId = 'someTestName';
+    //   const AName = 'VersionA';
+    //   const BName = 'VersionB';
+    //   const addonUrl = '/some-test-url';
+
+    //   const root = render({
+    //     _tracking: fakeTracking,
+    //     AName,
+    //     BName,
+    //     nameId,
+    //   });
+
+    //   const { experimentIsOn } = root.instance().props;
+
+    //   const variantName = experimentIsOn
+    //     ? `AB_TEST_${nameId}_${AName}`
+    //     : `AB_TEST_${nameId}_${BName}`;
+
+    //   // This is called via render which we need to do
+    //   // to get access to click.
+    //   sinon.assert.calledWith(fakeTracking.sendEvent, {
+    //     action: `${nameId} Page View`,
+    //     category: `AMO ${variantName}`,
+    //     label: '',
+    //   });
+
+    //   sinon.assert.calledOnce(fakeTracking.sendEvent);
+
+    //   // Simulate clicking an "a" tag with trackClick.
+    //   root.instance().props.trackClick(
+    //     createFakeEvent({
+    //       ...createFakeEvent(),
+    //       currentTarget: {
+    //         nodeName: 'A',
+    //       },
+    //     }),
+    //     addonUrl,
+    //   );
+
+    //   sinon.assert.calledWith(fakeTracking.sendEvent, {
+    //     action: `${nameId} Click`,
+    //     category: `AMO ${variantName}`,
+    //     label: addonUrl,
+    //   });
+
+    //   sinon.assert.calledTwice(fakeTracking.sendEvent);
+    // });
   });
 });
