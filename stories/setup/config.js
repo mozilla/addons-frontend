78bdb2147b5421643eaf43a1e7583d0c2988caeb
Rebecca <rebjmullin@gmail.com>
2018-09-14 14:20:12 -0400

    updating to use storybook chapters

--- stories/setup/config.js
+++ stories/setup/config.js
@@ -1,16 +1,29 @@
-import { configure } from '@storybook/react';
+import { configure, setAddon } from '@storybook/react';
 import { setOptions } from '@storybook/addon-options';
+import chaptersAddon, { setDefaults } from 'react-storybook-addon-chapters';
+
+// TBD: overrides chapters defaults. Do We want this?
+setDefaults({
+	sectionOptions: {
+		showSource: false,
+		allowSourceToggling: false,
+		showPropTables: false,
+		allowPropTablesToggling: false,
+	},
+});
+
+setAddon(chaptersAddon);
 
 // Override some global-y setup options.
 // See https://www.npmjs.com/package/@storybook/addon-options.
 setOptions({
-  name: 'Mozilla Addons frontend',
-  url: 'https://github.com/mozilla/addons-frontend',
+	name: 'Mozilla Addons frontend',
+	url: 'https://github.com/mozilla/addons-frontend',
 });
 
 function loadStories() {
-  /* eslint-disable global-require */
-  require('./../index');
+	/* eslint-disable global-require */
+	require('./../index');
 }
 
 configure(loadStories, module);
--- stories/ui/Badge.js
+++ stories/ui/Badge.js
@@ -1,21 +1,41 @@
 /* @flow */
 import React from 'react';
+import chaptersAddon from 'react-storybook-addon-chapters';
 import { storiesOf } from '@storybook/react';
 
 import Badge from 'ui/components/Badge';
 
 const label = 'Hello Badge';
 
-storiesOf('Badge', module)
-  .add('default (no type)', () => <Badge label={label} />)
-  .add('experimental type', () => <Badge type="experimental" label={label} />)
-  .add('featured type', () => <Badge type="featured" label={label} />)
-  .add('restart-required type', () => (
-    <Badge type="restart-required" label={label} />
-  ))
-  .add('not-compatible type', () => (
-    <Badge type="not-compatible" label={label} />
-  ))
-  .add('requires-payment type', () => (
-    <Badge type="requires-payment" label={label} />
-  ));
+const sections = [
+  {
+    type: 'featured',
+  },
+  {
+    type: 'experimental',
+  },
+  {
+    type: 'restart-required',
+  },
+  {
+    type: 'not-compatible',
+  },
+  {
+    type: 'requires-payment',
+  },
+];
+
+storiesOf('Badge').addWithChapters('Badge variations', {
+  useTheme: true,
+  chapters: [
+    {
+      sections: sections.map((section) => {
+        return {
+          subtitle: `type=${section.type}`,
+          sectionFn: () => <Badge {...section} label={label} />,
+        };
+      }),
+    },
+  ],
+});
+
