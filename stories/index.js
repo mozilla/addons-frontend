c5ca1ec7291a02a34e973c7d3ac11f108df84873
Rebecca <rebjmullin@gmail.com>
2018-09-19 19:36:11 -0400

    updating styles path

--- stories/index.js
+++ stories/index.js
@@ -2,7 +2,7 @@
 import 'amo/components/App/styles.scss';
 import 'core/css/inc/lib.scss';
 
-import './setup/storybook.override.styles.scss';
+import './setup/styles.scss';
 
 import './ui/Badge';
 import './ui/Button';
