316a3611452cdf74ef5c37407245f65b12764534
William Durand <will+git@drnd.me>
2018-09-20 22:02:02 +0200

    Simplify AddonCompatibilityError disco component

--- src/disco/components/AddonCompatibilityError/index.js
+++ src/disco/components/AddonCompatibilityError/index.js
@@ -1,38 +1,33 @@
-/* eslint-disable react/no-danger */
+/* @flow */
 import * as React from 'react';
-import PropTypes from 'prop-types';
-import { compose } from 'redux';
-import { connect } from 'react-redux';
 
 import {
   INCOMPATIBLE_FIREFOX_FOR_IOS,
   INCOMPATIBLE_UNDER_MIN_VERSION,
 } from 'core/constants';
 import translate from 'core/i18n/translate';
-import { sanitizeHTMLWithExternalLinks } from 'disco/utils';
+import type { I18nType } from 'core/types/i18n';
 
 import './style.scss';
 
+type Props = {|
+  reason: string,
+|};
+
+type InternalProps = {|
+  ...Props,
+  i18n: I18nType,
+|};
+
 // Messages in the disco pane are a bit less specific as we don't care about
 // non-Firefox clients and the copy space is limited.
-export class AddonCompatibilityErrorBase extends React.Component {
-  static propTypes = {
-    i18n: PropTypes.object.isRequired,
-    minVersion: PropTypes.string.isRequired,
-    reason: PropTypes.string.isRequired,
-  };
-
+export class AddonCompatibilityErrorBase extends React.Component<
+  InternalProps,
+> {
   render() {
-    const { i18n, minVersion, reason } = this.props;
+    const { i18n, reason } = this.props;
+
     let message;
-
-    if (typeof reason === 'undefined') {
-      throw new Error('AddonCompatibilityError requires a "reason" prop');
-    }
-    if (typeof minVersion === 'undefined') {
-      throw new Error('minVersion is required; it cannot be undefined');
-    }
-
     if (reason === INCOMPATIBLE_FIREFOX_FOR_IOS) {
       message = i18n.gettext(
         'Firefox for iOS does not currently support add-ons.',
@@ -47,20 +42,12 @@ export class AddonCompatibilityErrorBase extends React.Component {
       message = i18n.gettext('This add-on does not support your browser.');
     }
 
-    return (
-      <div
-        className="AddonCompatibilityError"
-        dangerouslySetInnerHTML={sanitizeHTMLWithExternalLinks(message, ['a'])}
-      />
-    );
+    return <div className="AddonCompatibilityError">{message}</div>;
   }
 }
 
-export function mapStateToProps(state) {
-  return { lang: state.api.lang };
-}
+const AddonCompatibilityError: React.ComponentType<Props> = translate()(
+  AddonCompatibilityErrorBase,
+);
 
-export default compose(
-  connect(mapStateToProps),
-  translate(),
-)(AddonCompatibilityErrorBase);
+export default AddonCompatibilityError;
