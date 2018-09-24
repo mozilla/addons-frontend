729e60a95fa6510256ef53c7cc01130e62c2b92d
Rebecca <rebjmullin@gmail.com>
2018-09-24 16:58:44 -0400

    I6387 storybook setup followup

--- stories/setup/webpack.config.babel.js
+++ stories/setup/webpack.config.babel.js
@@ -14,4 +14,29 @@ module.exports = {
   module: {
     rules: [...getRules({ babelOptions, bundleStylesWithJs: true })],
   },
+  // The following plugins are needed to help handle
+  // server-side imported components such as core/logger.
+  // Is this the best way??
+  plugins: [
+    new webpack.DefinePlugin({
+      CLIENT_CONFIG: JSON.stringify(getClientConfig(config)),
+    }),
+
+    new webpack.NormalModuleReplacementPlugin(
+      /config$/,
+      'core/client/config.js',
+    ),
+
+    // This swaps the server side window object with a standard browser window.
+    new webpack.NormalModuleReplacementPlugin(
+      /core\/window/,
+      'core/browserWindow.js',
+    ),
+  ],
+  resolve: {
+    alias: {
+      normalize: 'normalize.css/normalize.css',
+    },
+    modules: [path.resolve('./src'), 'node_modules'],
+  },
 };
