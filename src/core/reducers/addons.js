dabb05c281c79d3e60b3be61b7683268dbc4818d
Kumar McMillan <kumar.mcmillan@gmail.com>
2018-09-25 08:57:16 -0500

    Revert "Unload related add-on for set-review actions (#6335)"
    This reverts commit 40d1ba4e8fe83bfdcf4bc44ba3854a5ba8a4ea61.
    
    This patch is causing https://github.com/mozilla/addons-frontend/issues/6381 so we need to fix the original bug in a different way.

--- src/core/reducers/addons.js
+++ src/core/reducers/addons.js
@@ -1,17 +1,9 @@
 /* @flow */
 import { oneLine } from 'common-tags';
 
-import {
-  SET_LATEST_REVIEW,
-  SET_REVIEW,
-  UNLOAD_ADDON_REVIEWS,
-} from 'amo/actions/reviews';
+import { UNLOAD_ADDON_REVIEWS } from 'amo/actions/reviews';
 import { ADDON_TYPE_THEME } from 'core/constants';
-import type {
-  SetLatestReviewAction,
-  SetReviewAction,
-  UnloadAddonReviewsAction,
-} from 'amo/actions/reviews';
+import type { UnloadAddonReviewsAction } from 'amo/actions/reviews';
 import type { AppState } from 'amo/store';
 import type { AppState as DiscoAppState } from 'disco/store';
 import type { ErrorHandlerType } from 'core/errorHandler';
@@ -323,49 +315,15 @@ export const getAllAddons = (state: AppState): Array<AddonType> => {
   return Object.values(addons);
 };
 
-export function unloadAddonFromState(
-  addonsState: AddonsState,
-  addonId: number,
-) {
-  const addon = addonsState.byID[`${addonId}`];
-  if (addon) {
-    return {
-      ...addonsState,
-      byID: {
-        ...addonsState.byID,
-        [`${addonId}`]: undefined,
-      },
-      byGUID: {
-        ...addonsState.byGUID,
-        [addon.guid]: undefined,
-      },
-      bySlug: {
-        ...addonsState.bySlug,
-        [addon.slug]: undefined,
-      },
-      loadingBySlug: {
-        ...addonsState.loadingBySlug,
-        [addon.slug]: false,
-      },
-    };
-  }
-  return addonsState;
-}
-
 type Action =
   | FetchAddonAction
   | LoadAddonsAction
   | LoadAddonResultsAction
-  | SetLatestReviewAction
-  | SetReviewAction
   | UnloadAddonReviewsAction;
 
 export default function addonsReducer(
   state: AddonsState = initialState,
   action: Action,
-  {
-    _unloadAddonFromState = unloadAddonFromState,
-  }: {| _unloadAddonFromState: typeof unloadAddonFromState |} = {},
 ) {
   switch (action.type) {
     case FETCH_ADDON: {
@@ -413,11 +371,32 @@ export default function addonsReducer(
         loadingBySlug,
       };
     }
-    case SET_REVIEW:
-      return _unloadAddonFromState(state, action.payload.addon.id);
-    case SET_LATEST_REVIEW:
-    case UNLOAD_ADDON_REVIEWS:
-      return _unloadAddonFromState(state, action.payload.addonId);
+    case UNLOAD_ADDON_REVIEWS: {
+      const { addonId } = action.payload;
+      const addon = state.byID[`${addonId}`];
+      if (addon) {
+        return {
+          ...state,
+          byID: {
+            ...state.byID,
+            [`${addonId}`]: undefined,
+          },
+          byGUID: {
+            ...state.byGUID,
+            [addon.guid]: undefined,
+          },
+          bySlug: {
+            ...state.bySlug,
+            [addon.slug]: undefined,
+          },
+          loadingBySlug: {
+            ...state.loadingBySlug,
+            [addon.slug]: false,
+          },
+        };
+      }
+      return state;
+    }
     default:
       return state;
   }
