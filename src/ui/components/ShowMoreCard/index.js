aac1aca9a8abebda4a2c108fd605663044a819ac
Rebecca Mullin <rebjmullin@gmail.com>
2018-09-26 13:14:02 -0400

    I5553 showmorecard to use setuistate (#5874)
    * adding ids
    
    * updating to use setuiState, flow
    
    * updating corresponding tests
    
    * removing commented out code
    
    * fixing flow issues
    
    * adjusting test
    
    * updating click test
    
    * updating tests
    
    * fixing seen issue -part 1
    
    * updating def state
    
    * updating to make it work :/
    
    * updating tests/logic
    
    * cleaning up / renaming some vars to make it clearer
    
    * fixing (maybe:))
    
    * updating logic a bit to acct for diff childrn type, updating flow"
    
    * updating tests to match changes
    
    * updating per comments- round 1
    
    * updating per comments- 2
    
    * updating tests name
    
    * updating test to show more steps
    
    * fixing logic
    
    * fixing flow
    
    * adding didmount ck
    
    * reverting didmount
    
    * fixing flow

--- src/ui/components/ShowMoreCard/index.js
+++ src/ui/components/ShowMoreCard/index.js
@@ -1,13 +1,15 @@
+/* @flow */
 /* eslint-disable react/no-danger */
-
+import invariant from 'invariant';
 import makeClassName from 'classnames';
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import { compose } from 'redux';
 
 import translate from 'core/i18n/translate';
+import withUIState from 'core/withUIState';
 import { sanitizeHTML } from 'core/utils';
 import Card from 'ui/components/Card';
+import type { I18nType } from 'core/types/i18n';
 
 import './styles.scss';
 
@@ -15,41 +17,109 @@ import './styles.scss';
 // beyond which it will add read more link.
 export const MAX_HEIGHT = 150;
 
-export class ShowMoreCardBase extends React.Component {
-  static propTypes = {
-    children: PropTypes.node,
-    className: PropTypes.string,
-    header: PropTypes.node,
-    i18n: PropTypes.object.isRequired,
-  };
+type UIStateType = {|
+  showAllContent: boolean,
+  readMoreExpanded: boolean,
+|};
 
-  constructor(props) {
-    super(props);
-    this.state = { expanded: true };
+type Props = {|
+  children: React.Element<any>,
+  className?: string,
+  header?: React.Element<any> | string,
+  id: string,
+|};
+
+type InternalProps = {|
+  ...Props,
+  i18n: I18nType,
+  setUIState: ($Shape<UIStateType>) => void,
+  uiState: UIStateType,
+|};
+
+const initialUIState: UIStateType = {
+  showAllContent: true,
+  readMoreExpanded: false,
+};
+
+export class ShowMoreCardBase extends React.Component<InternalProps> {
+  contents: HTMLElement | null;
+
+  componentWillReceiveProps(nextProps: InternalProps) {
+    const { children } = this.props;
+    const { children: newChildren } = nextProps;
+
+    let html =
+      children.props &&
+      children.props.dangerouslySetInnerHTML &&
+      children.props.dangerouslySetInnerHTML.__html;
+
+    let newHtml =
+      newChildren.props &&
+      newChildren.props.dangerouslySetInnerHTML &&
+      newChildren.props.dangerouslySetInnerHTML.__html;
+
+    // If it's not html, check for plain text.
+    if (!html && children && !children.props) {
+      html = children;
+    }
+
+    if (!newHtml && newChildren && !newChildren.props) {
+      newHtml = newChildren;
+    }
+
+    // Reset UIState if component html has changed.
+    // This is needed because if you return to an addon that you've
+    // already visited the component doesn't hit unmount again and the store
+    // keeps the last component's UIState which isn't what we want.
+    if (newHtml && html !== newHtml) {
+      this.resetUIState();
+    }
+
+    // If the read more has already been expanded, we can skip
+    // the call to truncate.
+    // Ideally this would only be called one time and it wouldn't be
+    // needed after the initial set up but we need this here (vs componentDidMount)
+    // to get an accurate clientHeight.
+    if (!this.props.uiState.readMoreExpanded) {
+      this.truncateToMaxHeight(this.contents);
+    }
   }
 
-  componentWillReceiveProps() {
-    this.truncateToMaxHeight(this.contents);
+  resetUIState() {
+    this.props.setUIState({
+      ...initialUIState,
+    });
   }
 
-  onClick = (event) => {
-    event.preventDefault();
-    this.setState({ expanded: true });
-  };
-
-  truncateToMaxHeight = (contents) => {
-    // If the contents are short enough they don't need a "show more" link; the
-    // contents are expanded by default.
-    if (contents.clientHeight >= MAX_HEIGHT) {
-      this.setState({ expanded: false });
-    } else {
-      this.setState({ expanded: true });
+  truncateToMaxHeight = (contents: HTMLElement | null) => {
+    const { uiState } = this.props;
+    if (contents) {
+      // If the contents are short enough they don't need a "show more" link; the
+      // contents are expanded by default.
+      if (uiState.showAllContent && contents.clientHeight >= MAX_HEIGHT) {
+        this.props.setUIState({
+          ...uiState,
+          showAllContent: false,
+        });
+      }
     }
   };
 
+  onClick = (event: SyntheticEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+
+    this.props.setUIState({
+      showAllContent: true,
+      readMoreExpanded: true,
+    });
+  };
+
   render() {
-    const { children, className, header, i18n } = this.props;
-    const { expanded } = this.state;
+    const { children, className, header, id, i18n, uiState } = this.props;
+    const { showAllContent } = uiState;
+
+    invariant(children, 'The children property is required');
+    invariant(id, 'The id property is required');
 
     const readMoreLink = (
       <a
@@ -71,10 +141,10 @@ export class ShowMoreCardBase extends React.Component {
     return (
       <Card
         className={makeClassName('ShowMoreCard', className, {
-          'ShowMoreCard--expanded': expanded,
+          'ShowMoreCard--expanded': showAllContent,
         })}
         header={header}
-        footerLink={expanded ? null : readMoreLink}
+        footerLink={showAllContent ? null : readMoreLink}
       >
         <div
           className="ShowMoreCard-contents"
@@ -89,4 +159,16 @@ export class ShowMoreCardBase extends React.Component {
   }
 }
 
-export default compose(translate())(ShowMoreCardBase);
+export const extractId = (props: Props) => {
+  return props.id;
+};
+
+export default compose(
+  translate(),
+  withUIState({
+    fileName: __filename,
+    extractId,
+    initialState: initialUIState,
+    resetOnUnmount: true,
+  }),
+)(ShowMoreCardBase);
