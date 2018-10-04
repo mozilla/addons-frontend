05e3a726111e26f17fab5d9bf913c9e57a7348a5
William Durand <will+git@drnd.me>
2018-10-04 09:57:50 +0200

    Remove logRequests middleware (#6488)

--- src/core/middleware/index.js
+++ src/core/middleware/index.js
@@ -2,5 +2,4 @@ export { prefixMiddleware } from './prefixMiddleware';
 export { trailingSlashesMiddleware } from './trailingSlash';
 export { csp, frameguard, hsts, getNoScriptStyles } from './security';
 export { serveAssetsLocally } from './staticAssets';
-export { logRequests } from './logRequests';
 export { datadogTiming } from './datadogTiming';
