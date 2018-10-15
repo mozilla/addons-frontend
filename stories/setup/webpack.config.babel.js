70066bfead5e60022befebe09270ed2af3d1d7f6
Rebecca Mullin <rebjmullin@gmail.com>
2018-10-15 13:13:54 -0400

    I6172 cleaning up stories webpack config (#6571)
    * I6172 cleaning up config
    
    * addressing comments
    
    * removing ck for circular dep plugin
    
    * updating per comments

--- stories/setup/webpack.config.babel.js
+++ stories/setup/webpack.config.babel.js
@@ -1,58 +1,17 @@
-import 'core/polyfill';
+import { getRules } from '../../webpack-common';
+import webpackConfig, {
+  babelOptions as defaultBabelOptions,
+} from '../../webpack.dev.config.babel';
 
-import path from 'path';
-
-import config from 'config';
-import autoprefixer from 'autoprefixer';
-import webpack from 'webpack';
-
-import { getClientConfig } from 'core/utils';
+// We need to add the docgen plugin to get Flow to work.
+const babelOptions = {
+  ...defaultBabelOptions,
+  plugins: [...defaultBabelOptions.plugins, 'react-docgen'],
+};
 
 module.exports = {
+  ...webpackConfig,
   module: {
-    rules: [
-      {
-        test: /\.svg$/,
-        use: [{ loader: 'svg-url-loader', options: { limit: 10000 } }],
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader', options: { importLoaders: 2 } },
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: [
-                autoprefixer({
-                  browsers: ['last 2 versions'],
-                  grid: false,
-                }),
-              ],
-            },
-          },
-          { loader: 'sass-loader', options: { outputStyle: 'expanded' } },
-        ],
-      },
-    ],
-  },
-  // The following plugins are needed to help handle
-  // server-side imported components such as core/logger.
-  // Is this the best way??
-  plugins: [
-    new webpack.DefinePlugin({
-      CLIENT_CONFIG: JSON.stringify(getClientConfig(config)),
-    }),
-
-    new webpack.NormalModuleReplacementPlugin(
-      /config$/,
-      'core/client/config.js',
-    ),
-  ],
-  resolve: {
-    alias: {
-      normalize: 'normalize.css/normalize.css',
-    },
-    modules: [path.resolve('./src'), 'node_modules'],
+    rules: [...getRules({ babelOptions, bundleStylesWithJs: true })],
   },
 };
