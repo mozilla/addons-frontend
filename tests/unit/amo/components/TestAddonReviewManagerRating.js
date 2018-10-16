cb9ff021001d9afd98c7329c4c936c8e3897f0ba
Kumar McMillan <kumar.mcmillan@gmail.com>
2018-10-16 16:39:28 -0500

    Extract AddonReviewManagerRating

---
+++ tests/unit/amo/components/TestAddonReviewManagerRating.js
@@ -0,0 +1,52 @@
+import * as React from 'react';
+
+import AddonReviewManagerRating, {
+  AddonReviewManagerRatingBase,
+} from 'amo/components/AddonReviewManagerRating';
+import { fakeI18n, shallowUntilTarget } from 'tests/unit/helpers';
+import Rating from 'ui/components/Rating';
+
+describe(__filename, () => {
+  function render(otherProps = {}) {
+    const props = {
+      onSelectRating: sinon.stub(),
+      i18n: fakeI18n(),
+      rating: null,
+      ...otherProps,
+    };
+
+    return shallowUntilTarget(
+      <AddonReviewManagerRating {...props} />,
+      AddonReviewManagerRatingBase,
+    );
+  }
+
+  it('lets you specify className', () => {
+    const className = 'MyClass';
+    const root = render({ className });
+
+    expect(root).toHaveClassName(className);
+    expect(root).toHaveClassName('AddonReviewManagerRating');
+  });
+
+  it('renders children', () => {
+    const children = <span className="some-children" />;
+    const root = render({ children });
+
+    expect(root.find('.some-children')).toHaveLength(1);
+  });
+
+  it('passes rating to Rating', () => {
+    const rating = 4;
+    const root = render({ rating });
+
+    expect(root.find(Rating)).toHaveProp('rating', rating);
+  });
+
+  it('passes onSelectRating to Rating', () => {
+    const onSelectRating = sinon.stub();
+    const root = render({ onSelectRating });
+
+    expect(root.find(Rating)).toHaveProp('onSelectRating', onSelectRating);
+  });
+});
