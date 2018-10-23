b40a238268791b3dc47538380dd73d26211ade72
William Durand <will+git@drnd.me>
2018-10-23 14:35:53 +0200

    Add description meta tag to search tools page

--- src/amo/pages/SearchTools/index.js
+++ src/amo/pages/SearchTools/index.js
@@ -2,26 +2,45 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
+import Helmet from 'react-helmet';
 
 import Search from 'amo/components/Search';
 import { ADDON_TYPE_OPENSEARCH, SEARCH_SORT_RELEVANCE } from 'core/constants';
+import translate from 'core/i18n/translate';
 import { convertFiltersToQueryParams } from 'core/searchUtils';
 import type { SearchFilters } from 'amo/components/AutoSearchInput';
+import type { I18nType } from 'core/types/i18n';
 
 type Props = {|
   filters: SearchFilters,
 |};
 
-export class SearchToolsBase extends React.Component<Props> {
+type InternalProps = {|
+  ...Props,
+  i18n: I18nType,
+|};
+
+export class SearchToolsBase extends React.Component<InternalProps> {
   render() {
-    const { filters } = this.props;
+    const { filters, i18n } = this.props;
 
     return (
-      <Search
-        enableSearchFilters
-        filters={filters}
-        paginationQueryParams={convertFiltersToQueryParams(filters)}
-      />
+      <React.Fragment>
+        <Helmet>
+          <meta
+            name="description"
+            content={i18n.gettext(`Firefox extensions that customize the way
+              you search—everything from privacy-enhanced searching to
+              website-specific searches, image searching, and more.`)}
+          />
+        </Helmet>
+
+        <Search
+          enableSearchFilters
+          filters={filters}
+          paginationQueryParams={convertFiltersToQueryParams(filters)}
+        />
+      </React.Fragment>
     );
   }
 }
@@ -36,6 +55,7 @@ export function mapStateToProps() {
 }
 
 const SearchTools: React.ComponentType<Props> = compose(
+  translate(),
   connect(mapStateToProps),
 )(SearchToolsBase);
 
