c0376947237e6b6612f3de53a905e8755ef2b308
William Durand <will+git@drnd.me>
2018-10-25 10:49:59 +0200

    Convert component to Flow

--- src/amo/components/CategoryHeader/index.js
+++ src/amo/components/CategoryHeader/index.js
@@ -1,45 +1,46 @@
+/* @flow */
 import makeClassName from 'classnames';
 import * as React from 'react';
-import PropTypes from 'prop-types';
 
 import CategoryIcon from 'amo/components/CategoryIcon';
 import { getCategoryColor } from 'core/utils';
 import Card from 'ui/components/Card';
 import LoadingText from 'ui/components/LoadingText';
+import type { CategoryType } from 'amo/types/categories';
 
 import './styles.scss';
 
-export class CategoryHeaderBase extends React.Component {
-  static propTypes = {
-    category: PropTypes.object,
-  };
+type Props = {|
+  category: CategoryType | null,
+|};
 
+export class CategoryHeaderBase extends React.PureComponent<Props> {
   render() {
     const { category } = this.props;
 
     const addonType = category && category.type ? category.type : null;
 
-    let color = 0;
     let icon = null;
+    let className = null;
 
-    if (category) {
-      color = getCategoryColor(category);
+    if (category && addonType) {
       icon = category.slug === 'other' ? `other-${addonType}` : category.slug;
+      className = `CategoryHeader--type-${addonType}`;
     }
 
     return (
       <Card
-        className={makeClassName('CategoryHeader', {
+        className={makeClassName('CategoryHeader', className, {
           'CategoryHeader--loading': !category,
-          [`CategoryHeader--type-${addonType}`]: addonType,
         })}
       >
         <div className="CategoryHeader-wrapper">
           {icon && (
             <div className="CategoryHeader-icon">
-              <CategoryIcon name={icon} color={color} />
+              <CategoryIcon name={icon} color={getCategoryColor(category)} />
             </div>
           )}
+
           <div className="CategoryHeader-contents">
             <h1 className="CategoryHeader-name">
               {category ? category.name : <LoadingText />}
