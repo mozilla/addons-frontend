46060727ec481c6cbfa0c5946aeeef154bf8f073
Rebecca <rebjmullin@gmail.com>
2018-10-25 15:05:05 -0400

    fixing lint

--- tests/unit/amo/components/TestHomeHeroGuides.js
+++ tests/unit/amo/components/TestHomeHeroGuides.js
@@ -3,21 +3,11 @@ import * as React from 'react';
 import HomeHeroGuides, {
   HomeHeroGuidesBase,
 } from 'amo/components/HomeHeroGuides';
-import {
-  dispatchClientMetadata,
-  fakeI18n,
-  shallowUntilTarget,
-} from 'tests/unit/helpers';
+import { fakeI18n, shallowUntilTarget } from 'tests/unit/helpers';
 import Hero from 'ui/components/Hero';
-import Icon from 'ui/components/Icon';
-
-import Link from 'amo/components/Link';
 
 describe(__filename, () => {
-  const render = ({
-    store = dispatchClientMetadata().store,
-    ...props
-  } = {}) => {
+  const render = (props) => {
     return shallowUntilTarget(
       <HomeHeroGuides i18n={fakeI18n()} {...props} />,
       HomeHeroGuidesBase,
