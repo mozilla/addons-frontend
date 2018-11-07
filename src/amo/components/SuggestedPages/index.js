076e5634aa16ea41ce026e9db64ba7be0d5b993f
William Durand <will+git@drnd.me>
2018-11-07 20:11:12 +0100

    Update SuggestedPages component too

--- src/amo/components/SuggestedPages/index.js
+++ src/amo/components/SuggestedPages/index.js
@@ -1,19 +1,41 @@
+/* @flow */
+import config from 'config';
 import * as React from 'react';
-import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { compose } from 'redux';
 
 import Link from 'amo/components/Link';
-import { ADDON_TYPE_EXTENSION, ADDON_TYPE_THEME } from 'core/constants';
+import {
+  ADDON_TYPE_EXTENSION,
+  ADDON_TYPE_THEME,
+  CLIENT_APP_ANDROID,
+} from 'core/constants';
 import translate from 'core/i18n/translate';
 import { visibleAddonType } from 'core/utils';
+import type { AppState } from 'amo/store';
+import type { I18nType } from 'core/types/i18n';
 
-export class SuggestedPagesBase extends React.Component {
-  static propTypes = {
-    i18n: PropTypes.object.isRequired,
+type Props = {||};
+
+type InternalProps = {|
+  ...Props,
+  _config: typeof config,
+  i18n: I18nType,
+  clientApp: string,
+|};
+
+export class SuggestedPagesBase extends React.Component<InternalProps> {
+  static defaultProps = {
+    _config: config,
   };
 
   render() {
-    const { i18n } = this.props;
+    const { _config, clientApp, i18n } = this.props;
+
+    const showThemes =
+      clientApp === CLIENT_APP_ANDROID
+        ? _config.get('enableFeatureStaticThemesForAndroid')
+        : true;
 
     return (
       <section className="SuggestedPages">
@@ -25,11 +47,13 @@ export class SuggestedPagesBase extends React.Component {
               {i18n.gettext('Browse all extensions')}
             </Link>
           </li>
-          <li>
-            <Link to={`/${visibleAddonType(ADDON_TYPE_THEME)}/`}>
-              {i18n.gettext('Browse all themes')}
-            </Link>
-          </li>
+          {showThemes && (
+            <li className="SuggestedPages-link-themes">
+              <Link to={`/${visibleAddonType(ADDON_TYPE_THEME)}/`}>
+                {i18n.gettext('Browse all themes')}
+              </Link>
+            </li>
+          )}
           <li>
             <Link to="/">{i18n.gettext('Add-ons Home Page')}</Link>
           </li>
@@ -39,4 +63,15 @@ export class SuggestedPagesBase extends React.Component {
   }
 }
 
-export default compose(translate())(SuggestedPagesBase);
+const mapStateToProps = (state: AppState) => {
+  return {
+    clientApp: state.api.clientApp,
+  };
+};
+
+const SuggestedPages: React.ComponentType<Props> = compose(
+  connect(mapStateToProps),
+  translate(),
+)(SuggestedPagesBase);
+
+export default SuggestedPages;
