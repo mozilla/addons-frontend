8892b7775574436a41bf40ffa456d475a11d6867
William Durand <will+git@drnd.me>
2018-11-07 14:22:02 +0100

    Also add HeadLinks to the categories page

--- src/amo/pages/CategoriesPage/index.js
+++ src/amo/pages/CategoriesPage/index.js
@@ -1,11 +1,14 @@
 /* @flow */
 import * as React from 'react';
-import { compose } from 'redux';
+import Helmet from 'react-helmet';
 
 import Categories from 'amo/components/Categories';
+import HeadLinks from 'amo/components/HeadLinks';
+import { ADDON_TYPE_EXTENSION, ADDON_TYPE_THEME } from 'core/constants';
 import { apiAddonType } from 'core/utils';
 import translate from 'core/i18n/translate';
 import type { ReactRouterMatchType } from 'core/types/router';
+import type { I18nType } from 'core/types/i18n';
 
 import './styles.scss';
 
@@ -16,16 +19,44 @@ type Props = {|
   |},
 |};
 
-export class CategoriesPageBase extends React.Component<Props> {
-  render() {
-    const { params } = this.props.match;
-    const addonType = apiAddonType(params.visibleAddonType);
+type InternalProps = {|
+  ...Props,
+  i18n: I18nType,
+|};
 
-    return <Categories addonType={addonType} className="CategoriesPage" />;
+export class CategoriesPageBase extends React.Component<InternalProps> {
+  getPageTitle(addonType: string) {
+    const { i18n } = this.props;
+
+    switch (addonType) {
+      case ADDON_TYPE_EXTENSION:
+        return i18n.gettext('All extension categories');
+      case ADDON_TYPE_THEME:
+        return i18n.gettext('All theme categories');
+      default:
+        return null;
+    }
+  }
+
+  render() {
+    const { match } = this.props;
+    const addonType = apiAddonType(match.params.visibleAddonType);
+
+    return (
+      <React.Fragment>
+        <Helmet>
+          <title>{this.getPageTitle(addonType)}</title>
+        </Helmet>
+
+        <HeadLinks />
+
+        <Categories addonType={addonType} className="CategoriesPage" />
+      </React.Fragment>
+    );
   }
 }
 
-const CategoriesPage: React.ComponentType<Props> = compose(translate())(
+const CategoriesPage: React.ComponentType<Props> = translate()(
   CategoriesPageBase,
 );
 
