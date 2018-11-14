524a6cbb7b18678bcfc05a3dd0332ea2d270681a
William Durand <will+git@drnd.me>
2018-11-14 12:35:22 +0100

    Fix import order in some files

--- src/amo/sagas/home.js
+++ src/amo/sagas/home.js
@@ -1,7 +1,7 @@
 /* @flow */
 import { oneLine } from 'common-tags';
-
 import { all, call, put, select, takeLatest } from 'redux-saga/effects';
+
 import { getCollectionAddons } from 'amo/api/collections';
 import {
   LANDING_PAGE_EXTENSION_COUNT,
