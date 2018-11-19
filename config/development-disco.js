9dd284498b403a3e86e0767a2a7f34a1417ad9f9
Stuart Colville <scolville@mozilla.com>
2018-11-19 17:09:51 +0000

    Enable HCT in production

--- config/development-disco.js
+++ config/development-disco.js
@@ -1,7 +1,4 @@
 module.exports = {
   trackingEnabled: false,
   loggingLevel: 'debug',
-  // This requires further manual configuration to collect data.
-  // Please see docs/telemetry.md for more info.
-  hctEnabled: true,
 };
