ce829f19aa77d5855294b454a7406eacfca4d51d
William Durand <will+git@drnd.me>
2018-11-20 17:09:48 +0100

    Configure minimizer

--- webpack.prod.config.babel.js
+++ webpack.prod.config.babel.js
@@ -5,7 +5,8 @@ import config from 'config';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import SriPlugin from 'webpack-subresource-integrity';
 import WebpackIsomorphicToolsPlugin from 'webpack-isomorphic-tools/plugin';
-import UglifyJsPlugin from 'uglifyjs-webpack-plugin';
+import TerserPlugin from 'terser-webpack-plugin';
+import OptimizeCssAssetsPlugin from 'optimize-css-assets-webpack-plugin';
 
 import SriDataPlugin from './src/core/server/sriDataPlugin';
 import { getPlugins, getRules } from './webpack-common';
@@ -39,8 +40,36 @@ export default {
     // have unique names in the error handlers.
     __filename: true,
   },
+  optimization: {
+    minimizer: [
+      // We do not use UglifyJsPlugin because it does not work as intended with
+      // our config, but TerserPlugin is very similar.
+      new TerserPlugin({
+        cache: true,
+        parallel: true,
+        // Even though devtool is set to source-map, this must be true to
+        // output source maps:
+        sourceMap: true,
+        // Do not change these options without busting the cache.
+        // See: https://github.com/mozilla/addons-frontend/issues/5796
+        terserOptions: {
+          output: {
+            comments: false,
+          },
+          compress: {
+            drop_console: true,
+          },
+        },
+      }),
+      new OptimizeCssAssetsPlugin(),
+    ],
+  },
   plugins: [
     ...getPlugins(),
+    // This plugin is not recommended anymore but it still works for us and
+    // WebpackIsomorphicToolsPlugin has a dependency on it... Removing this
+    // plugin means moving to a new isomorphic tool, which seems super
+    // complicated.
     new ExtractTextPlugin({
       filename: '[name]-[hash].css',
       allChunks: true,
