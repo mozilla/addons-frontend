8191c93c53049c7691a078c19b2d034af1a5cbad
William Durand <will+git@drnd.me>
2018-11-20 10:12:11 +0100

    fix test case

--- tests/unit/core/api/test_categories.js
+++ tests/unit/core/api/test_categories.js
@@ -1,35 +1,23 @@
-/* global window */
-import config from 'config';
-
-import * as api from 'core/api/categories';
+import * as api from 'core/api';
+import { getCategories } from 'core/api/categories';
 import { createApiResponse, dispatchClientMetadata } from 'tests/unit/helpers';
 
 describe(__filename, () => {
-  let mockWindow;
-  const apiVersion = config.get('apiVersion');
-
-  beforeEach(() => {
-    mockWindow = sinon.mock(window);
-  });
-
   describe('getCategories', () => {
-    function mockResponse(responseProps = {}) {
-      return createApiResponse({
-        jsonData: {
-          results: [{ slug: 'foo' }, { slug: 'food' }, { slug: 'football' }],
-        },
-        ...responseProps,
-      });
-    }
+    it('calls the categories API', async () => {
+      const { api: apiState } = dispatchClientMetadata().state;
+      const mockApi = sinon.mock(api);
 
-    it('calls the right API endpoint', async () => {
-      mockWindow
-        .expects('fetch')
-        .withArgs(sinon.match(`/api/${apiVersion}/addons/categories/`))
-        .returns(mockResponse());
+      mockApi
+        .expects('callApi')
+        .withArgs({
+          apiState,
+          endpoint: `addons/categories`,
+        })
+        .resolves(createApiResponse());
 
-      await api.getCategories({ api: dispatchClientMetadata().state.api });
-      mockWindow.verify();
+      await getCategories({ api: apiState });
+      mockApi.verify();
     });
   });
 });
