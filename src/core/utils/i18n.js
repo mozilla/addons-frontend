3bdf76fdba81716540b6c249fd749b245ad73877
Rebecca <rebjmullin@gmail.com>
2018-11-20 11:28:04 -0500

    updating logic

--- src/core/utils/i18n.js
+++ src/core/utils/i18n.js
@@ -4,17 +4,21 @@ import type { I18nType } from 'core/types/i18n';
 type GetLocalizedTextWithLinkPartsParams = {|
   i18n: I18nType,
   text: string,
+  linkStart?: string,
+  linkEnd?: string,
 |};
 
 export const getLocalizedTextWithLinkParts = ({
   i18n,
   text,
+  linkStart = 'linkStart',
+  linkEnd = 'linkEnd',
 }: GetLocalizedTextWithLinkPartsParams): Object => {
   const linkEnds = '__LINK__';
 
   const localizedExploreMoreLink = i18n.sprintf(text, {
-    linkStart: linkEnds,
-    linkEnd: linkEnds,
+    [linkStart]: linkEnds,
+    [linkEnd]: linkEnds,
   });
 
   const parts = localizedExploreMoreLink.split(linkEnds);
