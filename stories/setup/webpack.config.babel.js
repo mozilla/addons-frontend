4158c9dcb8211c55792b36331c0619a1f0181b65
William Durand <will+git@drnd.me>
2018-11-21 15:07:07 +0100

    Fix storybook-build

--- stories/setup/webpack.config.babel.js
+++ stories/setup/webpack.config.babel.js
@@ -1,17 +1,25 @@
-import { getRules } from '../../webpack-common';
-import webpackConfig, {
-  babelOptions as defaultBabelOptions,
-} from '../../webpack.dev.config.babel';
+import fs from 'fs';
+import path from 'path';
+
+import { getPlugins, getRules } from '../../webpack-common';
+import webpackProdConfig from '../../webpack.prod.config.babel';
+
+const babelrc = fs.readFileSync(path.join(__dirname, '..', '..', '.babelrc'));
+const babelrcObject = JSON.parse(babelrc);
 
 // We need to add the docgen plugin to get Flow to work.
-const babelOptions = {
-  ...defaultBabelOptions,
-  plugins: [...defaultBabelOptions.plugins, 'react-docgen'],
-};
+const babelOptions = Object.assign({}, babelrcObject, {
+  plugins: [...babelrcObject.plugins, 'react-docgen'],
+});
 
 module.exports = {
-  ...webpackConfig,
+  node: webpackProdConfig.node,
+  optimization: {
+    minimizer: [],
+  },
   module: {
     rules: [...getRules({ babelOptions, bundleStylesWithJs: true })],
   },
+  plugins: [...getPlugins()],
+  resolve: webpackProdConfig.resolve,
 };
