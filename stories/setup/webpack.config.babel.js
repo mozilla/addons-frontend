272990448ba003d7ef8c20907531a205635fad2e
William Durand <will+git@drnd.me>
2018-11-22 09:41:32 +0100

    Add more webpack functions

--- stories/setup/webpack.config.babel.js
+++ stories/setup/webpack.config.babel.js
@@ -1,16 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 
-import { getPlugins, getRules } from '../../webpack-common';
+import { getAssetRules, getStyleRules, getPlugins } from '../../webpack-common';
 import webpackProdConfig from '../../webpack.prod.config.babel';
-
-const babelrc = fs.readFileSync(path.join(__dirname, '..', '..', '.babelrc'));
-const babelrcObject = JSON.parse(babelrc);
-
-// We need to add the docgen plugin to get Flow to work.
-const babelOptions = Object.assign({}, babelrcObject, {
-  plugins: [...babelrcObject.plugins, 'react-docgen'],
-});
 
 module.exports = {
   node: webpackProdConfig.node,
@@ -18,7 +10,7 @@ module.exports = {
     minimizer: [],
   },
   module: {
-    rules: [...getRules({ babelOptions, bundleStylesWithJs: true })],
+    rules: [...getStyleRules({ bundleStylesWithJs: true }), ...getAssetRules()],
   },
   plugins: [...getPlugins()],
   resolve: webpackProdConfig.resolve,
