def9ad244c4e13c900044201ec217b0d22da846f
Sean <S.Prashad@outlook.com>
2018-12-14 17:24:33 -0500

    I2449 - Remove CSP config tests (#7091)

--- tests/unit/core/middleware/test_cspMiddleware.js
+++ tests/unit/core/middleware/test_cspMiddleware.js
@@ -5,15 +5,116 @@ import parse from 'content-security-policy-parser';
 import { csp } from 'core/middleware';
 import { getFakeConfig, getFakeLogger } from 'tests/unit/helpers';
 
-describe(__filename, () => {
-  describe('CSP Middleware', () => {
-    const existingNodeEnv = process.env.NODE_ENV;
+const deployedEnvs = ['dev', 'production', 'stage'];
 
-    afterEach(() => {
-      process.env.NODE_ENV = existingNodeEnv;
-      delete process.env.NODE_APP_INSTANCE;
+const cdnHosts = {
+  dev: 'https://addons-dev-cdn.allizom.org',
+  stage: 'https://addons-stage-cdn.allizom.org',
+  production: 'https://addons.cdn.mozilla.net',
+};
+
+const apiHosts = {
+  dev: 'https://addons-dev.allizom.org',
+  stage: 'https://addons.allizom.org',
+  production: 'https://addons.mozilla.org',
+};
+
+describe(__filename, () => {
+  const existingNodeEnv = process.env.NODE_ENV;
+  const existingNodeAppInstance = process.env.NODE_APP_INSTANCE;
+
+  afterEach(() => {
+    process.env.NODE_ENV = existingNodeEnv;
+    delete process.env.NODE_APP_INSTANCE;
+    if (existingNodeAppInstance) {
+      process.env.NODE_APP_INSTANCE = existingNodeAppInstance;
+    }
+  });
+
+  describe('CSP Config', () => {
+    for (const env of deployedEnvs) {
+      // eslint-disable-next-lint no-loop-func
+      it(`should have a source-list config for ${env}`, () => {
+        process.env.NODE_ENV = env;
+        // Reset the require cache so that the config require
+        // takes into account changes to NODE_ENV.
+        jest.resetModules();
+        const cdnHost = cdnHosts[env];
+        const apiHost = apiHosts[env];
+        // eslint-disable-next-line global-require
+        const config = require('config');
+        const cspConfig = config.get('CSP').directives;
+        expect(cspConfig.scriptSrc).toContain(cdnHost);
+        expect(cspConfig.scriptSrc).not.toContain("'self'");
+        expect(cspConfig.imgSrc).toContain(cdnHost);
+        expect(cspConfig.imgSrc).toContain("'self'");
+        expect(cspConfig.styleSrc).toContain(cdnHost);
+        expect(cspConfig.styleSrc).not.toContain("'self'");
+        expect(cspConfig.connectSrc).toContain(apiHost);
+        expect(cspConfig.connectSrc).not.toContain("'self'");
+      });
+    }
+  });
+
+  describe('CSP defaults', () => {
+    beforeEach(() => {
+      process.env.NODE_ENV = 'production';
+      // Reset the require cache so that the config require
+      // takes into account changes to NODE_ENV.
+      jest.resetModules();
     });
 
+    it('should default default-src to "\'none\'"', () => {
+      // eslint-disable-next-line global-require
+      const config = require('config');
+      const cspConfig = config.get('CSP').directives;
+      expect(cspConfig.defaultSrc).toEqual(["'none'"]);
+    });
+
+    it('should default base-uri to "\'self\'"', () => {
+      // eslint-disable-next-line global-require
+      const config = require('config');
+      const cspConfig = config.get('CSP').directives;
+      expect(cspConfig.baseUri).toEqual(["'self'"]);
+    });
+
+    it('should default form-action to "\'none\'"', () => {
+      // eslint-disable-next-line global-require
+      const config = require('config');
+      const cspConfig = config.get('CSP').directives;
+      expect(cspConfig.formAction).toEqual(["'none'"]);
+    });
+
+    it('should default frame-src to "\'none\'"', () => {
+      // eslint-disable-next-line global-require
+      const config = require('config');
+      const cspConfig = config.get('CSP').directives;
+      expect(cspConfig.frameSrc).toEqual(["'none'"]);
+    });
+
+    it('should default child-src to "\'none\'"', () => {
+      // eslint-disable-next-line global-require
+      const config = require('config');
+      const cspConfig = config.get('CSP').directives;
+      expect(cspConfig.childSrc).toEqual(["'none'"]);
+    });
+
+    it('should default object-src to "\'none\'"', () => {
+      // eslint-disable-next-line global-require
+      const config = require('config');
+      const cspConfig = config.get('CSP').directives;
+      expect(cspConfig.objectSrc).toEqual(["'none'"]);
+    });
+
+    it('should default media-src to "\'none\'"', () => {
+      // eslint-disable-next-line global-require
+      const config = require('config');
+      const cspConfig = config.get('CSP').directives;
+      expect(cspConfig.mediaSrc).toEqual(["'none'"]);
+    });
+  });
+
+  describe('CSP Middleware', () => {
     it('provides the expected style-src directive', () => {
       process.env.NODE_ENV = 'prod';
       process.env.NODE_APP_INSTANCE = 'amo';
