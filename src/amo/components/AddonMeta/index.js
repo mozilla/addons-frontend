fcebdc17cde2d45b5195445378840daaf0632382
Srestha Srivastava <sresthasrivastava.ss@gmail.com>
2019-01-21 18:39:43 +0530

    Update index.js

--- src/amo/components/AddonMeta/index.js
+++ src/amo/components/AddonMeta/index.js
@@ -10,7 +10,6 @@ import MetadataCard from 'ui/components/MetadataCard';
 import Rating from 'ui/components/Rating';
 import RatingsByStar from 'amo/components/RatingsByStar';
 import type { I18nType } from 'core/types/i18n';
-import { ADDON_TYPE_SEARCH } from 'core/constants';
 import { isSearch } from 'core/utils';
 import './styles.scss';
 
@@ -44,7 +43,7 @@ export class AddonMetaBase extends React.Component<InternalProps> {
     if (!addon) {
       userCount = null;
       userTitle = i18n.gettext('Users');
-    } else if(isSearch(addon.type)){
+    } else if (isSearch(addon.type)) {
       userTitle = null;
     } else if (averageDailyUsers) {
       userCount = i18n.formatNumber(averageDailyUsers);
