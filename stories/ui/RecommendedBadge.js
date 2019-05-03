52698273357991b3f7fd14af22c35d2a11169a63
Bob Silverberg <bob.silverberg@gmail.com>
2019-05-03 10:38:26 -0400

    Update story and add RTL support

--- stories/ui/RecommendedBadge.js
+++ stories/ui/RecommendedBadge.js
@@ -1,15 +1,10 @@
 /* @flow */
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { fakeI18n } from 'tests/unit/helpers';
 
 import RecommendedBadge from 'ui/components/RecommendedBadge';
 
-import Provider from '../setup/Provider';
-
-storiesOf('RecommendedBadge', module)
-  .addDecorator((story) => (
-    <div className="RecommendedBadge--storybook">
-      <Provider story={story()} />
-    </div>
-  ))
-  .add('default', () => <RecommendedBadge />);
+storiesOf('RecommendedBadge', module).add('default', () => (
+  <RecommendedBadge i18n={fakeI18n({ includeJedSpy: false })} />
+));
