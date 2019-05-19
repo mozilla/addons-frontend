acd82222311c68712757cf6345385be6d311470b
Robert Duda <robertduda20@gmail.com>
2019-05-19 11:19:31 +0200

    ran prettier.

--- src/amo/pages/UserProfile/index.js
+++ src/amo/pages/UserProfile/index.js
@@ -354,9 +354,7 @@ export class UserProfileBase extends React.Component<InternalProps> {
                   className="UserProfile-homepage"
                   term={i18n.gettext('Homepage')}
                 >
-                  <a href={user.homepage}>
-                    {i18n.gettext('Homepage')}
-                  </a>
+                  <a href={user.homepage}>{i18n.gettext('Homepage')}</a>
                 </Definition>
               ) : null}
               {user && user.location ? (
