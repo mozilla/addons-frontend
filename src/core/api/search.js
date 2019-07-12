4c7e0bf7112ce1aaa608a7998c3298fb11bf39c2
William Durand <will+git@drnd.me>
2019-07-12 13:15:50 +0200

    prevent issues at the api level

--- src/core/api/search.js
+++ src/core/api/search.js
@@ -1,10 +1,14 @@
 /* @flow */
+import { oneLine } from 'common-tags';
+
+import { SEARCH_SORT_RANDOM } from 'core/constants';
 import { callApi } from 'core/api';
 import {
   addVersionCompatibilityToFilters,
   convertFiltersToQueryParams,
   fixFiltersForAndroidThemes,
 } from 'core/searchUtils';
+import log from 'core/logger';
 import type { ApiState } from 'core/reducers/api';
 
 // See: https://addons-server.readthedocs.io/en/latest/topics/api/addons.html#search
@@ -36,6 +40,22 @@ export function search({ api, auth = false, filters = {} }: SearchParams) {
     filters: fixFiltersForAndroidThemes({ api, filters }),
     userAgentInfo: api.userAgentInfo,
   });
+
+  // The API says: 'The "sort" parameter "random" can only be specified when
+  // the "featured" or "recommended" parameter is also present, and the "q"
+  // parameter absent.'
+  // Let's make sure we don't send invalid filters.
+  if (newFilters.sort && newFilters.sort === SEARCH_SORT_RANDOM) {
+    if (
+      !(newFilters.recommended || newFilters.featured) ||
+      ((newFilters.recommended || newFilters.featured) && newFilters.q)
+    ) {
+      delete newFilters.sort;
+
+      log.warn(oneLine`search api filter "sort=random" has been removed before
+        calling the api to avoid an incompatibility error.`);
+    }
+  }
 
   return callApi({
     endpoint: 'addons/search',
