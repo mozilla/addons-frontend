1aea16512280204acb0640c8a89c2583db906121
Benjamin Forehand Jr <bennyjr169@gmail.com>
2019-08-01 13:27:00 -0500

    Update smoke tests to use requests instead of seleium. (#8416)
    * Removed selenium, now using requests.

--- tests/smoke/test_homepage.py
+++ tests/smoke/test_homepage.py
@@ -1,18 +1,9 @@
 """Smoke tests for addons-frontend docker image."""
 
-import pytest
+import requests
 
 
-@pytest.fixture
-def firefox_options(firefox_options):
-    """Set up Firefox options."""
-    firefox_options.add_argument("-headless")
-    firefox_options.add_argument('-foreground')
-    return firefox_options
-
-
-@pytest.mark.nondestructive
-def test_hidden_comment_is_found(base_url, selenium):
+def test_hidden_comment_is_found(base_url):
     """Test hidden element is found."""
-    selenium.get(base_url)
-    assert "<!-- Godzilla of browsers -->" in selenium.page_source
+    r = requests.get(base_url)
+    assert "<!-- Godzilla of browsers -->" in r.text
