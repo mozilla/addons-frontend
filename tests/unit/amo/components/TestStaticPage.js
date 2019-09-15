01803d5dd5d1698a6d2f4c162924c1ad54be8e7c
xu3u4 <u4xu3@yahoo.com.tw>
2019-09-15 22:33:44 +0900

    Make StaticPage component for Static pages

---
+++ tests/unit/amo/components/TestStaticPage.js
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+
+import StaticPage from 'amo/components/StaticPage';
+import HeadLinks from 'amo/components/HeadLinks';
+import HeadMetaTags from 'amo/components/HeadMetaTags';
+import Card from 'ui/components/Card';
+
+describe(__filename, () => {
+  const render = (
+    { title, metaDescription, children } = {
+      title: 'Title',
+      metaDescription: 'Description',
+      children: <div>Test</div>,
+    },
+  ) =>
+    shallow(
+      <StaticPage title={title} metaDescription={metaDescription}>
+        {children}
+      </StaticPage>,
+    );
+
+  it('renders StaticPage Component', () => {
+    const title = 'Static page title';
+    const metaDescription = 'Static page HeadMetaTag description';
+    const children = <div>Static Page Content</div>;
+    const wrapper = render({ title, metaDescription, children });
+
+    expect(wrapper.find(HeadLinks)).toHaveLength(1);
+
+    expect(wrapper.find(Card)).toHaveClassName('StaticPage');
+    expect(wrapper.find(Card)).toHaveProp('header', title);
+
+    expect(wrapper.find(HeadMetaTags)).toHaveProp('title', title);
+    expect(wrapper.find(HeadMetaTags)).toHaveProp(
+      'description',
+      metaDescription,
+    );
+    expect(wrapper.find('.StaticPage-content').at(0)).toIncludeText(
+      'Static Page Content',
+    );
+  });
+});
--- tests/unit/amo/pages/StaticPages/TestReviewGuide.js
+++ tests/unit/amo/pages/StaticPages/TestReviewGuide.js
@@ -3,8 +3,7 @@ import * as React from 'react';
 import ReviewGuide, {
   ReviewGuideBase,
 } from 'amo/pages/StaticPages/ReviewGuide';
-import HeadLinks from 'amo/components/HeadLinks';
-import HeadMetaTags from 'amo/components/HeadMetaTags';
+import StaticPage from 'amo/components/StaticPage';
 import { fakeI18n, shallowUntilTarget } from 'tests/unit/helpers';
 
 describe(__filename, () => {
@@ -17,23 +16,13 @@ describe(__filename, () => {
 
   it('outputs an review guide page', () => {
     const root = render();
-    expect(root).toHaveClassName('StaticPage');
+
     expect(root.find('#review-guide')).toExist();
   });
 
-  it('renders a HeadMetaTags component', () => {
+  it('renders a StaticPage component', () => {
     const root = render();
 
-    expect(root.find(HeadMetaTags)).toHaveLength(1);
-    expect(root.find(HeadMetaTags).prop('title')).toEqual('Review Guidelines');
-    expect(root.find(HeadMetaTags).prop('description')).toMatch(
-      /Guidelines, tips, and/,
-    );
-  });
-
-  it('renders a HeadLinks component', () => {
-    const root = render();
-
-    expect(root.find(HeadLinks)).toHaveLength(1);
+    expect(root.find(StaticPage)).toHaveLength(1);
   });
 });
