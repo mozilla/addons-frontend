5d228be220aabbf21c516a2e13c48244c64f5bad
Bob Silverberg <bob.silverberg@gmail.com>
2019-09-19 11:55:14 -0400

    Remove decorations (swoosh) from Primary Hero shelf

--- src/amo/components/HeroRecommendation/index.js
+++ src/amo/components/HeroRecommendation/index.js
@@ -94,70 +94,6 @@ export class HeroRecommendationBase extends React.Component<InternalProps> {
     });
   };
 
-  renderOverlayShape() {
-    const gradientA = 'HeroRecommendation-gradient-a';
-    const gradientB = 'HeroRecommendation-gradient-b';
-
-    return (
-      <svg
-        className="HeroRecommendation-overlayShape"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 334 307"
-      >
-        <defs>
-          <linearGradient
-            id={gradientA}
-            x1="37.554%"
-            x2="20.41%"
-            y1="51.425%"
-            y2="49.159%"
-          >
-            <stop
-              className="HeroRecommendation-gradientA-startColor"
-              offset="0%"
-            />
-            <stop
-              className="HeroRecommendation-gradientA-endColor"
-              offset="100%"
-            />
-          </linearGradient>
-          <linearGradient
-            id={gradientB}
-            x1="0%"
-            x2="100%"
-            y1="36.092%"
-            y2="36.092%"
-          >
-            <stop
-              className="HeroRecommendation-gradientB-startColor"
-              offset="0%"
-            />
-            <stop
-              className="HeroRecommendation-gradientB-endColor"
-              offset="100%"
-            />
-          </linearGradient>
-        </defs>
-        <g fill="none">
-          <path
-            className="HeroRecommendation-solidSwoosh"
-            d="M0 307h267c-52.113-48.37-94.22-112.103-120.226-188.126C122.438 47.736 63.457 3.044 0 0v307z"
-          />
-          <path
-            fill={`url(#${gradientA})`}
-            d="M0 307c17.502-9.934 34.574-21.458 51.072-34.602C121.246 216.49 216.932 232.615 271 307H0z"
-            transform="matrix(-1 0 0 1 334 0)"
-          />
-          <path
-            fill={`url(#${gradientB})`}
-            d="M334 307H114c31.79-24.866 61.545-54.258 88.533-88.135C237.964 174.386 285.12 148.906 334 143v164z"
-            transform="matrix(-1 0 0 1 334 0)"
-          />
-        </g>
-      </svg>
-    );
-  }
-
   render() {
     const { _isInternalURL, i18n, shelfData } = this.props;
     const { addon, description, external, featuredImage } = shelfData;
@@ -219,7 +155,6 @@ export class HeroRecommendationBase extends React.Component<InternalProps> {
           />
           {link}
         </div>
-        {this.renderOverlayShape()}
       </section>
     );
   }
