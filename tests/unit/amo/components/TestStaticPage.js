95471bb3f10570fb7f7301ddb1a3e6bfa2c3bbbe
William Durand <will+git@drnd.me>
2019-09-30 09:41:41 +0200

    Minor tweaks

--- tests/unit/amo/components/TestStaticPage.js
+++ tests/unit/amo/components/TestStaticPage.js
@@ -7,30 +7,28 @@ import HeadMetaTags from 'amo/components/HeadMetaTags';
 import Card from 'ui/components/Card';
 
 describe(__filename, () => {
-  const render = (
-    { title, metaDescription, children } = {
-      title: 'Title',
-      metaDescription: 'Description',
-      children: <div>Test</div>,
-    },
-  ) =>
-    shallow(
+  const render = ({
+    children = <div>content</div>,
+    metaDescription = 'some description',
+    title = 'some title',
+  } = {}) => {
+    return shallow(
       <StaticPage title={title} metaDescription={metaDescription}>
         {children}
       </StaticPage>,
     );
+  };
 
-  it('renders StaticPage Component', () => {
+  it('renders a StaticPage Component', () => {
     const title = 'Static page title';
     const metaDescription = 'Static page HeadMetaTag description';
     const children = <div>Static Page Content</div>;
+
     const wrapper = render({ title, metaDescription, children });
 
     expect(wrapper.find(HeadLinks)).toHaveLength(1);
-
     expect(wrapper.find(Card)).toHaveClassName('StaticPage');
     expect(wrapper.find(Card)).toHaveProp('header', title);
-
     expect(wrapper.find(HeadMetaTags)).toHaveProp('title', title);
     expect(wrapper.find(HeadMetaTags)).toHaveProp(
       'description',
