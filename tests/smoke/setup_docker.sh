938809bab83201b5a792ef5b8831785df538e877
Benjamin Forehand Jr <bennyjr169@gmail.com>
2019-11-22 15:11:33 -0600

    Remove step. (#8979)

--- tests/smoke/setup_docker.sh
+++ tests/smoke/setup_docker.sh
@@ -4,7 +4,7 @@ git clone --depth 1 https://github.com/mozilla/addons-server.git
 docker build -f addons-server/Dockerfile --build-arg GROUP_ID=$(id -g) --build-arg USER_ID=$(id -u) -t addons/addons-server:latest addons-server/
 docker-compose -f addons-server/docker-compose.yml -f tests/ui/docker-compose.functional-tests.yml up -d --build
 docker-compose -f addons-server/docker-compose.yml -f tests/ui/docker-compose.functional-tests.yml ps
-mkdir addons-server/site-static
+sudo chown -R  $USER:$USER .
 # Make sure dependencies get updated in worker and web container
 docker-compose -f addons-server/docker-compose.yml -f tests/ui/docker-compose.functional-tests.yml exec worker make -f Makefile-docker update_deps update_assets
 docker-compose -f addons-server/docker-compose.yml -f tests/ui/docker-compose.functional-tests.yml restart worker
