da6cea1ffe5a62647fb4d6face728b1810f158cf
Bob Silverberg <bob.silverberg@gmail.com>
2020-01-10 12:07:55 -0500

    Enhance WrongPlatformWarning to support the new warning for Fenix

--- src/amo/components/WrongPlatformWarning/index.js
+++ src/amo/components/WrongPlatformWarning/index.js
@@ -8,7 +8,10 @@ import { connect } from 'react-redux';
 import { CLIENT_APP_ANDROID } from 'core/constants';
 import translate from 'core/i18n/translate';
 import { sanitizeHTML } from 'core/utils';
-import { correctedLocationForPlatform } from 'core/utils/compatibility';
+import {
+  correctedLocationForPlatform,
+  isFenix,
+} from 'core/utils/compatibility';
 import Notice, { warningInfoType } from 'ui/components/Notice';
 import type { AppState } from 'amo/store';
 import type { UserAgentInfoType } from 'core/reducers/api';
@@ -17,15 +20,20 @@ import type { ReactRouterLocationType } from 'core/types/router';
 
 import './styles.scss';
 
+// TODO: Update this to the correct SUMO article.
+export const FENIX_LINK_DESTINATION = 'https://support.mozilla.org/kb/';
+
 type Props = {|
   className?: string,
   fixAndroidLinkMessage?: string,
   fixFirefoxLinkMessage?: string,
+  fixFenixLinkMessage?: string,
 |};
 
 type InternalProps = {|
   ...Props,
   _correctedLocationForPlatform: typeof correctedLocationForPlatform,
+  _isFenix: typeof isFenix,
   clientApp: string,
   i18n: I18nType,
   location: ReactRouterLocationType,
@@ -35,11 +43,13 @@ type InternalProps = {|
 export class WrongPlatformWarningBase extends React.Component<InternalProps> {
   static defaultProps = {
     _correctedLocationForPlatform: correctedLocationForPlatform,
+    _isFenix: isFenix,
   };
 
   render() {
     const {
       _correctedLocationForPlatform,
+      _isFenix,
       className,
       clientApp,
       i18n,
@@ -47,34 +57,47 @@ export class WrongPlatformWarningBase extends React.Component<InternalProps> {
       userAgentInfo,
     } = this.props;
 
-    const newLocation = _correctedLocationForPlatform({
-      clientApp,
-      location,
-      userAgentInfo,
-    });
+    let message;
 
-    if (!newLocation) {
-      return null;
-    }
+    if (_isFenix(userAgentInfo)) {
+      message = i18n.sprintf(
+        this.props.fixFenixLinkMessage ||
+          i18n.gettext(
+            `To find add-ons compatible with Firefox on Android,
+               <a href="%(newLocation)s">click here</a>.`,
+          ),
+        { newLocation: FENIX_LINK_DESTINATION },
+      );
+    } else {
+      const newLocation = _correctedLocationForPlatform({
+        clientApp,
+        location,
+        userAgentInfo,
+      });
 
-    const fixAndroidLinkMessage =
-      this.props.fixAndroidLinkMessage ||
-      i18n.gettext(
-        `To find add-ons compatible with Firefox on Android,
+      if (!newLocation) {
+        return null;
+      }
+
+      const fixAndroidLinkMessage =
+        this.props.fixAndroidLinkMessage ||
+        i18n.gettext(
+          `To find add-ons compatible with Firefox on Android,
                <a href="%(newLocation)s">visit our mobile site</a>.`,
-      );
+        );
 
-    const fixFirefoxLinkMessage =
-      this.props.fixFirefoxLinkMessage ||
-      i18n.gettext(
-        `To find add-ons compatible with Firefox on desktop,
+      const fixFirefoxLinkMessage =
+        this.props.fixFirefoxLinkMessage ||
+        i18n.gettext(
+          `To find add-ons compatible with Firefox on desktop,
                <a href="%(newLocation)s">visit our desktop site</a>.`,
-      );
+        );
 
-    const message =
-      clientApp === CLIENT_APP_ANDROID
-        ? i18n.sprintf(fixFirefoxLinkMessage, { newLocation })
-        : i18n.sprintf(fixAndroidLinkMessage, { newLocation });
+      message =
+        clientApp === CLIENT_APP_ANDROID
+          ? i18n.sprintf(fixFirefoxLinkMessage, { newLocation })
+          : i18n.sprintf(fixAndroidLinkMessage, { newLocation });
+    }
 
     return (
       <div className={makeClassName('WrongPlatformWarning', className)}>
