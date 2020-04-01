mozilla/addons-frontend

commit 6be493910f23a8da45a5144da70cb11599d6a5a6

Fix logo in Header (small screen/anonymous pages) (#9313)

src/amo/components/Header/index.js
@@ -3,6 +3,7 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import config from 'config';
+import makeClassName from 'classnames';
 
 import GetFirefoxButton, {
   GET_FIREFOX_BUTTON_TYPE_HEADER,
@@ -152,7 +153,13 @@ export class HeaderBase extends React.Component {
   }
 
   render() {
-    const { _config, i18n, isHomePage, location } = this.props;
+    const {
+      _config,
+      i18n,
+      isHomePage,
+      loadedPageIsAnonymous,
+      location,
+    } = this.props;
 
     const headerLink = (
       <Link className="Header-title" to="/">
@@ -164,7 +171,11 @@ export class HeaderBase extends React.Component {
     );
 
     return (
-      <header className="Header">
+      <header
+        className={makeClassName('classHeader', {
+          'Header--loaded-page-is-anonymous': loadedPageIsAnonymous,
+        })}
+      >
         <div className="Header-wrapper">
           <div className="Header-content">
             {isHomePage ? (

src/amo/components/Header/styles.scss
@@ -36,12 +36,21 @@
   width: 100%;
   z-index: 4;
 
+  .Header--loaded-page-is-anonymous & {
+    margin-top: 39px;
+  }
+
   @include respond-to(medium) {
     @include text-align-start();
 
     align-self: end;
     grid-row: 1 / 2;
     margin: 48px 0 0;
+
+    &,
+    .Header--loaded-page-is-anonymous & {
+      margin: 48px 0 0;
+    }
   }
 
   @include respond-to(extraLarge) {
@@ -50,7 +59,11 @@
     align-self: center;
     grid-column: 1;
     grid-row: 2 / 2;
-    margin-top: 0;
+
+    &,
+    .Header--loaded-page-is-anonymous & {
+      margin-top: 0;
+    }
   }
 }
 

tests/unit/amo/components/TestHeader.js
@@ -36,6 +36,7 @@ describe(__filename, () => {
   it('renders an <h1> when isHomepage is true', () => {
     const root = renderHeader({ isHomePage: true });
 
+    expect(root).not.toHaveClassName('Header--loaded-page-is-anonymous');
     expect(root.find('.Header-title-wrapper')).toHaveTagName('h1');
     expect(root.find('.Header-title').type()).toEqual(Link);
     expect(root.find('.Header-title .visually-hidden').childAt(0)).toHaveText(
@@ -211,6 +212,7 @@ describe(__filename, () => {
 
     const root = renderHeader({ store });
 
+    expect(root).toHaveClassName('Header--loaded-page-is-anonymous');
     expect(root.find(DropdownMenu)).toHaveLength(0);
   });
 
@@ -220,6 +222,7 @@ describe(__filename, () => {
 
     const root = renderHeader({ store });
 
+    expect(root).toHaveClassName('Header--loaded-page-is-anonymous');
     expect(root.find(AuthenticateButton)).toHaveLength(0);
   });
 });

commit 7f2f708606a1a706fd35010caf389ff23c82e43c

Fix typo in className
Regression introduced in https://github.com/mozilla/addons-frontend/commit/6be493910f23a8da45a5144da70cb11599d6a5a6

src/amo/components/Header/index.js
@@ -172,7 +172,7 @@ export class HeaderBase extends React.Component {
 
     return (
       <header
-        className={makeClassName('classHeader', {
+        className={makeClassName('Header', {
           'Header--loaded-page-is-anonymous': loadedPageIsAnonymous,
         })}
       >