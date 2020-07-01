42f42aea795146aff85778008aa34e4cbba511fe
William Durand <will+git@drnd.me>
2020-07-01 16:13:28 +0200

    Return domain in set-cookie

--- config/lib/shared.js
+++ config/lib/shared.js
@@ -2,9 +2,13 @@ export const addonsServerDevCDN = 'https://addons-dev-cdn.allizom.org';
 export const addonsServerProdCDN = 'https://addons.cdn.mozilla.net';
 export const addonsServerStageCDN = 'https://addons-stage-cdn.allizom.org';
 
-export const apiDevHost = 'https://addons-dev.allizom.org';
-export const apiProdHost = 'https://addons.mozilla.org';
-export const apiStageHost = 'https://addons.allizom.org';
+export const devDomain = 'addons-dev.allizom.org';
+export const prodDomain = 'addons.mozilla.org';
+export const stageDomain = 'addons.allizom.org';
+
+export const apiDevHost = `https://${devDomain}`;
+export const apiProdHost = `https://${prodDomain}`;
+export const apiStageHost = `https://${stageDomain}`;
 
 export const baseUrlDev = apiDevHost;
 export const baseUrlProd = apiProdHost;
