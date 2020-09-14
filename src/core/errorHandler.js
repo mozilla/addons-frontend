cc6fa8211e28bb854fd348a3f42affe549faf73c
Bob Silverberg <bob.silverberg@gmail.com>
2020-09-14 16:02:30 -0400

    Fix lint errors

--- src/core/errorHandler.js
+++ src/core/errorHandler.js
@@ -7,6 +7,7 @@ import { clearError, setError, setErrorMessage } from 'core/actions/errors';
 import log from 'core/logger';
 import { normalizeFileNameId } from 'core/utils';
 import ErrorList from 'ui/components/ErrorList';
+import type { ErrorHandlerType } from 'core/types/errorHandler';
 
 function generateHandlerId({ name = '' } = {}) {
   return `${name}-${Math.random().toString(36).substr(2, 9)}`;
