230f8673e7fc1ed33580bec8a7a3480f5c449210
William Durand <will+git@drnd.me>
2020-10-27 13:19:00 +0100

    Fix smoke tests

--- tests/smoke/setup_docker.sh
+++ tests/smoke/setup_docker.sh
@@ -2,14 +2,14 @@
 set -x
 git clone --depth 1 https://github.com/mozilla/addons-server.git
 docker build -f addons-server/Dockerfile --build-arg GROUP_ID=$(id -g) --build-arg USER_ID=$(id -u) -t addons/addons-server:latest addons-server/
-docker-compose -f addons-server/docker-compose.yml -f tests/ui/docker-compose.functional-tests.yml up -d --build
-docker-compose -f addons-server/docker-compose.yml -f tests/ui/docker-compose.functional-tests.yml ps
+docker-compose -f addons-server/docker-compose.yml -f docker-compose.functional-tests.yml up -d --build
+docker-compose -f addons-server/docker-compose.yml -f docker-compose.functional-tests.yml ps
 sudo chown -R  $USER:$USER .
 # Make sure dependencies get updated in worker and web container
-docker-compose -f addons-server/docker-compose.yml -f tests/ui/docker-compose.functional-tests.yml exec worker make -f Makefile-docker update_deps update_assets
-docker-compose -f addons-server/docker-compose.yml -f tests/ui/docker-compose.functional-tests.yml restart worker
-docker-compose -f addons-server/docker-compose.yml -f tests/ui/docker-compose.functional-tests.yml exec web make -f Makefile-docker update_deps update_assets
-docker-compose -f addons-server/docker-compose.yml -f tests/ui/docker-compose.functional-tests.yml restart web
-docker-compose -f addons-server/docker-compose.yml -f tests/ui/docker-compose.functional-tests.yml exec web make setup-ui-tests
-docker-compose -f addons-server/docker-compose.yml -f tests/ui/docker-compose.functional-tests.yml stop nginx
-docker-compose -f addons-server/docker-compose.yml -f tests/ui/docker-compose.functional-tests.yml up -d
+docker-compose -f addons-server/docker-compose.yml -f docker-compose.functional-tests.yml exec worker make -f Makefile-docker update_deps update_assets
+docker-compose -f addons-server/docker-compose.yml -f docker-compose.functional-tests.yml restart worker
+docker-compose -f addons-server/docker-compose.yml -f docker-compose.functional-tests.yml exec web make -f Makefile-docker update_deps update_assets
+docker-compose -f addons-server/docker-compose.yml -f docker-compose.functional-tests.yml restart web
+docker-compose -f addons-server/docker-compose.yml -f docker-compose.functional-tests.yml exec web make setup-ui-tests
+docker-compose -f addons-server/docker-compose.yml -f docker-compose.functional-tests.yml stop nginx
+docker-compose -f addons-server/docker-compose.yml -f docker-compose.functional-tests.yml up -d
