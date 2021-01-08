bc67738218868c6bbfa4831867b5e6587aed304a
William Durand <will+git@drnd.me>
2021-01-08 15:17:22 +0100

    Set local ports to 700x because 400x is used for production (#9970)
    * Set local ports to 600x because 400x is used for production
    
    * Allow to override webpack port
    
    * use 700x

--- config/development-local.js
+++ config/development-local.js
@@ -1,13 +1,13 @@
 // This config should be used with a local addons-server setup.
 module.exports = {
   apiHost: 'http://olympia.test',
-  proxyPort: 4000,
+  proxyPort: 7000,
 
   serverHost: 'olympia.test',
 
   baseURL: 'http://olympia.test',
 
-  webpackServerPort: 4001,
+  webpackServerPort: 7001,
 
   mozillaUserId: 10968,
   CSP: false,
