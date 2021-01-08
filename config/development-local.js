b8da1886b9c37a01a3d719cd669e63a3c4593ac1
William Durand <will+git@drnd.me>
2021-01-08 14:22:30 +0100

    use 700x

--- config/development-local.js
+++ config/development-local.js
@@ -1,13 +1,13 @@
 // This config should be used with a local addons-server setup.
 module.exports = {
   apiHost: 'http://olympia.test',
-  proxyPort: 6000,
+  proxyPort: 7000,
 
   serverHost: 'olympia.test',
 
   baseURL: 'http://olympia.test',
 
-  webpackServerPort: 6001,
+  webpackServerPort: 7001,
 
   mozillaUserId: 10968,
   CSP: false,
