412e428c3e82264dab4fbcfde2ccc179a7069ec5
William Durand <will+git@drnd.me>
2021-03-02 18:35:46 +0100

    Replace photoswipe react lib

--- tests/unit/amo/components/TestScreenShots.js
+++ tests/unit/amo/components/TestScreenShots.js
@@ -1,11 +1,8 @@
 import { shallow, mount } from 'enzyme';
 import * as React from 'react';
-import { PhotoSwipeGallery } from 'react-photoswipe';
+import { Gallery, Item } from 'react-photoswipe-gallery';
 
-import ScreenShots, {
-  PHOTO_SWIPE_OPTIONS,
-  thumbnailContent,
-} from 'amo/components/ScreenShots';
+import ScreenShots, { PHOTO_SWIPE_OPTIONS } from 'amo/components/ScreenShots';
 
 const HEIGHT = 400;
 const WIDTH = 200;
@@ -34,36 +31,25 @@ describe(__filename, () => {
 
   it('renders the previews', () => {
     const root = shallow(<ScreenShots previews={previews} />);
-    const gallery = root.children().children();
 
-    expect(gallery.type()).toEqual(PhotoSwipeGallery);
-    expect(gallery.prop('items')).toEqual(previews);
-    expect(gallery.prop('thumbnailContent')).toEqual(thumbnailContent);
-  });
+    expect(root.find(Gallery)).toHaveLength(1);
+    expect(root.find(Gallery)).toHaveProp('options', PHOTO_SWIPE_OPTIONS);
+    expect(root.find(Item)).toHaveLength(previews.length);
 
-  it('renders custom thumbnail', () => {
-    const thumbnailSrc = 'http://example.com/thumbnail.png';
-    const thumbnailHeight = 123;
-    const thumbnailWidth = 200;
+    previews.forEach((preview, index) => {
+      const item = root.find(Item).at(index);
+      expect(item).toHaveProp('original', preview.src);
+      expect(item).toHaveProp('thumbnail', preview.thumbnail_src);
+      expect(item).toHaveProp('width', preview.w);
+      expect(item).toHaveProp('height', preview.h);
+      expect(item).toHaveProp('title', preview.title);
 
-    const item = {
-      src: 'https://foo.com/img.png',
-      title: 'test title',
-      h: HEIGHT,
-      w: WIDTH,
-      thumbnail_src: thumbnailSrc,
-      thumbnail_h: thumbnailHeight,
-      thumbnail_w: thumbnailWidth,
-    };
-
-    const thumbnail = shallow(thumbnailContent(item));
-
-    expect(thumbnail.type()).toEqual('img');
-    expect(thumbnail.prop('src')).toEqual(thumbnailSrc);
-    expect(thumbnail.prop('height')).toEqual(thumbnailHeight);
-    expect(thumbnail.prop('width')).toEqual(thumbnailWidth);
-    expect(thumbnail.prop('alt')).toEqual('test title');
-    expect(thumbnail.prop('title')).toEqual('test title');
+      const image = item.shallow();
+      expect(image.type()).toEqual('img');
+      expect(image).toHaveProp('src', preview.thumbnail_src);
+      expect(image).toHaveProp('width', preview.thumbnail_w);
+      expect(image).toHaveProp('height', preview.thumbnail_h);
+    });
   });
 
   it('scrolls to the active item on close', () => {
@@ -83,114 +69,12 @@ describe(__filename, () => {
     };
     sinon.stub(root.instance().viewport, 'querySelector').returns(list);
 
-    const photoswipe = { getCurrentIndex: () => 1 };
-    root.instance().onClose(photoswipe);
+    const fakePhotoswipe = {
+      getCurrentIndex: () => 1,
+      listen: (event, callback) => callback(),
+    };
+    root.instance().onOpenPhotoswipe(fakePhotoswipe);
     // 0 += 500 - 55
     expect(list.scrollLeft).toEqual(445);
-  });
-
-  describe('PHOTO_SWIPE_OPTIONS.getThumbBoundsFn', () => {
-    const { getThumbBoundsFn } = PHOTO_SWIPE_OPTIONS;
-
-    const getFakeDocument = ({ left, top, width }) => {
-      const fakeImg = {
-        getBoundingClientRect: () => ({
-          height: 123,
-          left,
-          top,
-          width,
-        }),
-      };
-
-      const fakeThumbnail = {
-        getElementsByTagName: () => [fakeImg],
-      };
-
-      const fakeDocument = {
-        querySelectorAll: () => [fakeThumbnail],
-      };
-
-      return fakeDocument;
-    };
-
-    it('returns false if thumbnail does not exist', () => {
-      const bounds = getThumbBoundsFn(0);
-
-      expect(bounds).toEqual(false);
-    });
-
-    it('returns false if _document is null', () => {
-      const bounds = getThumbBoundsFn(0, { _document: null });
-
-      expect(bounds).toEqual(false);
-    });
-
-    it('returns false if _window is null', () => {
-      const bounds = getThumbBoundsFn(0, {
-        _document: getFakeDocument({ left: 1, top: 2, width: 3 }),
-        _window: null,
-      });
-
-      expect(bounds).toEqual(false);
-    });
-
-    it('returns an object with x, y and w values', () => {
-      const left = 123;
-      const top = 124;
-      const width = 100;
-
-      const fakeDocument = getFakeDocument({ left, top, width });
-
-      const bounds = getThumbBoundsFn(0, { _document: fakeDocument });
-
-      expect(bounds).toEqual({
-        w: width,
-        x: left,
-        y: top,
-      });
-    });
-
-    it('uses window.pageYOffset to compute `y` if available', () => {
-      const left = 123;
-      const top = 124;
-      const width = 100;
-
-      const fakeDocument = getFakeDocument({ left, top, width });
-
-      const fakeWindow = {
-        pageYOffset: 20,
-      };
-
-      const bounds = getThumbBoundsFn(0, {
-        _document: fakeDocument,
-        _window: fakeWindow,
-      });
-
-      expect(bounds).toEqual({
-        w: width,
-        x: left,
-        y: top + fakeWindow.pageYOffset,
-      });
-    });
-
-    it('uses document.documentElement.scrollTop to compute `y` if available', () => {
-      const left = 123;
-      const top = 124;
-      const width = 100;
-      const scrollTop = 30;
-
-      const fakeDocument = getFakeDocument({ left, top, width });
-      fakeDocument.documentElement = {
-        scrollTop,
-      };
-
-      const bounds = getThumbBoundsFn(0, { _document: fakeDocument });
-
-      expect(bounds).toEqual({
-        w: width,
-        x: left,
-        y: top + scrollTop,
-      });
-    });
   });
 });
