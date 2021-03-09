3e0266024caffb3fdbb188676c2118ccbd79b386
William Durand <will+git@drnd.me>
2021-03-09 11:14:21 +0100

    Remove SurveyNotice in AppBanner (#10167)

--- src/amo/components/AppBanner/index.js
+++ src/amo/components/AppBanner/index.js
@@ -1,30 +1,19 @@
 /* @flow */
 import makeClassName from 'classnames';
 import * as React from 'react';
-import { withRouter } from 'react-router-dom';
 
 import SiteNotices from 'amo/components/SiteNotices';
-import SurveyNotice from 'amo/components/SurveyNotice';
-import type { ReactRouterLocationType } from 'amo/types/router';
 
 import './styles.scss';
 
 type Props = {| className?: string |};
 
-type InternalProps = {|
-  ...Props,
-  location: ReactRouterLocationType,
-|};
-
-export const AppBannerBase = ({ className, location }: InternalProps) => {
+export const AppBannerBase = ({ className }: Props) => {
   return (
     <div className={makeClassName('AppBanner', className)}>
       <SiteNotices />
-      <SurveyNotice location={location} />
     </div>
   );
 };
 
-const AppBanner: React.ComponentType<Props> = withRouter(AppBannerBase);
-
-export default AppBanner;
+export default AppBannerBase;
