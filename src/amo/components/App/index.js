485193cf148a192d91ff3492563c6c08acd66674
Andrew Williamson <awilliamson@mozilla.com>
2021-03-12 12:19:49 +0000

    move fonts under src

--- src/amo/components/App/index.js
+++ src/amo/components/App/index.js
@@ -11,7 +11,7 @@ import { compose } from 'redux';
 
 // We have to import these styles first to have them listed first in the final
 // CSS file. See: https://github.com/mozilla/addons-frontend/issues/3565
-import 'amo/fonts/inter.scss';
+import 'fonts/inter.scss';
 import 'normalize.css/normalize.css';
 import './styles.scss';
 
