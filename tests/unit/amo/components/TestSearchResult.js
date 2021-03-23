bf7d87dc04e2490676ce7b5efd5532c8bd903311
Andrew Williamson <awilliamson@mozilla.com>
2021-03-23 10:23:30 +0000

    lazy load theme search result images (#10247)
    * lazy load theme search result images
    
    * rm blank line

--- tests/unit/amo/components/TestSearchResult.js
+++ tests/unit/amo/components/TestSearchResult.js
@@ -366,6 +366,7 @@ describe(__filename, () => {
     const image = root.find('.SearchResult-icon');
 
     expect(image.prop('src')).toEqual(headerImageFull);
+    expect(image.prop('loading')).toEqual('lazy');
   });
 
   it('displays a fallback image for themes that only have 1 preview option', () => {
