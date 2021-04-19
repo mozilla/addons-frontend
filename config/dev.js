a638a4e996ae819593cad8752c3d5df6f62bf14f
Bob Silverberg <bob.silverberg@gmail.com>
2021-04-19 11:30:29 -0400

    Enable the CTA experiment on all environments

--- config/dev.js
+++ config/dev.js
@@ -46,8 +46,4 @@ module.exports = {
   extensionWorkshopUrl: 'https://extensionworkshop-dev.allizom.org',
 
   enableFeatureLinkToNewBlog: true,
-
-  experiments: {
-    '20210404_download_cta_experiment': true,
-  },
 };
