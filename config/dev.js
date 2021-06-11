7328c455b565d7c56bcc1f6715da7fb1de50a918
William Durand <will+git@drnd.me>
2021-06-11 14:33:05 +0200

    Link to the new blog in all envs (#10673)

--- config/dev.js
+++ config/dev.js
@@ -45,8 +45,6 @@ module.exports = {
 
   extensionWorkshopUrl: 'https://extensionworkshop-dev.allizom.org',
 
-  enableFeatureLinkToNewBlog: true,
-
   experiments: {
     '20210531_download_funnel_experiment': true,
   },
