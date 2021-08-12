377c69525a4962a3fa7dfb7bf7196aba1310f39f
Andrew Williamson <awilliamson@mozilla.com>
2021-08-12 17:10:11 +0100

    add TODO about param names

--- src/amo/searchUtils.js
+++ src/amo/searchUtils.js
@@ -9,6 +9,10 @@ import {
 import log from 'amo/logger';
 import { USER_AGENT_OS_IOS } from 'amo/reducers/api';
 
+// TODO: we need to specify these API params in `paramsToFilter` so that the
+// app uses them when making API calls but ideally we'd revisit the filter
+// names when we need to use the filters in our code (because `foo__lte` isn't
+// a great name).
 export function generateThresholdParams(param) {
   return ['__gt', '__lt', '__lte', '__gte', ''].reduce((object, key) => {
     return { ...object, [`${param}${key}`]: `${param}${key}` };
