80d136b768ff8c04310f4351ba7a07fe5bea6c54
championshuttler <shivams2799@gmail.com>
2021-10-19 21:53:25 +0530

    Rename the link to the blog on AMO (#10963)
    Co-authored-by: William Durand <will+git@drnd.me>
    Co-authored-by: Sonia Singla <ssingla@igalia.com>

--- src/amo/components/SectionLinks/index.js
+++ src/amo/components/SectionLinks/index.js
@@ -100,7 +100,7 @@ export class SectionLinksBase extends React.Component<InternalProps> {
             prependClientApp={false}
             prependLang={false}
           >
-            {i18n.gettext('Firefox Add-on Reviews')}
+            {i18n.gettext('Firefox Add-ons Blog')}
           </Link>
         </DropdownMenuItem>,
       );
