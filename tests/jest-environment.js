5ec1c9ec8797b5a7cc003fc7cdbec3fa4a133423
Bob Silverberg <bob.silverberg@gmail.com>
2021-11-26 15:51:11 -0500

    Adjust jest environment and reporters

---
+++ tests/jest-environment.js
@@ -0,0 +1,22 @@
+const { TextEncoder, TextDecoder } = require('util');
+
+const JsdomEnvironment = require('jest-environment-jsdom');
+
+class CustomEnvironment extends JsdomEnvironment {
+  async setup() {
+    await super.setup();
+    // These are needed for Jest + jsdom v17.
+    this.global.TextEncoder = TextEncoder;
+    this.global.TextDecoder = TextDecoder;
+  }
+
+  async teardown() {
+    await super.teardown();
+  }
+
+  getVmContext() {
+    return super.getVmContext();
+  }
+}
+
+module.exports = CustomEnvironment;
