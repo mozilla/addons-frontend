45a27d9cd64ca3f68373030f59a8aa3bf1048f31
Bob Silverberg <bob.silverberg@gmail.com>
2021-12-08 17:49:35 -0500

    Avoid global.window

--- src/amo/purify.js
+++ src/amo/purify.js
@@ -1,13 +1,5 @@
-/* global window */
 import createDOMPurify from 'dompurify';
 
-// We need a window from jsdom when it does not exist, which is when we are
-// running on node (for SSR). 'amo/window' provides this for us, by importing
-// jsdom, but we don't want to import that in tests, as the tests already
-// import jsdom via the "jsdom" testEnvironment.
-if (typeof window === 'undefined') {
-  // eslint-disable-next-line global-require
-  global.window = require('amo/window');
-}
+import universalWindow from 'amo/window';
 
-export default createDOMPurify(window);
+export default createDOMPurify(universalWindow);
