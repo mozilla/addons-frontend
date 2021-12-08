26cf478f4fc83e70201a5bdf9d05e62283bbe949
Bob Silverberg <bob.silverberg@gmail.com>
2021-12-08 16:27:39 -0500

    Conditionally import jsdom so that it doesn't get imported twice in tests

--- src/amo/purify.js
+++ src/amo/purify.js
@@ -1,5 +1,13 @@
+/* global window */
 import createDOMPurify from 'dompurify';
 
-import universalWindow from 'amo/window';
+// We need a window from jsdom when it does not exist, which is when we are
+// running on node (for SSR). 'amo/window' provides this for us, by importing
+// jsdom, but we don't want to import that in tests, as the tests already
+// import jsdom via the "jsdom" testEnvironment.
+if (typeof window === 'undefined') {
+  // eslint-disable-next-line global-require
+  global.window = require('amo/window');
+}
 
-export default createDOMPurify(universalWindow);
+export default createDOMPurify(window);
