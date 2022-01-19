53a4004128786a4b8547bb5b95d808829642df07
Andrew Williamson <awilliamson@mozilla.com>
2022-01-19 14:54:37 +0000

    Add test for reload button

--- tests/unit/amo/components/Errors/TestAuthExpired.js
+++ tests/unit/amo/components/Errors/TestAuthExpired.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 
+import Button from 'amo/components/Button';
 import ErrorComponent from 'amo/components/Errors/ErrorComponent';
 import AuthExpired, {
   AuthExpiredBase,
@@ -17,17 +18,19 @@ import { API_ERROR_SIGNATURE_EXPIRED } from 'amo/constants';
 describe(__filename, () => {
   let store;
 
-  const render = () => {
+  const render = (customProps = {}) => {
     const error = createApiError({
       apiURL: 'http://test.com',
       response: { status: 401, code: API_ERROR_SIGNATURE_EXPIRED },
     });
     store.dispatch(loadErrorPage({ error }));
+    const props = {
+      i18n: fakeI18n(),
+      store,
+      ...customProps,
+    };
 
-    return shallowUntilTarget(
-      <AuthExpired i18n={fakeI18n()} store={store} />,
-      AuthExpiredBase,
-    );
+    return shallowUntilTarget(<AuthExpired {...props} />, AuthExpiredBase);
   };
 
   beforeEach(() => {
@@ -48,5 +51,21 @@ describe(__filename, () => {
     render();
 
     sinon.assert.calledWith(dispatchSpy, logOutUserAction());
+  });
+
+  it('renders a reload button', () => {
+    const _window = { location: { reload: sinon.stub() } };
+    const root = render({ _window });
+
+    const button = root.find(Button);
+    expect(button.childAt(0).text()).toContain('Reload the page');
+    expect(button.prop('onClick')).toBeDefined();
+
+    const action = button.prop('onClick');
+    // Simulate the callback on button press.
+    action();
+
+    // The button should reload the location.
+    sinon.assert.called(_window.location.reload);
   });
 });
