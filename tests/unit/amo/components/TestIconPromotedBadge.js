d04312cb9c2c179e9f6232458cdbab6bfc6ca41f
Bob Silverberg <bob.silverberg@gmail.com>
2022-02-25 09:43:37 -0500

    Improve the API of our custom test queries (#11320)

--- tests/unit/amo/components/TestIconPromotedBadge.js
+++ tests/unit/amo/components/TestIconPromotedBadge.js
@@ -70,7 +70,7 @@ describe(__filename, () => {
 
     // Icon will render a <span> with a class of
     // 'visually-hidden' if an `alt` prop was passed.
-    expect(screen.queryByClassName('visually-hidden')).toHaveLength(0);
+    expect(screen.queryByClassName('visually-hidden')).not.toBeInTheDocument();
   });
 
   it.each(['recommended', 'verified'])(
