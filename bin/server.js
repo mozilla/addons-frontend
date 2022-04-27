4f4caf2f0494ecd0a9738b8f55dfc3792781d5ee
Mathieu Pillard <mpillard@mozilla.com>
2022-04-27 12:47:52 +0200

    Replace piping with nodemon for hot-reloading

--- bin/server.js
+++ bin/server.js
@@ -1,22 +1,10 @@
 #!/usr/bin/env node
-/* eslint-disable global-require, no-console */
-const path = require('path');
 
-const chokidar = require('chokidar');
 require('@babel/register')({
   plugins: ['dynamic-import-node'],
 });
-const config = require('config');
-const touch = require('touch');
 
 if (process.env.NODE_ENV === 'development') {
-  if (!require('piping')({
-    hook: true,
-    ignore: /(\/\.|~$|\.json|\.scss$)/i,
-  })) {
-    return;
-  }
-
   if (process.env.USE_HTTPS_FOR_DEV) {
     // Skip SSL check to avoid the 'self signed certificate in certificate
     // chain' error.
@@ -24,18 +12,4 @@ if (process.env.NODE_ENV === 'development') {
   }
 }
 
-require('amo/server/base').runServer().then(() => {
-  if (config.get('isDevelopment')) {
-    const basePath = config.get('basePath');
-    const watcher = chokidar.watch(path.join(basePath, 'src'));
-
-    watcher.on('ready', () => {
-      console.log('👀 The file watcher for server hot reload is ready.');
-
-      watcher.on('all', () => {
-        // Touching this file triggers a server reload.
-        touch.sync(__filename);
-      });
-    });
-  }
-});
+require('amo/server/base').runServer();
