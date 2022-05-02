0bb3189d17566315e678189ac14d97b7c83fdd76
Bob Silverberg <bob.silverberg@gmail.com>
2022-05-02 14:41:20 -0400

    Migrate TestAuthenticateButton.js

--- tests/unit/amo/components/TestAuthenticateButton.js
+++ tests/unit/amo/components/TestAuthenticateButton.js
@@ -1,221 +1,207 @@
+/* global window */
 import * as React from 'react';
+import { createEvent, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
-import { setAuthToken } from 'amo/reducers/api';
-import { loadCurrentUserAccount, logOutUser } from 'amo/reducers/users';
+import { logOutUser } from 'amo/reducers/users';
 import * as api from 'amo/api';
 import AuthenticateButton, {
-  AuthenticateButtonBase,
   createHandleLogOutFunction,
-  mapStateToProps,
 } from 'amo/components/AuthenticateButton';
 import { loadSiteStatus } from 'amo/reducers/site';
 import {
-  createContextWithFakeRouter,
-  createFakeEvent,
-  createUserAccountResponse,
+  createHistory,
   dispatchClientMetadata,
-  dispatchSignInActions,
-  fakeI18n,
-  createFakeLocation,
-  shallowUntilTarget,
-  userAuthSessionId,
+  dispatchSignInActionsWithStore,
+  render as defaultRender,
+  screen,
 } from 'tests/unit/helpers';
-import Icon from 'amo/components/Icon';
 
 describe(__filename, () => {
-  function render({ location = createFakeLocation(), ...customProps } = {}) {
-    const { store } = dispatchSignInActions();
-    const props = {
-      handleLogOut: sinon.stub(),
-      i18n: fakeI18n(),
-      store,
-      ...customProps,
-    };
+  let store;
 
-    return shallowUntilTarget(
-      <AuthenticateButton {...props} />,
-      AuthenticateButtonBase,
-      {
-        shallowOptions: createContextWithFakeRouter({ location }),
-      },
-    );
-  }
+  const savedLocation = window.location;
+
+  beforeEach(() => {
+    store = dispatchClientMetadata().store;
+    delete window.location;
+    window.location = Object.assign(new URL('https://example.org'), {
+      assign: jest.fn(),
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks().resetModules();
+    window.location = savedLocation;
+  });
+
+  const render = ({ location, ...props } = {}) => {
+    const renderOptions = {
+      history: createHistory({
+        initialEntries: [location || '/'],
+      }),
+      store,
+    };
+    return defaultRender(<AuthenticateButton {...props} />, renderOptions);
+  };
 
   it('passes along a className', () => {
     const className = 'MyComponent-auth-button';
-    const root = render({ className });
+    render({ className });
 
-    expect(root).toHaveClassName(className);
+    expect(screen.getByRole('link')).toHaveClass(className);
   });
 
   it('renders an Icon by default', () => {
-    const root = render();
+    render();
 
-    expect(root.find(Icon)).toHaveLength(1);
+    expect(screen.getByClassName('Icon-user-dark')).toBeInTheDocument();
   });
 
   it('lets you hide the Icon', () => {
-    const root = render({ noIcon: true });
+    render({ noIcon: true });
 
-    expect(root.find(Icon)).toHaveLength(0);
+    expect(screen.queryByClassName('Icon-user-dark')).not.toBeInTheDocument();
   });
 
   it('lets you customize the log in text', () => {
-    const { store } = dispatchClientMetadata();
-    const root = render({ logInText: 'Maybe log in?', store });
+    const logInText = 'Maybe log in?';
+    render({ logInText });
 
-    expect(root.childAt(1)).toHaveText('Maybe log in?');
-    expect(root).toHaveProp('href', '#login');
+    expect(screen.getByRole('link', { name: logInText })).toHaveAttribute(
+      'href',
+      '#login',
+    );
   });
 
   it('lets you customize the log out text', () => {
-    const { store } = dispatchSignInActions();
-    const root = render({ logOutText: 'Maybe log out?', store });
+    const logOutText = 'Maybe log out?';
+    dispatchSignInActionsWithStore({ store });
+    render({ logOutText });
 
-    expect(root.childAt(1)).toHaveText('Maybe log out?');
-    expect(root).toHaveProp('href', '#logout');
+    expect(screen.getByRole('link', { name: logOutText })).toHaveAttribute(
+      'href',
+      '#logout',
+    );
   });
 
   it('shows a log in button when unauthenticated', () => {
-    const { store } = dispatchClientMetadata();
-    const handleLogIn = sinon.spy();
-    const location = createFakeLocation();
-    const root = render({ handleLogIn, location, store });
+    const handleLogIn = jest.fn();
+    const location = '/some-location/';
+    render({ handleLogIn, location });
 
-    expect(root.childAt(1)).toHaveText('Log in');
+    const link = screen.getByRole('link', { name: 'Log in' });
+    const clickEvent = createEvent.click(link);
+    const preventDefaultWatcher = jest.spyOn(clickEvent, 'preventDefault');
 
-    const clickEvent = createFakeEvent();
-    root.simulate('click', clickEvent);
-
-    sinon.assert.calledWith(handleLogIn, location);
-    sinon.assert.calledOnce(clickEvent.preventDefault);
+    fireEvent(link, clickEvent);
+    expect(handleLogIn).toHaveBeenCalledWith(
+      expect.objectContaining({ pathname: location }),
+    );
+    expect(preventDefaultWatcher).toHaveBeenCalled();
   });
 
   it('shows a log out button when authenticated', () => {
-    const { store } = dispatchSignInActions();
-    const handleLogOut = sinon.spy();
-    const root = render({ handleLogOut, store });
+    dispatchSignInActionsWithStore({ store });
+    const handleLogOut = jest.fn();
+    render({ handleLogOut });
 
-    expect(root.childAt(1)).toHaveText('Log out');
+    const link = screen.getByRole('link', { name: 'Log out' });
+    const clickEvent = createEvent.click(link);
+    const preventDefaultWatcher = jest.spyOn(clickEvent, 'preventDefault');
 
-    const clickEvent = createFakeEvent();
-    root.simulate('click', clickEvent);
-
-    sinon.assert.calledOnce(handleLogOut);
-    sinon.assert.calledOnce(clickEvent.preventDefault);
+    fireEvent(link, clickEvent);
+    expect(handleLogOut).toHaveBeenCalledWith({ api: store.getState().api });
+    expect(preventDefaultWatcher).toHaveBeenCalled();
   });
 
   it('updates the location on handleLogIn', () => {
-    const { store } = dispatchSignInActions();
-    const _window = { location: '/foo' };
-    const location = createFakeLocation({
-      pathname: '/bar',
-      query: { q: 'wat' },
+    const location = '/some/location/';
+    const mockLoginURL = '/some/login/';
+    const startLoginUrl = jest.spyOn(api, 'startLoginUrl');
+    startLoginUrl.mockReturnValue(mockLoginURL);
+    render({ location });
+
+    userEvent.click(screen.getByRole('link', { name: 'Log in' }));
+
+    expect(startLoginUrl).toHaveBeenCalledWith({
+      location: expect.objectContaining({ pathname: location }),
     });
-    const startLoginUrlStub = sinon
-      .stub(api, 'startLoginUrl')
-      .returns('https://https://addons.mozilla.org/login');
-
-    const { handleLogIn } = mapStateToProps(store.getState(), {});
-    handleLogIn(location, { _window });
-
-    expect(_window.location).toEqual(
-      'https://https://addons.mozilla.org/login',
-    );
-    sinon.assert.calledWith(startLoginUrlStub, { location });
+    expect(window.location.assign).toHaveBeenCalledWith(mockLoginURL);
   });
 
-  it('retrieves `isAuthenticated` from state', () => {
-    const { store } = dispatchClientMetadata();
-    const user = createUserAccountResponse();
+  it('calls logOutFromServer on handleLogOut', () => {
+    const logOutFromServer = jest.spyOn(api, 'logOutFromServer');
+    logOutFromServer.mockResolvedValue(true);
+    dispatchSignInActionsWithStore({ store });
+    render();
 
-    expect(mapStateToProps(store.getState(), {}).siteUser).toEqual(null);
+    userEvent.click(screen.getByRole('link', { name: 'Log out' }));
 
-    store.dispatch(setAuthToken(userAuthSessionId()));
-    store.dispatch(loadCurrentUserAccount({ user }));
-
-    expect(mapStateToProps(store.getState(), {}).siteUser).toMatchObject(user);
-  });
-
-  it('allows a signed-in user to log out', () => {
-    const { store } = dispatchSignInActions();
-    const handleLogOut = sinon.stub();
-    const allProps = {
-      handleLogOut,
-      i18n: fakeI18n(),
-    };
-
-    const wrapper = shallowUntilTarget(
-      <AuthenticateButton store={store} {...allProps} />,
-      AuthenticateButtonBase,
-      {
-        shallowOptions: createContextWithFakeRouter(),
-      },
-    );
-
-    wrapper.simulate('click', createFakeEvent());
-
-    sinon.assert.calledWith(handleLogOut, {
+    expect(logOutFromServer).toHaveBeenCalledWith({
       api: store.getState().api,
     });
   });
 
   it('is disabled when the site is in readonly mode', () => {
-    const { store } = dispatchSignInActions();
     store.dispatch(loadSiteStatus({ readOnly: true, notice: null }));
 
-    const root = render({ store });
+    render();
 
-    expect(root).toHaveProp('disabled', true);
-    expect(root).toHaveProp(
-      'title',
-      expect.stringContaining('currently unavailable'),
-    );
+    expect(
+      screen.getByTitle(
+        'This action is currently unavailable. Please reload the page in a moment.',
+      ),
+    ).toHaveClass('Button--disabled');
   });
 
   it('is not disabled when the site is not in readonly mode', () => {
-    const { store } = dispatchSignInActions();
     store.dispatch(loadSiteStatus({ readOnly: false, notice: null }));
 
-    const root = render({ store });
+    render();
 
-    expect(root).toHaveProp('disabled', false);
-    expect(root).toHaveProp('title', null);
+    expect(screen.getByRole('link', { name: 'Log in' })).not.toHaveClass(
+      'Button--disabled',
+    );
   });
 
   describe('createHandleLogOutFunction()', () => {
     it('logs out a signed-in user on the client side even if the API returns an error', () => {
       const error = api.createApiError({ response: { status: 401 } });
-      sinon.stub(api, 'logOutFromServer').returns(Promise.reject(error));
+      const dispatch = jest.spyOn(store, 'dispatch');
+      const logOutFromServer = jest.spyOn(api, 'logOutFromServer');
+      logOutFromServer.mockRejectedValue(error);
+      dispatchSignInActionsWithStore({ store });
 
-      const { store, state } = dispatchSignInActions();
-      const dispatchSpy = sinon.spy(store, 'dispatch');
-
-      const handleLogOut = createHandleLogOutFunction(dispatchSpy);
-      const apiState = state.api;
+      const handleLogOut = createHandleLogOutFunction(dispatch);
+      const apiState = store.getState().api;
       // This makes sure the state contains a token because user is logged in.
       expect(apiState.token).toBeTruthy();
 
       return handleLogOut({ api: apiState }).then(() => {
-        sinon.assert.calledWith(api.logOutFromServer, { api: apiState });
-        sinon.assert.calledWith(dispatchSpy, logOutUser());
+        expect(logOutFromServer).toHaveBeenCalledWith({
+          api: apiState,
+        });
+        expect(dispatch).toHaveBeenCalledWith(logOutUser());
       });
     });
 
     it('logs out a signed-in user on the server and client sides', () => {
-      sinon.stub(api, 'logOutFromServer').returns(Promise.resolve());
+      const dispatch = jest.spyOn(store, 'dispatch');
+      const logOutFromServer = jest.spyOn(api, 'logOutFromServer');
+      logOutFromServer.mockResolvedValue(true);
+      dispatchSignInActionsWithStore({ store });
 
-      const { store, state } = dispatchSignInActions();
-      const dispatchSpy = sinon.spy(store, 'dispatch');
-
-      const handleLogOut = createHandleLogOutFunction(dispatchSpy);
-      const apiState = state.api;
+      const handleLogOut = createHandleLogOutFunction(dispatch);
+      const apiState = store.getState().api;
       // This makes sure the state contains a token because user is logged in.
       expect(apiState.token).toBeTruthy();
 
       return handleLogOut({ api: apiState }).then(() => {
-        sinon.assert.calledWith(api.logOutFromServer, { api: apiState });
-        sinon.assert.calledWith(dispatchSpy, logOutUser());
+        expect(logOutFromServer).toHaveBeenCalledWith({
+          api: apiState,
+        });
+        expect(dispatch).toHaveBeenCalledWith(logOutUser());
       });
     });
   });
