23046bfa6c4ae68fe826f12d4b3beaf35598e8d7
Bob Silverberg <bob.silverberg@gmail.com>
2022-05-02 13:43:17 -0400

    Migrate TestNotFound.js and TestNotFoundPage.js

--- tests/unit/amo/pages/ErrorPages/TestNotFoundPage.js
+++ tests/unit/amo/pages/ErrorPages/TestNotFoundPage.js
@@ -1,13 +1,81 @@
-import { shallow } from 'enzyme';
-import * as React from 'react';
+import { oneLine } from 'common-tags';
 
-import NotFound from 'amo/components/Errors/NotFound';
-import NotFoundPage from 'amo/pages/ErrorPages/NotFoundPage';
+import {
+  createHistory,
+  fakeI18n,
+  renderPage as defaultRender,
+  screen,
+} from 'tests/unit/helpers';
 
 describe(__filename, () => {
-  it('renders a NotFound component', () => {
-    const root = shallow(<NotFoundPage />);
+  const render = (i18n = fakeI18n()) =>
+    defaultRender({
+      history: createHistory({
+        initialEntries: ['/en-US/firefox/404/'],
+      }),
+      i18n,
+    });
 
-    expect(root.find(NotFound)).toHaveLength(1);
+  it('renders a NotFound Page', () => {
+    render();
+
+    expect(
+      screen.getByText('Oops! We can’t find that page'),
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByTextAcrossTags(
+        `Try visiting the page later, as the theme or extension may become ` +
+          `available again. Alternatively, you may be able to find what you’re ` +
+          `looking for in one of the available extensions or themes, or by ` +
+          `asking for help on our community forums.`,
+      ),
+    ).toBeInTheDocument();
+
+    expect(screen.getByRole('link', { name: 'extensions' })).toHaveAttribute(
+      'href',
+      '/en-US/firefox/extensions/',
+    );
+    expect(screen.getByRole('link', { name: 'themes' })).toHaveAttribute(
+      'href',
+      '/en-US/firefox/themes/',
+    );
+    expect(
+      screen.getByRole('link', { name: 'community forums' }),
+    ).toHaveAttribute('href', expect.stringContaining('discourse'));
+  });
+
+  it('handles a localized string with links inverted', () => {
+    const localizedString = oneLine`Some content with links inverted:
+      %(themeStart)sthemes%(themeEnd)s or
+      %(communityStart)scommunity forums%(communityEnd)s or
+      %(extensionStart)sextensions%(extensionEnd)s.`;
+
+    const i18n = fakeI18n();
+    // We override the `gettext` function to inject a localized string with the
+    // two links inverted. This was the issue in
+    // https://github.com/mozilla/addons-frontend/issues/7597.
+    i18n.gettext = (string) => {
+      if (string.startsWith('Try visiting')) {
+        return localizedString;
+      }
+
+      return string;
+    };
+
+    // It should not crash.
+    render(i18n);
+
+    expect(screen.getByRole('link', { name: 'extensions' })).toHaveAttribute(
+      'href',
+      '/en-US/firefox/extensions/',
+    );
+    expect(screen.getByRole('link', { name: 'themes' })).toHaveAttribute(
+      'href',
+      '/en-US/firefox/themes/',
+    );
+    expect(
+      screen.getByRole('link', { name: 'community forums' }),
+    ).toHaveAttribute('href', expect.stringContaining('discourse'));
   });
 });
