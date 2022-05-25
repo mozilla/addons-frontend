20f642692ca5fdc7c699901d637b90e603750d91
Mathieu Pillard <mpillard@mozilla.com>
2022-05-25 11:47:17 +0200

    Move static pages tests into a single file and search by role+text

---
+++ tests/unit/amo/pages/TestStaticPages.js
@@ -0,0 +1,35 @@
+import {
+  createHistory,
+  renderPage as defaultRender,
+  screen,
+} from 'tests/unit/helpers';
+
+describe(__filename, () => {
+  function render({ location }) {
+    return defaultRender({
+      history: createHistory({
+        initialEntries: [location],
+      }),
+    });
+  }
+
+  it('outputs the about page', () => {
+    render({ location: '/en-US/about' });
+
+    expect(screen.getByText('About Firefox Add-ons')).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', { name: 'A community of creators' }),
+    ).toBeInTheDocument();
+  });
+
+  it('outputs the review guide page', () => {
+    render({ location: '/en-US/review_guide' });
+
+    expect(screen.getByText('Review Guidelines')).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', {
+        name: 'Frequently Asked Questions about Reviews',
+      }),
+    ).toBeInTheDocument();
+  });
+});
