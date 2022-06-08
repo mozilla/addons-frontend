08b0bb931932d1829ef890523e99662f398386aa
Bob Silverberg <bob.silverberg@gmail.com>
2022-06-08 10:02:41 -0400

    Migrate TestScrollToTop.js (#11571)

--- tests/unit/amo/components/TestScrollToTop.js
+++ tests/unit/amo/components/TestScrollToTop.js
@@ -1,61 +1,76 @@
 import * as React from 'react';
 
-import ScrollToTop, { ScrollToTopBase } from 'amo/components/ScrollToTop';
+import ScrollToTop from 'amo/components/ScrollToTop';
 import {
-  createContextWithFakeRouter,
-  createFakeLocation,
-  shallowUntilTarget,
+  dispatchClientMetadata,
+  onLocationChanged,
+  render as defaultRender,
+  screen,
 } from 'tests/unit/helpers';
 
 describe(__filename, () => {
+  let store;
+
+  beforeEach(() => {
+    store = dispatchClientMetadata().store;
+  });
+
   const render = ({ location, ...props } = {}) => {
-    return shallowUntilTarget(<ScrollToTop {...props} />, ScrollToTopBase, {
-      shallowOptions: createContextWithFakeRouter({ location }),
-    });
+    return defaultRender(<ScrollToTop {...props} />, { location, store });
   };
 
   it('renders nothing if there are no children', () => {
-    const root = render();
-    expect(root.html()).toEqual(null);
+    const { root } = render();
+
+    expect(root).toEqual(null);
   });
 
   it('renders children', () => {
-    const root = render({ children: <p /> });
-    expect(root.find('p')).toHaveLength(1);
+    const childText = 'Some text to look for';
+    render({ children: <div>{childText}</div> });
+
+    expect(screen.getByText(childText)).toBeInTheDocument();
   });
 
   it('calls window.scrollTo() when location changes', () => {
     const _window = {
-      scrollTo: sinon.spy(),
+      scrollTo: jest.fn(),
     };
 
-    const root = render({ _window });
-    sinon.assert.notCalled(_window.scrollTo);
+    render({ _window, location: '/' });
+    expect(_window.scrollTo).not.toHaveBeenCalled();
 
-    root.setProps({ location: createFakeLocation() });
-    sinon.assert.calledWith(_window.scrollTo, 0, 0);
+    store.dispatch(
+      onLocationChanged({
+        pathname: '/another/path/',
+      }),
+    );
+
+    expect(_window.scrollTo).toHaveBeenCalledWith(0, 0);
   });
 
   // eslint-disable-next-line jest/expect-expect
   it('does not call window.scrollTo() when window is not defined', () => {
     const _window = null;
 
-    const root = render({ _window });
-
     // Make sure this doesn't throw.
-    root.setProps({ location: createFakeLocation() });
+    render({ _window });
   });
 
   it('does not call window.scrollTo() when location does not change', () => {
     const _window = {
-      scrollTo: sinon.spy(),
+      scrollTo: jest.fn(),
     };
-    const location = createFakeLocation();
 
-    const root = render({ _window, location });
-    sinon.assert.notCalled(_window.scrollTo);
+    render({ _window, location: '/' });
+    expect(_window.scrollTo).not.toHaveBeenCalled();
 
-    root.setProps({ location });
-    sinon.assert.notCalled(_window.scrollTo);
+    store.dispatch(
+      onLocationChanged({
+        pathname: '/',
+      }),
+    );
+
+    expect(_window.scrollTo).not.toHaveBeenCalled();
   });
 });
