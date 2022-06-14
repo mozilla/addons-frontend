f0cfb519f345fc896bad3292cf9af40f99c7f78a
Bob Silverberg <bob.silverberg@gmail.com>
2022-06-14 08:19:19 -0400

    Migrate TestOverlayCard.js and TestOverlay.js (#11579)

--- tests/unit/amo/components/TestOverlayCard.js
+++ tests/unit/amo/components/TestOverlayCard.js
@@ -1,54 +1,156 @@
-import { shallow } from 'enzyme';
 import * as React from 'react';
+import { fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
-import Overlay from 'amo/components/Overlay';
+import { extractId } from 'amo/components/Overlay';
 import OverlayCard from 'amo/components/OverlayCard';
-import { dispatchClientMetadata } from 'tests/unit/helpers';
+import { setUIState } from 'amo/reducers/uiState';
+import {
+  dispatchClientMetadata,
+  render as defaultRender,
+  screen,
+} from 'tests/unit/helpers';
 
 describe(__filename, () => {
-  const getProps = ({ ...props } = {}) => {
-    return {
-      className: 'OverlayCard',
-      id: 'OverlayCard',
-      store: dispatchClientMetadata().store,
-      ...props,
-    };
-  };
+  const id = 'OverlayCard';
+  let store;
 
-  function render(props = {}) {
-    return shallow(<OverlayCard {...getProps(props)} />);
-  }
-
-  it('passes onEscapeOverlay to Overlay', () => {
-    const onEscapeOverlay = sinon.stub();
-    const root = render({ onEscapeOverlay });
-
-    expect(root.find(Overlay)).toHaveProp('onEscapeOverlay', onEscapeOverlay);
+  beforeEach(() => {
+    store = dispatchClientMetadata().store;
   });
 
-  it('passes the header', () => {
-    const root = render({ header: 'this is a header' });
+  function render({ overlayChildren, ...props } = {}) {
+    const html = <div>a child div</div>;
 
-    expect(root.find('.OverlayCard')).toHaveProp('header', 'this is a header');
+    return defaultRender(
+      <OverlayCard id={id} {...props}>
+        {overlayChildren || html}
+      </OverlayCard>,
+      { store },
+    );
+  }
+
+  it('passes the header', () => {
+    const header = 'This is a header';
+    render({ header });
+
+    expect(screen.getByText(header)).toBeInTheDocument();
   });
 
   it('passes a footer link', () => {
-    const footerLink = <a href="/somewhere">link</a>;
-    const root = render({ footerLink });
+    const linkText = 'a link';
+    const linkHref = '/some/path/';
+    render({ footerLink: <a href={linkHref}>{linkText}</a> });
 
-    expect(root.find('.OverlayCard')).toHaveProp('footerLink', footerLink);
+    expect(screen.getByRole('link', { name: linkText })).toHaveAttribute(
+      'href',
+      linkHref,
+    );
   });
 
   it('passes footer text', () => {
     const footerText = 'footer text';
-    const root = render({ footerText });
+    render({ footerText });
 
-    expect(root.find('.OverlayCard')).toHaveProp('footerText', footerText);
+    expect(screen.getByText(footerText)).toBeInTheDocument();
   });
 
   it('passes children', () => {
-    const root = render({ children: <div className="kids">hi</div> });
+    const childText = 'hi';
+    render({ overlayChildren: <div className="kids">hi</div> });
 
-    expect(root.find('.OverlayCard').children()).toHaveText('hi');
+    expect(screen.getByText(childText)).toBeInTheDocument();
+  });
+
+  describe('Tests for Overlay', () => {
+    it('is hidden by default', () => {
+      render();
+
+      expect(screen.getByClassName('Overlay')).not.toHaveClass(
+        'Overlay--visible',
+      );
+    });
+
+    it('becomes visible when its mounted with visibleOnLoad prop as true', () => {
+      render({ visibleOnLoad: true });
+
+      expect(screen.getByClassName('Overlay')).toHaveClass('Overlay--visible');
+    });
+
+    it('becomes visible when the UIState changes to visible: true', () => {
+      render();
+
+      expect(screen.getByClassName('Overlay')).not.toHaveClass(
+        'Overlay--visible',
+      );
+
+      store.dispatch(
+        setUIState({
+          id: `src/amo/components/Overlay/index.js-${id}`,
+          change: { visible: true },
+        }),
+      );
+
+      expect(screen.getByClassName('Overlay')).toHaveClass('Overlay--visible');
+    });
+
+    it('renders extra className if provided', () => {
+      const className = 'I-am-so-over-it';
+      render({ className });
+
+      expect(screen.getByClassName('OverlayCard')).toHaveClass(className);
+    });
+
+    it('renders children', () => {
+      const text = 'overriding div text..';
+      render({ overlayChildren: text });
+
+      expect(screen.getByText(text)).toBeInTheDocument();
+    });
+
+    it('calls onEscapeOverlay when clicking the background', () => {
+      const onEscapeOverlay = jest.fn();
+      render({ onEscapeOverlay });
+
+      userEvent.click(screen.getByRole('presentation'));
+
+      expect(onEscapeOverlay).toHaveBeenCalled();
+    });
+
+    it('hides when you click the background', () => {
+      render({ visibleOnLoad: true });
+
+      expect(screen.getByClassName('Overlay')).toHaveClass('Overlay--visible');
+
+      userEvent.click(screen.getByRole('presentation'));
+
+      expect(screen.getByClassName('Overlay')).not.toHaveClass(
+        'Overlay--visible',
+      );
+    });
+
+    it('hides when the "Esc" key is pressed', () => {
+      render({ visibleOnLoad: true });
+
+      expect(screen.getByClassName('Overlay')).toHaveClass('Overlay--visible');
+
+      // This clicks the Escape key.
+      fireEvent.keyDown(screen.getByClassName('Overlay'), {
+        key: 'Escape',
+        keyCode: 27,
+        which: 27,
+      });
+
+      expect(screen.getByClassName('Overlay')).not.toHaveClass(
+        'Overlay--visible',
+      );
+    });
+
+    describe('extractId', () => {
+      it('returns a unique ID provided by the ID prop', () => {
+        const theId = 'some-id';
+        expect(extractId({ id: theId })).toEqual(theId);
+      });
+    });
   });
 });
