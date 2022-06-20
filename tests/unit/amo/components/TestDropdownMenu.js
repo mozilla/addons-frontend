ab164d4ab59e39af882d7b20cd7170b70bc769ca
Bob Silverberg <bob.silverberg@gmail.com>
2022-06-20 10:34:43 -0400

    Migrate TestDropdownMenu.js and TestDropdownMenuItem.js

--- tests/unit/amo/components/TestDropdownMenu.js
+++ tests/unit/amo/components/TestDropdownMenu.js
@@ -1,162 +1,273 @@
 import * as React from 'react';
-import { mount } from 'enzyme';
+import userEvent from '@testing-library/user-event';
 
-import DropdownMenu, { DropdownMenuBase } from 'amo/components/DropdownMenu';
+import DropdownMenu from 'amo/components/DropdownMenu';
 import DropdownMenuItem from 'amo/components/DropdownMenuItem';
-import Icon from 'amo/components/Icon';
-import { createFakeEvent, shallowUntilTarget } from 'tests/unit/helpers';
+import Link from 'amo/components/Link';
+import { render as defaultRender, screen } from 'tests/unit/helpers';
 
 describe(__filename, () => {
-  const renderComponent = (componentInstance) => {
-    return shallowUntilTarget(componentInstance, DropdownMenuBase, {
-      // This is needed because of `react-onclickoutside`, see:
-      // https://github.com/mozilla/addons-frontend/issues/5879
-      shallowOptions: { disableLifecycleMethods: true },
-    });
+  const defaultMenuText = 'Menu';
+  let _window = {
+    matchMedia: jest.fn().mockReturnValue({ matches: false }),
+  };
+
+  const getMenu = () => screen.getByClassName('DropdownMenu');
+  const getMenuButton = (name = defaultMenuText) =>
+    screen.getByRole('button', { name });
+  const getItem = () => screen.getByRole('listitem');
+
+  const clickMenu = (name = defaultMenuText) =>
+    userEvent.click(getMenuButton(name));
+
+  const render = ({ children, text = defaultMenuText, ...props } = {}) => {
+    return defaultRender(
+      <DropdownMenu _window={_window} text={text} {...props}>
+        {children}
+      </DropdownMenu>,
+    );
   };
 
   it('renders a menu', () => {
-    const menu = renderComponent(<DropdownMenu text="Menu" />);
+    render();
 
-    expect(menu).toHaveClassName('DropdownMenu');
-    expect(menu.find('.DropdownMenu-button')).toIncludeText('Menu');
-    expect(menu.find(Icon)).toHaveLength(1);
-    expect(menu.find('.DropdownMenu-items')).toHaveLength(0);
+    expect(screen.getByClassName('DropdownMenu')).toBeInTheDocument();
+    expect(getMenu()).toHaveTextContent(defaultMenuText);
+    expect(screen.getByClassName('Icon-inverted-caret')).toBeInTheDocument();
+
+    clickMenu();
+
+    expect(screen.queryByRole('list')).not.toBeInTheDocument();
+    expect(screen.queryByRole('listitem')).not.toBeInTheDocument();
   });
 
   it('renders items passed as children', () => {
-    const menu = renderComponent(
-      <DropdownMenu text="Menu">
-        <DropdownMenuItem>A section</DropdownMenuItem>
-      </DropdownMenu>,
-    );
+    render({ children: <DropdownMenuItem>A section</DropdownMenuItem> });
 
-    expect(menu.find('.DropdownMenu-items')).toHaveLength(1);
-    expect(menu.find(DropdownMenuItem)).toHaveLength(1);
+    clickMenu();
+
+    expect(screen.getByRole('list')).toBeInTheDocument();
+    expect(screen.getAllByRole('listitem')).toHaveLength(1);
   });
 
   it('toggles the menu state when button is clicked', () => {
-    const menu = renderComponent(<DropdownMenu text="Menu" />);
-    expect(menu).not.toHaveClassName('DropdownMenu--active');
+    render();
+    expect(getMenu()).not.toHaveClass('DropdownMenu--active');
 
     // User clicks the menu main button.
-    menu.find('.DropdownMenu-button').simulate('click', createFakeEvent());
-    expect(menu).toHaveClassName('DropdownMenu--active');
+    clickMenu();
+    expect(getMenu()).toHaveClass('DropdownMenu--active');
 
     // User clicks the menu main button, again.
-    menu.find('.DropdownMenu-button').simulate('click', createFakeEvent());
-    expect(menu).not.toHaveClassName('DropdownMenu--active');
+    clickMenu();
+    expect(getMenu()).not.toHaveClass('DropdownMenu--active');
   });
 
   it('resets the menu state on blur', () => {
-    const root = mount(<DropdownMenu text="Menu" />);
-    // When using `mount`, Enzyme 3+ requires to `.find()` the elements all the
-    // time, in case something has changed.
-    const getMenu = () => root.find('.DropdownMenu');
+    // Render the menu in a document with another element.
+    defaultRender(
+      <div>
+        <h1>Some Heading</h1>
+        <DropdownMenu _window={_window} text={defaultMenuText} />
+      </div>,
+    );
 
     // User clicks the menu main button.
-    getMenu().find('.DropdownMenu-button').simulate('click', createFakeEvent());
-    expect(getMenu()).toHaveClassName('DropdownMenu--active');
+    clickMenu();
+    expect(getMenu()).toHaveClass('DropdownMenu--active');
 
     // User clicks somewhere else.
-    // The first `instance()` call is Enzyme API, the second `getInstance()`
-    // call is react-onclickoutside API. See:
-    // https://github.com/Pomax/react-onclickoutside#but-how-can-i-access-my-component-it-has-an-api-that-i-rely-on
-    root.instance().getInstance().handleClickOutside();
-    // When using `mount`, Enzyme 3+ requires to `.update()` when we manually
-    // make changes, like above.
-    root.update();
-
-    expect(getMenu()).not.toHaveClassName('DropdownMenu--active');
+    userEvent.click(screen.getByRole('heading'));
+    expect(getMenu()).not.toHaveClass('DropdownMenu--active');
   });
 
   it('resets the menu state on click', () => {
     // See: https://github.com/mozilla/addons-frontend/issues/3452
-    const root = mount(
-      <DropdownMenu text="Menu">
+    render({
+      children: (
         <DropdownMenuItem>
           <a className="TestLink" href="/test-link/">
             Test!
           </a>
         </DropdownMenuItem>
-      </DropdownMenu>,
-    );
-    const getMenu = () => root.find('.DropdownMenu');
+      ),
+    });
 
     // User clicks the menu main button to open it.
-    getMenu().find('.DropdownMenu-button').simulate('click', createFakeEvent());
-    expect(getMenu()).toHaveClassName('DropdownMenu--active');
+    clickMenu();
+    expect(getMenu()).toHaveClass('DropdownMenu--active');
 
     // User clicks a link.
-    getMenu().find('.TestLink').simulate('click', createFakeEvent());
-    expect(getMenu()).not.toHaveClassName('DropdownMenu--active');
+    userEvent.click(screen.getByRole('link'));
+    expect(getMenu()).not.toHaveClass('DropdownMenu--active');
   });
 
   it('sets active on mouseEnter/clears on mouseLeave', () => {
-    const _window = {
-      matchMedia: sinon.stub().returns({ matches: true }),
-    };
-
-    // We do this instead of a :hover with CSS to allow clearing the active
-    // state after a click.
-    const root = mount(<DropdownMenu text="Menu" _window={_window} />);
-    const getMenu = () => root.find('.DropdownMenu');
+    _window.matchMedia = jest.fn().mockReturnValue({ matches: true });
+    render();
 
     // User hovers on the menu.
-    getMenu().simulate('mouseEnter', createFakeEvent());
-    expect(getMenu()).toHaveClassName('DropdownMenu--active');
-    sinon.assert.calledWith(_window.matchMedia, '(hover)');
+    userEvent.hover(getMenu());
+    expect(getMenu()).toHaveClass('DropdownMenu--active');
+    expect(_window.matchMedia).toHaveBeenCalledWith('(hover)');
+
+    _window.matchMedia.mockClear();
 
     // User's mouse leaves the menu.
-    _window.matchMedia.resetHistory();
-    getMenu().simulate('mouseLeave', createFakeEvent());
-    expect(getMenu()).not.toHaveClassName('DropdownMenu--active');
-    sinon.assert.calledWith(_window.matchMedia, '(hover)');
+    userEvent.unhover(getMenu());
+    expect(getMenu()).not.toHaveClass('DropdownMenu--active');
+    expect(_window.matchMedia).toHaveBeenCalledWith('(hover)');
   });
 
   it('does not touch active on mouseleave/enter if device doesnt support hover', () => {
-    const _window = {
-      matchMedia: sinon.stub().returns({ matches: false }),
-    };
-
-    // We do this instead of a :hover with CSS to allow clearing the active
-    // state after a click.
-    const root = mount(<DropdownMenu text="Menu" _window={_window} />);
-    const getMenu = () => root.find('.DropdownMenu');
+    _window.matchMedia = jest.fn().mockReturnValue({ matches: false });
+    render();
 
     // User hovers on the menu.
-    getMenu().simulate('mouseEnter', createFakeEvent());
-    expect(getMenu()).not.toHaveClassName('DropdownMenu--active');
-    sinon.assert.calledWith(_window.matchMedia, '(hover)');
+    userEvent.hover(getMenu());
+    expect(getMenu()).not.toHaveClass('DropdownMenu--active');
+    expect(_window.matchMedia).toHaveBeenCalledWith('(hover)');
 
     // User's mouse leaves the menu (no changes).
-    _window.matchMedia.resetHistory();
-    getMenu().simulate('mouseLeave', createFakeEvent());
-    expect(getMenu()).not.toHaveClassName('DropdownMenu--active');
-    sinon.assert.calledWith(_window.matchMedia, '(hover)');
+    userEvent.unhover(getMenu());
+    expect(getMenu()).not.toHaveClass('DropdownMenu--active');
+    expect(_window.matchMedia).toHaveBeenCalledWith('(hover)');
   });
 
   it('does not touch active on mouseleave/enter if window is null', () => {
-    // We do this instead of a :hover with CSS to allow clearing the active
-    // state after a click.
-    const root = mount(<DropdownMenu text="Menu" _window={null} />);
-    const getMenu = () => root.find('.DropdownMenu');
+    _window = null;
+    render();
 
     // User hovers on the menu.
-    getMenu().simulate('mouseEnter', createFakeEvent());
-    expect(getMenu()).not.toHaveClassName('DropdownMenu--active');
+    userEvent.hover(getMenu());
+    expect(getMenu()).not.toHaveClass('DropdownMenu--active');
 
     // User's mouse leaves the menu (no changes).
-    getMenu().simulate('mouseLeave', createFakeEvent());
-    expect(getMenu()).not.toHaveClassName('DropdownMenu--active');
+    userEvent.unhover(getMenu());
+    expect(getMenu()).not.toHaveClass('DropdownMenu--active');
   });
 
   it('optionally takes a class name', () => {
-    const menu = renderComponent(
-      <DropdownMenu text="Menu" className="my-class" />,
-    );
+    const className = 'MyClass';
+    render({ className });
 
-    expect(menu).toHaveClassName('DropdownMenu');
-    expect(menu).toHaveClassName('my-class');
+    expect(getMenu()).toHaveClass(className);
+  });
+
+  describe('Tests for DropdownMenuItem', () => {
+    const renderWithItems = (children) => {
+      render({ children });
+      clickMenu();
+    };
+
+    it('renders a section when only `children` prop is supplied', () => {
+      const text = 'A section';
+      renderWithItems(<DropdownMenuItem>{text}</DropdownMenuItem>);
+
+      const item = getItem();
+      expect(item).toHaveClass('DropdownMenuItem');
+      expect(item).toHaveClass('DropdownMenuItem-section');
+      expect(item).toHaveTextContent(text);
+    });
+
+    it('renders a `Link` passed in children', () => {
+      const linkText = 'a link';
+      const linkHref = '/some/path/';
+      renderWithItems(
+        <DropdownMenuItem>
+          <Link to={linkHref}>{linkText}</Link>
+        </DropdownMenuItem>,
+      );
+
+      const item = getItem();
+      expect(item).toHaveClass('DropdownMenuItem');
+      expect(item).toHaveClass('DropdownMenuItem-link');
+      expect(screen.getByRole('link', { name: linkText })).toHaveAttribute(
+        'href',
+        `/en-US/android${linkHref}`,
+      );
+    });
+
+    it('can visually detach a link item', () => {
+      renderWithItems(
+        <DropdownMenuItem detached>
+          <Link to="/">a link detached from the rest of the menu</Link>
+        </DropdownMenuItem>,
+      );
+
+      const item = getItem();
+      expect(item).toHaveClass('DropdownMenuItem');
+      expect(item).toHaveClass('DropdownMenuItem-link');
+      expect(item).toHaveClass('DropdownMenuItem--detached');
+    });
+
+    it('renders a `button` when `onClick` prop is supplied', () => {
+      const buttonText = 'A button';
+      const onClick = jest.fn();
+      renderWithItems(
+        <DropdownMenuItem onClick={onClick}>{buttonText}</DropdownMenuItem>,
+      );
+
+      const item = getItem();
+      expect(item).toHaveClass('DropdownMenuItem');
+      expect(item).toHaveClass('DropdownMenuItem-link');
+
+      userEvent.click(screen.getByRole('button', { name: buttonText }));
+      expect(onClick).toHaveBeenCalled();
+    });
+
+    it('can visually detach a button item', () => {
+      const onClick = jest.fn();
+      renderWithItems(
+        <DropdownMenuItem onClick={onClick} detached>
+          A button that is detached from the rest of the menu
+        </DropdownMenuItem>,
+      );
+
+      const item = getItem();
+      expect(item).toHaveClass('DropdownMenuItem');
+      expect(item).toHaveClass('DropdownMenuItem-link');
+      expect(item).toHaveClass('DropdownMenuItem--detached');
+    });
+
+    it('optionally takes a class name', () => {
+      const className = 'MyClass';
+      renderWithItems(
+        <DropdownMenuItem className={className}>A section</DropdownMenuItem>,
+      );
+
+      const item = getItem();
+      expect(item).toHaveClass('DropdownMenuItem');
+      expect(item).toHaveClass('DropdownMenuItem-section');
+      expect(item).toHaveClass(className);
+    });
+
+    it('can render a disabled button', () => {
+      const buttonText = 'A button';
+      const onClick = jest.fn();
+      renderWithItems(
+        <DropdownMenuItem onClick={onClick} disabled>
+          {buttonText}
+        </DropdownMenuItem>,
+      );
+
+      expect(getItem()).toHaveClass('DropdownMenuItem--disabled');
+      expect(screen.getByRole('button', { name: buttonText })).toBeDisabled();
+    });
+
+    it('renders a button with a title', () => {
+      const onClick = jest.fn();
+      const title = 'some title';
+      renderWithItems(
+        <DropdownMenuItem onClick={onClick} title={title}>
+          A button with title
+        </DropdownMenuItem>,
+      );
+
+      expect(screen.getByRole('button', { name: title })).toHaveAttribute(
+        'title',
+        title,
+      );
+    });
   });
 });
