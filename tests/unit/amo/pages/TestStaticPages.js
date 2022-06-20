58c174ba19204e5ee2ed3f93937ec6eb71eb9228
Bob Silverberg <bob.silverberg@gmail.com>
2022-06-20 13:38:31 -0400

    Migrate /components/TestStaticPage.js

--- tests/unit/amo/pages/TestStaticPages.js
+++ tests/unit/amo/pages/TestStaticPages.js
@@ -1,4 +1,10 @@
-import { renderPage as defaultRender, screen } from 'tests/unit/helpers';
+import { waitFor } from '@testing-library/react';
+
+import {
+  getElement,
+  renderPage as defaultRender,
+  screen,
+} from 'tests/unit/helpers';
 
 describe(__filename, () => {
   function render({ location }) {
@@ -23,5 +29,25 @@ describe(__filename, () => {
         name: 'Frequently Asked Questions about Reviews',
       }),
     ).toBeInTheDocument();
+  });
+
+  describe('Tests for StaticPage component', () => {
+    it('renders a StaticPage Component', async () => {
+      render({ location: '/en-US/about' });
+
+      await waitFor(() => {
+        expect(getElement('meta[name="description"]')).toHaveAttribute(
+          'content',
+          `The official Mozilla site for downloading Firefox extensions and ` +
+            `themes. Add new features and change the browser’s appearance to ` +
+            `customize your web experience.`,
+        );
+      });
+
+      expect(getElement('title')).toHaveTextContent(
+        'About Firefox Add-ons – Add-ons for Firefox Android (en-US)',
+      );
+      expect(getElement('link[rel="canonical"]')).toBeInTheDocument();
+    });
   });
 });
