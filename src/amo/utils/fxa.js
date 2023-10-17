bb927537580ff9b9c650c397c7219e8add81d5fd
Greg Sherman <gsherman@mozilla.com>
2023-10-17 13:19:49 -0400

    use getTime method for utc comp

--- src/amo/utils/fxa.js
+++ src/amo/utils/fxa.js
@@ -2,4 +2,5 @@
 export const MZA_LAUNCH_DATETIME: Date = new Date(
   Date.UTC(2023, 10, 1, 19, 0, 0, 0),
 );
-export const isMzaBranding = (): boolean => new Date() > MZA_LAUNCH_DATETIME;
+export const isMzaBranding = (): boolean =>
+  new Date().getTime() >= MZA_LAUNCH_DATETIME.getTime();
