f79c3fe3141324bf35c3efc6a93d8d2f101a9516
Andrew Williamson <awilliamson@mozilla.com>
2023-10-24 11:11:22 +0100

    move mza_launch_datetime to allow it to be tested

--- src/amo/utils/fxa.js
+++ src/amo/utils/fxa.js
@@ -1,5 +1,5 @@
 /* @flow */
 export const MZA_LAUNCH_DATETIME: Date = new Date(
-  Date.UTC(2023, 10, 1, 19, 0, 0, 0),
+  Date.UTC(2023, 09, 1, 19, 0, 0, 0),
 );
 export const isMzaBranding = (): boolean => new Date() >= MZA_LAUNCH_DATETIME;
