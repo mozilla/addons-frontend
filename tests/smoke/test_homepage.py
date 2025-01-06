19293798558747e584074465167b786bbfdb1a63
Mathieu Pillard <mpillard@mozilla.com>
2025-01-06 16:01:22 +0100

    Replace smoke test with a basic test against __frontend_lbheartbeat__
    We only really need care to check if the docker image express server
    works, so we use an endpoint that doesn't require the API (or even
    the built statics) with a simpler setup.
    
    Because it's lighter it can be included in the default job that is
    ran even on PRs.
    
    `tox` is also removed as it's just not necessary anymore.

--- tests/smoke/test_homepage.py
+++
@@ -1,9 +0,0 @@
-"""Smoke tests for addons-frontend docker image."""
-
-import requests
-
-
-def test_hidden_comment_is_found(base_url):
-    """Test hidden element is found."""
-    r = requests.get(base_url)
-    assert "<!-- Godzilla of browsers -->" in r.text
